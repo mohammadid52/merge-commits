4f945f2c23545c8a0adc53d7ac167acec9c98626
Jayne Phillips <jaynephillips61@gmail.com>
2020-09-01 17:20:58 +0200

    teacher view changes

--- src/components/Dashboard/LessonPlanner/LessonPlanHome.tsx
+++ src/components/Dashboard/LessonPlanner/LessonPlanHome.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import {
     Link
 } from 'react-router-dom'
+import Class from '../Classroom/Class';
+import Upcoming from '../Classroom//Upcoming';
+import Completed from '../Classroom/Completed';
 
 const LessonPlanHome = () => {
     return (
-        <div>
-            <Link to="/lesson-control?id=1">
-                Teacher View
-            </Link>
+        <div className={`w-full h-9.28/10 md:h-auto flex flex-col p-4 md:p-8`}>
+            <Class link="/lesson-control?id=1" />
+            {/* <Link to="/lesson-control?id=1">
+                Teacher View 
+            </Link> */}
+            <Upcoming />
+            <Completed />
         </div>
     )
 }
