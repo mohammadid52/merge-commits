a25d25b0bad248be961585e4f7ca6da60053d091
Jayne Phillips <jaynephillips61@gmail.com>
2020-09-04 12:57:08 +0200

    teacher view do first

--- src/components/TeacherView/ComponentViews/IntroView/DoFirst.tsx
+++ src/components/TeacherView/ComponentViews/IntroView/DoFirst.tsx
@@ -15,17 +15,23 @@ const DoFirst = (props: props) => {
     const questionArray = questions.items;
     const [ input, setInput ] = useState('');
 
-    const inputSwitch = (type: string) => {
-        switch(type) {
+    const inputSwitch = (question: {question: string, type: string}) => {
+        switch(question.type) {
             case "input" : 
                 return (
-                    <input className="p-2 bg-gray-300 w-full text-sm md:text-2xl text-gray-800 rounded-lg shadow-2" 
-                    />
+                    <div className="h-full flex flex-col justify-between">
+                        <p className={`${fullscreen ? 'text-xl' : 'text-sm'} h-3/10 mb-2`}>{question.question}</p>
+                        <input className={`${fullscreen ? 'h-6/10 ' : 'h-6/10'} p-2 bg-gray-300 w-full text-sm md:text-2xl text-gray-800 rounded-lg shadow-2`} 
+                        />
+                    </div>
                 )
             case "text" :
                 return (
-                    <textarea className="h-full p-6 bg-gray-300 w-full text-sm md:text-2xl text-gray-800 rounded-lg shadow-2" 
-                    />
+                    <div className="h-full flex flex-col justify-between">
+                        <p className={`${fullscreen ? 'text-xl' : 'text-sm'} mb-2`}>{question.question}</p>
+                        <textarea className={`${fullscreen ? 'h-7/10 ' : 'h-5.8/10'} p-6 bg-gray-300 w-full text-sm md:text-2xl text-gray-800 rounded-lg shadow-2`} 
+                        />
+                    </div>
                 )
             default :
             return null
@@ -45,11 +51,10 @@ const DoFirst = (props: props) => {
                 <h3 className={`${fullscreen ? 'text-2xl' : 'text-lg'} w-full text-gray-200 font-open font-bold border-b border-white mr-4 mb-4`}>
                     Do First
                 </h3>
-                <div className="w-full h-8.5/10 flex flex-col text-gray-200">
-                {questionArray.map((item: {question: any}, key: number) => (
-                    <div key={key}>
-                        <p className={`${fullscreen ? 'text-xl my-8' : 'text-base my-2'}`}>{item.question.question}</p>
-                        {inputSwitch(item.question.type)}
+                <div className="w-full h-8.5/10 flex flex-col text-gray-200 justify-around">
+                {questionArray.map((item: {question: {question: string, type: string}}, key: number) => (
+                    <div key={key} className="flex flex-col justify-between">
+                        {inputSwitch(item.question)}
                     </div>
                 ) )}
                 
