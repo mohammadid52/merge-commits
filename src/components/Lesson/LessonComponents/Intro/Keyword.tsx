a74b65537a49d0bcef9c4532cebc197f6f90bf19
Jayne Phillips <jaynephillips61@gmail.com>
2020-09-08 11:55:42 +0200

    changes

--- src/components/Lesson/LessonComponents/Intro/Keyword.tsx
+++ src/components/Lesson/LessonComponents/Intro/Keyword.tsx
@@ -18,8 +18,8 @@ const Keyword = () => {
 
 
     return (
-        <div className={`md:w-full md:h-full ${theme.block.bg} flex flex-col ${theme.block.text} text-lx rounded-sm shadow-inner`}>
-            <h1 className="text-2xl font-extrabold mb-6 underline">Keywords we will cover in this lesson:</h1>
+        <div className={`md:w-full md:h-full ${theme.block.bg} flex flex-col ${theme.block.text} text-lx rounded-sm`}>
+            <h1 className="text-2xl font-extrabold mb-4 underline">Keywords we will cover in this lesson:</h1>
             <div>
             <p className="text-lg "><span className="text-lg font-bold">Culture:</span> the beliefs, social practices, and characteristics of a racial, religious, or social group </p>
             <p className="text-lg "><span className="text-lg font-bold">Identity:</span> all of those things by which a person or thing is known or is considered as being </p>
