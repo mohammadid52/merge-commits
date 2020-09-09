64e6c7db88cb65259283cf854055d55323951baf
Andy Davis <AndrewMarkham@seznam.cz>
2020-09-09 17:57:41 +0200

    change

--- src/customGraphql/customQueries.ts
+++ src/customGraphql/customQueries.ts
@@ -280,6 +280,208 @@ export const getClassroom = /* GraphQL */ `
   }
 `;
 
+export const getClassroomStudent = /* GraphQL */ `
+  query GetClassroom($id: ID!) {
+    getClassroom(id: $id) {
+      id
+      open
+      roster
+      displayData {
+        breakdownComponent
+        warmUpData {
+          story
+          title
+          additional {
+            name
+            input
+          }
+        }
+        corelessonData {
+          selected {
+            anchor
+            color
+            content {
+              id
+              text
+            }
+            focus
+            id
+          }
+        }
+        activityData {
+          editInput
+          editMode
+          lines {
+            example
+            id
+            menuOpen
+            text
+          }
+          title
+        }
+      }
+      lessonID
+      lesson {
+        id
+        title
+        artist {
+          id
+          images
+          name
+          type
+          bio
+          quotes {
+            source
+            text
+          }
+        }
+        language
+        summary
+        objectives
+        checkpoints {
+          items {
+            position
+            checkpoint {
+              instructions
+              label
+              type
+              questions {
+                items {
+                  required
+                  question {
+                    label
+                    type
+                    question
+                    options {
+                      text
+                      icon
+                      label
+                      color
+                    }
+                  }
+                }
+                nextToken
+              }
+            }
+          }
+          nextToken
+        }
+        doFirstID
+        doFirst {
+          id
+          type
+          required
+          questions {
+            items {
+              question {
+                label
+                type
+                question
+                options {
+                  text
+                  icon
+                  label
+                  color
+                }
+              }
+            }
+            nextToken
+          }
+        }
+        warmUpId
+        warmUp {
+          id
+          title
+          label
+          stage
+          type
+          language
+          instructions {
+            video
+            link
+            text
+          }
+          inputs {
+            title
+            additionalInputs {
+              id
+              name
+              prompt
+              example
+            }
+          }
+          breakdown {
+            reflectionQuestions
+          }
+        }
+        coreLessonId
+        coreLesson {
+          id
+          title
+          label
+          stage
+          type
+          language
+          instructions {
+            video
+            link
+            text
+          }
+          content {
+            video
+            link
+            title
+            artist
+            text
+          }
+          tools {
+            name
+            color
+            icon
+          }
+          breakdown {
+            reflectionQuestions
+          }
+        }
+        activityId
+        activity {
+          id
+          title
+          label
+          stage
+          type
+          language
+          lineNumber
+          instructions {
+            video
+            link
+            text
+          }
+          writingPrompts {
+            id
+            name
+            prompt
+            example
+          }
+          breakdown {
+            reflectionQuestions
+          }
+        }
+      }
+      lessonPlan {
+        disabled
+        open
+        active
+        stage
+        type
+        displayMode
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
 export const getStudentData = /* GraphQL */ `
   query GetStudentData($classroomID: ID!, $studentID: String!) {
     getStudentData(classroomID: $classroomID, studentID: $studentID) {
@@ -326,6 +528,7 @@ export const getStudentData = /* GraphQL */ `
     }
   }
 `;
+
 export const getCourse = /* GraphQL */ `
   query GetCourse($id: ID!) {
     getCourse(id: $id) {
