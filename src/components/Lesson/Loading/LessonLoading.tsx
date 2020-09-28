91be1077c6deb4611c190ee822b507499267611e
samboosa5k <jasper.verbon@gmail.com>
2020-09-28 16:26:01 +0200

    fixed loading flash

--- src/components/Lesson/Loading/LessonLoading.tsx
+++ src/components/Lesson/Loading/LessonLoading.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LessonLoading = () => {
   return (
-    <div className='min-h-screen w-full bg-dark text-gray-200 font-open font-light flex flex-col animate-fadeIn  justify-center items-center'>
-        <div className='relative w-full mb-4 pb-4 flex flex-col justify-center items-center rounded-lg'>
+    <div className='min-h-screen w-full bg-dark text-gray-200 font-open font-light flex flex-col justify-center items-center'>
+        <div className='relative w-full mb-4 pb-4 flex flex-col justify-center items-center rounded-lg animate-fadeIn'>
           <div
             className={`w-64 h-48 p-8 text-lg text-center text-blue-100 flex flex-col justify-center items-center font-light bg-gray-300 rounded-t-lg`}>
             <img
