f1badefdbb3f43d18fec191e4b28802125dda111
samboosa5k <jasper.verbon@gmail.com>
2020-09-29 10:34:25 +0200

    added lowercase form emails

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -90,15 +90,22 @@ const Login = () => {
         }
     }
 
-    const handleChange = (e: { target: { id: any; value: any; }; }) => {
-        const { id, value } = e.target;
-        setInput(input => {
-            return {
-                ...input,
-                [id]: value,
-            }
-        })
-    }
+    const handleChange = (e: { target: { id: any; value: any } }) => {
+      const { id, value } = e.target;
+      setInput((input) => {
+        if (id === 'email') {
+          return {
+            ...input,
+            [id]: value.toLowerCase(),
+          };
+        } else {
+          return {
+            ...input,
+            [id]: value,
+          };
+        }
+      });
+    };
 
     const handleEnter = (e: any) => {
         if (e.key === 'Enter') {
