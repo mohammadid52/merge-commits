741c2e3a5bbafcffafc44f19a015f798538d2e08
samboosa5k <jasper.verbon@gmail.com>
2020-10-02 09:51:45 +0200

    webpack improved all errors gone

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,7 @@ const path = require('path');
 // const webpack = require('webpack');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  entry: path.resolve(__dirname, './src', 'index.tsx'),
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js',
