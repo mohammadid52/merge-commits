3bad4338d03dd0d10c67f729b2d2a75137206969
samboosa5k <jasper.verbon@gmail.com>
2020-10-12 14:24:46 +0200

    updated other sidemenu buttons

--- src/components/Dashboard/Menu/SideMenu.tsx
+++ src/components/Dashboard/Menu/SideMenu.tsx
@@ -2,19 +2,27 @@ import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { GlobalContext } from '../../../contexts/GlobalContext';
 
+import {LinkProps} from './Links';
+
 interface SideMenuProps {
     children: React.ReactNode;
     [key: string]: any;
+    // linkProps: LinkProps;
 }
 
-const SideMenu: React.FC = ({children, ...props}: SideMenuProps) => {
+const SideMenu: React.FC<SideMenuProps> = ({children, ...props}: SideMenuProps) => {
     const { theme } = useContext(GlobalContext);
+
+    const handleLink = (e: any) => {
+        const id = e.target.id.toLowerCase();
+        props.setCurrentPage(id);
+      };
 
     return (
         <div className={`min-h-screen w-44 md:flex flex-row md:flex-col  ${theme.sidemenu.bg}`}>
             <div className={`hidden w-full h-12 ${theme.sidemenu.bg} md:flex justify-center items-center text-2xl font-bold z-50`}>
-                <NavLink to="/dashboard">
-                    <img className="h-6" src="https://zoiqclients.s3.amazonaws.com/IconoclastArtist/IconoclastArtistsLogos/logo_white.svg" alt="Iconoclast Artists"/>
+                <NavLink id="dashboard" to="/dashboard" onClick={handleLink}>
+                    <img id="dashboard" className="h-6" src="https://zoiqclients.s3.amazonaws.com/IconoclastArtist/IconoclastArtistsLogos/logo_white.svg" alt="Iconoclast Artists"/>
                 </NavLink>
             </div>
             <div className={`sidebar w-full h-1/2 text-gray-200 cursor-pointer`}>
