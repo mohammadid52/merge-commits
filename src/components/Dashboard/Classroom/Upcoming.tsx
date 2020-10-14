406c9916eb57d44b7baa3d98e07b4f676e7e6b6e
samboosa5k <jasper.verbon@gmail.com>
2020-10-14 10:34:06 +0200

    fixed daSHboard style bugfs

--- src/components/Dashboard/Classroom/Upcoming.tsx
+++ src/components/Dashboard/Classroom/Upcoming.tsx
@@ -166,7 +166,7 @@ const UpcomingClass: React.FC<UpcomingProps> = (props: UpcomingProps) => {
                   <div className='w-full relative flex flex-col rounded-b-xl'>
                     <div
                       className={`${
-                        openCards.includes('upcoming-1') ? 'h-72' : 'h-32 '
+                        openCards.includes('upcoming-1') ? 'h-72' : 'h-32'
                       } p-4 mb-2 flex flex-col justify-start overflow-hidden ease-in-out duration-500`}>
                       <h1 className={`text-lg text-black font-open text-left`}>{lesson.title}</h1>
                       <p className={`text-sm text-left`}>
@@ -229,8 +229,8 @@ const UpcomingClass: React.FC<UpcomingProps> = (props: UpcomingProps) => {
           <div className='relative w-full h-full flex flex-col rounded-b-xl'>
             <div
               className={`${
-                openCards.includes('upcoming-2') ? 'h-20' : 'h-20 '
-              } p-4 flex flex-col justify-start overflow-hidden ease-in-out duration-500`}>
+                openCards.includes('upcoming-2') ? 'h-72' : 'h-32'
+              } mb-2 p-4 flex flex-col justify-start overflow-hidden ease-in-out duration-500`}>
               <h1 className={`text-lg text-black font-open text-left`}>Ode to the rat</h1>
               <p className={`text-sm text-left `}>Summary coming soon...</p>
             </div>
@@ -290,8 +290,8 @@ const UpcomingClass: React.FC<UpcomingProps> = (props: UpcomingProps) => {
           <div className='relative w-full h-full flex flex-col rounded-b-xl'>
             <div
               className={`${
-                openCards.includes('upcoming-3') ? 'h-20' : 'h-20 '
-              } p-4 flex flex-col justify-start overflow-hidden ease-in-out duration-500`}>
+                openCards.includes('upcoming-3') ? 'h-72' : 'h-32'
+              } mb-2 p-4 flex flex-col justify-start overflow-hidden ease-in-out duration-500`}>
               <h1 className={`text-lg text-black font-open text-left`}>My Honest Poem</h1>
               <p className={`text-sm text-left`}>Summary coming soon...</p>
             </div>
