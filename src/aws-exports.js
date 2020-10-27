fa56b6f5ab55d7abbdd5cbe5d2b475485a77d37b
Andy Davis <AndrewMarkham@seznam.cz>
2020-10-27 16:45:19 +0100

    env change

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,15 +3,15 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:9a1b4916-915e-4b65-a2c7-b2286ecc16fd",
+    "aws_cognito_identity_pool_id": "us-east-1:570f5ed7-e35a-4b86-9b32-1575f0b91ed5",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_MVf87ox0v",
-    "aws_user_pools_web_client_id": "79bnbhng10ml2ucaur9vuie7vi",
+    "aws_user_pools_id": "us-east-1_aZO8Dh08l",
+    "aws_user_pools_web_client_id": "5fqplej0kiqdp7pvjssq4i76v0",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://m2cm45pahrh5hdf35cngjfdaui.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://iuj7bhgjvbhbhazv6e3oi2zqky.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-nugdxn4bwzhizjsrr2pv2m64ne"
+    "aws_appsync_apiKey": "da2-6smjv3ukrndjbculzwsxwzdy6m"
 };
 
 
