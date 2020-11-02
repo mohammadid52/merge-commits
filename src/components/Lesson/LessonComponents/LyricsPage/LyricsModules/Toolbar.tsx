744c144d9de3ae636a6c348da94f3c71022ef16e
samboosa5k <jasper.verbon@gmail.com>
2020-11-02 12:36:38 +0100

    new styles

--- src/components/Lesson/LessonComponents/LyricsPage/LyricsModules/Toolbar.tsx
+++ src/components/Lesson/LessonComponents/LyricsPage/LyricsModules/Toolbar.tsx
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React, { useEffect, useRef, useContext, useState } from 'react';
-=======
-import React, { useContext, useState } from 'react';
->>>>>>> dev
 import { IconContext } from 'react-icons';
 import { FaEraser } from 'react-icons/fa';
 import { LessonContext } from '../../../../../contexts/LessonContext';
 import ToolTip from '../../../../General/ToolTip/ToolTip';
-<<<<<<< HEAD
 
 interface ToolbarProps {
   setColor: React.Dispatch<React.SetStateAction<string>>;
@@ -28,23 +23,7 @@ const ToolBar = (props: ToolbarProps) => {
     console.log(e.target.id, 'e');
   };
 
-  // const handleChange = (e: { target: { value: React.SetStateAction<string> } }) => {
-  //   setSearch(e.target.value);
-  // };
-  // const handleDrop = (e: {
-  //   preventDefault: () => void;
-  //   dataTransfer: { getData: (arg0: string) => string };
-  // }) => {
-  //   e.preventDefault();
-  //   const wordId = e.dataTransfer.getData('wordId');
-  //   const word = document.getElementById(wordId).innerText;
-  //   if (state.word_bank.indexOf(word) < 0) {
-  //     dispatch({ type: 'ADD_WORD', payload: word });
-  //   }
-  // };
-  // const handleDragOver = (e: { preventDefault: () => void }) => {
-  //   e.preventDefault();
-  // };
+
 
   const handleScroll = () => {
     if (ref.current) {
@@ -76,16 +55,15 @@ const ToolBar = (props: ToolbarProps) => {
 
       <div ref={ref}>
         <div className={`h-16 flex  justify-center items-center pt-2 z-50`}>
-          
+
           <div
-            className={`${
-              isSticky
-                ? 'fixed top-0 w-full flex flex-row justify-center items-center bg-dark-gray translate-y-full z-50'
-                : ''
-            } w-auto cursor-pointer flex flex-row`}>
-            
+            className={`${isSticky
+              ? 'fixed top-0 w-full flex flex-row justify-center items-center bg-dark-gray translate-y-full z-50'
+              : ''
+              } w-auto cursor-pointer flex flex-row`}>
+
             {buttons.map((button: { color: string; icon: string; name: string }, key: number) => (
-              
+
               <div
                 key={key}
                 id={button.color}
@@ -112,7 +90,7 @@ const ToolBar = (props: ToolbarProps) => {
                 {button.icon}
               </div>
             ))}
-            
+
             <div
               id='erase'
               className={`relative bg-gray-200 h-12 w-12 text-3xl rounded-lg mb-2 mx-4 flex flex-row justify-center items-center`}
@@ -123,82 +101,10 @@ const ToolBar = (props: ToolbarProps) => {
               </IconContext.Provider>
             </div>
 
-
           </div>
-
-=======
-interface ToolbarProps {
-  setColor: React.Dispatch<React.SetStateAction<string>>;
-}
-const ToolBar = (props: ToolbarProps) => {
-  const { setColor } = props;
-  const { state, dispatch } = useContext(LessonContext);
-  const [search, setSearch] = useState('');
-  const buttons = state.data.lesson.coreLesson.tools;
-  const handleClick = (e: any) => {
-    setColor(e.target.id);
-    console.log(e.target.id, 'e')
-  };
-  const handleChange = (e: { target: { value: React.SetStateAction<string> } }) => {
-    setSearch(e.target.value);
-  };
-  const handleDrop = (e: {
-    preventDefault: () => void;
-    dataTransfer: { getData: (arg0: string) => string };
-  }) => {
-    e.preventDefault();
-    const wordId = e.dataTransfer.getData('wordId');
-    const word = document.getElementById(wordId).innerText;
-    if (state.word_bank.indexOf(word) < 0) {
-      dispatch({ type: 'ADD_WORD', payload: word });
-    }
-  };
-  const handleDragOver = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-  };
-  return (
-    <div className='bg-medium-blue h-2.5/10 w-full p-4 rounded-lg flex flex-col items-center justify-around'>
-      <div className='relative flex flex-row justify-center items-center border-b border-white border-opacity-10 pb-1 mb-1'>
-        <h3 className='text-xl text-gray-200 font-open font-light flex'>
-          Highlighters
-        </h3>
-        {/* <p className='text-gray-600 text-sm text-center'>
-          (click a color and drag over words!) 
-        </p> */}
-      </div>
-      <div className='w-auto cursor-pointer flex flex-row justify-center items-center pt-2'>
-        {buttons.map((button: { color: string; icon: string, name: string }, key: number) => (
-          <div
-            key={key}
-            id={button.color}
-            className={`relative ${`bg-${button.color}`} h-12 w-12 text-3xl rounded-lg mb-2 mx-4 shadow-elem-dark flex flex-row justify-center items-center animate-fadeIn`}
-            onClick={handleClick }>
-            <ToolTip position='hidden-bottom' 
-            id={button.color}
-            cursor
-            header=''
-            width='w-24 px-1 flex justify-center items-center'
-            content= {<div className="font-bold flex text-center justify-center w-24">{button.name}</div>}
-            display='none' fontSize= 'text-xs'/>
-            {button.icon}
-          </div>
-        ))}
-        <div
-          id='erase'
-          className={`relative bg-gray-200 h-12 w-12 text-3xl rounded-lg mb-2 mx-4 shadow-elem-dark flex flex-row justify-center items-center`}
-          onClick={handleClick}>
-          {/* <ToolTip position='hidden-bottom' header='eraser' display='none' fontSize= 'text-xs px-2' cursor/> */}
-          <IconContext.Provider value={{ color: 'darkgray', size: '2rem' }}>
-            <FaEraser style={{pointerEvents: 'none'}}/>
-          </IconContext.Provider>
->>>>>>> dev
         </div>
       </div>
     </div>
   );
 };
 export default ToolBar;
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
