107431f673008ff623f49d6eecb10dab75f7656d
Jayne Phillips <jaynephillips61@gmail.com>
2020-11-03 12:03:39 +0100

    width

--- src/components/Lesson/LessonComponents/ListPage/ListModules/ListForm.tsx
+++ src/components/Lesson/LessonComponents/ListPage/ListModules/ListForm.tsx
@@ -135,7 +135,7 @@ const handleInputTest = (e: KeyboardEvent<HTMLTextAreaElement>, event: ChangeEve
       <div className='relative h-full flex flex-col items-center mb-5 mt-2'>
         <textarea
           id='story'
-          className=' w-6/10 h-full px-4 py-2 rounded-lg text-xl text-gray-100'
+          className=' w-9/10 h-full px-4 py-2 rounded-lg text-xl text-gray-100'
           style={{backgroundColor: '#23314600'}}
           name='list'
           placeholder={`${bullet} ${state.data.lesson.warmUp.inputs.textExample}`}
