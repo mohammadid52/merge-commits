000d746b30edf01c89dc9e5733a3d9bab9e3c86d
Aman Gupta <guptaaman9216@gmail.com>
2020-12-02 19:02:55 +0530

    Added storage s3 bucket for image upload

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -11,7 +11,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://iuj7bhgjvbhbhazv6e3oi2zqky.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-6smjv3ukrndjbculzwsxwzdy6m"
+    "aws_appsync_apiKey": "da2-6smjv3ukrndjbculzwsxwzdy6m",
+    "aws_user_files_s3_bucket": "iconoclastimages141704-uat",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
