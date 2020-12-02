08e2e475b28af21dd8e085b0b19ff208f3b1d7c8
samboosa5k <jasper.verbon@gmail.com>
2020-12-02 13:43:12 +0100

    lesson control buttons now show disabled/closed state; fullscreen and info buttons now visible; breakdown and checkpoint numbering is improved

--- src/components/General/LabelSwitch.tsx
+++ src/components/General/LabelSwitch.tsx
@@ -3,44 +3,32 @@ import React from 'react';
 // type Role = 'ADM' | 'BLD' | 'FLW' | 'CRD' | 'TR' | 'ST';
 
 type LabelProps = {
-  label: any;
+  label: string;
+  counter?: number;
 };
 
-const FooterLabels = ({ label }: LabelProps) => {
-  switch (label) {
-    case 'Intro':
-      return <div className='whitespace-pre'>Intro</div>;
-    case 'Warmup':
-      return <div className='whitespace-pre'>Warm Up</div>;
-    case 'Corelesson':
-      return <div className='whitespace-pre'>Core Lesson</div>;
-    case 'Activity':
-      return <div className='whitespace-pre'>Activity</div>;
-    case label.includes('checkpoint?'):
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Checkpoint?id=1':
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Checkpoint?id=8':
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Checkpoint?id=9':
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Checkpoint?id=10':
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Checkpoint?id=11':
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Checkpoint?id=12':
-      return <div className='whitespace-pre'>Checkpoint</div>;
-    case 'Warmup/breakdown':
-      return <div className='whitespace-pre'>Breakdown</div>;
-    case 'Corelesson/breakdown':
-      return <div className='whitespace-pre'>Breakdown</div>;
-    case 'Activity/breakdown':
-      return <div className='whitespace-pre'>Breakdown</div>;
-    case 'Outro':
-      return <div className='whitespace-pre'>Outro</div>;
-    default:
-      return label;
-  }
+const FooterLabels = (props: LabelProps) => {
+  const { label, counter } = props;
+
+  const labelSwitch = (input: string) => {
+    if (RegExp('Breakdown', 'i').test(input)) {
+      return 'Breakdown';
+    } else {
+      if (RegExp('intro', 'i').test(input)) return 'Intro';
+      if (RegExp('Warmup', 'i').test(input)) return 'Warm Up';
+      if (RegExp('Corelesson', 'i').test(input)) return 'Core Lesson';
+      if (RegExp('Activity', 'i').test(input)) return 'Activity';
+      if (RegExp('Checkpoint', 'i').test(input)) return 'Checkpoint';
+      if (RegExp('Outro', 'i').test(input)) return 'Outro';
+    }
+  };
+
+  return (
+    <div className='whitespace-pre'>
+      {labelSwitch(label)}
+      {counter}
+    </div>
+  );
 };
 
 export default FooterLabels;
