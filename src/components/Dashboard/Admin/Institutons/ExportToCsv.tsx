b6bb2e0f4966643a5cdacebb3c8eb91e449e8296
sublimedatasys <sahilshah@sublimedatasys.com>
2020-12-08 11:49:18 +0530

    Added headers and integrate actual data.

--- src/components/Dashboard/Admin/Institutons/ExportToCsv.tsx
+++ src/components/Dashboard/Admin/Institutons/ExportToCsv.tsx
@@ -21,8 +21,20 @@ const ExportToCsv: React.FC = () => {
       if (!fetchInstitutionData) {
         throw new Error('fail!');
       } else {
-        console.log('Log institutions BEFORE: ', fetchInstitutionData);
-        setInstitutionsList(fetchInstitutionData.data.listInstitutions.items);
+        const institutionList = fetchInstitutionData.data.listInstitutions.items;
+
+        // Extract contact info from the object of contacts.
+        const updatedList = institutionList.map((inst: any) => ({
+          ...inst,
+          contactName: inst.contact?.name,
+          contactEmail: inst.contact?.email,
+          contactPhone: inst.contact?.phone
+        })).map((item: any) => {
+          delete item.contact;
+          return item;
+        });
+
+        setInstitutionsList(updatedList);
       }
     } catch (error) {
       console.error(error);
@@ -37,7 +49,11 @@ const ExportToCsv: React.FC = () => {
     <div className="w-full h-1/2 md:h-full flex items-center justify-center mt-5">
       <div className={`h-full w-1/3 main_container`}>
         <button type="submit" className=" mt-5 inline-flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out items-center">
-          <CSVLink data={dummyCsvData} >Institutions</CSVLink>  {/* dummyCsvData will be replaced with institutionsList */}
+          <CSVLink
+            data={institutionsList}
+            headers={headers}
+            filename={"Institutions_list.csv"}
+          >Institutions</CSVLink>
         </button>
       </div>
     </div>
@@ -46,46 +62,23 @@ const ExportToCsv: React.FC = () => {
 
 export default ExportToCsv;
 
+// Header part or columns in csv table.
 
-
-const dummyCsvData = [
-  {
-    name: 'Institude name',
-    district: 'District name',
-    address: 'Amizara park, street byc, NY.',
-    city: 'NY',
-    state: 'GUJ',
-    zip: '363002',
-    phone: '1234567890',
-    website: 'data.exim@yopmail.com',
-    image: 'url(`Image.url`)',
-    createdAt: '20-12-2020',
-    updatedAt: '30-11-2020'
-  },
-  {
-    name: 'Institude name',
-    district: 'District name',
-    address: 'Amizara park, street byc, NY.',
-    city: 'NY',
-    state: 'GUJ',
-    zip: '363002',
-    phone: '1234567890',
-    website: 'data.exim@yopmail.com',
-    image: 'url(`Image.url`)',
-    createdAt: '20-12-2020',
-    updatedAt: '30-11-2020'
-  },
-  {
-    name: 'Institude name',
-    district: 'District name',
-    address: 'Amizara park, street byc, NY.',
-    city: 'NY',
-    state: 'GUJ',
-    zip: '363002',
-    phone: '1234567890',
-    website: 'data.exim@yopmail.com',
-    image: 'url(`Image.url`)',
-    createdAt: '20-12-2020',
-    updatedAt: '30-11-2020'
-  },
+const headers = [
+  { label: 'Name', key: 'name' },
+  { label: 'Phone', key: 'phone' },
+  { label: 'Website', key: 'website' },
+  { label: 'Contact Name', key: 'contactName' },
+  { label: 'Contact Email', key: 'contactEmail' },
+  { label: 'Contact Phone', key: 'contactPhone' },
+  { label: 'Address Line 1', key: 'address' },
+  { label: 'Address Line 2', key: 'addressLine2' },
+  { label: 'City', key: 'city' },
+  { label: 'District name', key: 'district' },
+  { label: 'State', key: 'state' },
+  { label: 'Zip', key: 'zip' },
+  { label: 'Image', key: 'image' },
+  { label: 'Type', key: 'type' },
+  { label: 'Created At', key: 'createdAt' },
+  { label: 'Updated At', key: 'updatedAt' },
 ];
