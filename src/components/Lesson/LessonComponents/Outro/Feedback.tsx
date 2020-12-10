ce30943eafbab887737d1cb3e7bf03f99ad72f42
samboosa5k <jasper.verbon@gmail.com>
2020-12-10 16:12:34 +0100

    rating stars on outro function properly

--- src/components/Lesson/LessonComponents/Outro/Feedback.tsx
+++ src/components/Lesson/LessonComponents/Outro/Feedback.tsx
@@ -1,67 +1,65 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { IconContext } from 'react-icons/lib/esm/iconContext';
-import { FaThumbsDown, FaThumbsUp } from 'react-icons/fa';
-import { LessonContext } from '../../../../contexts/LessonContext';
+import React, {useContext, useEffect, useState} from 'react';
+import {LessonContext} from '../../../../contexts/LessonContext';
 import StarRating from './Feedback/StarRating';
 
 interface FeedbackProps {
-  setFeedback: React.Dispatch<
-    React.SetStateAction<{
-      like: string;
-      text: string;
-    }>
-  >;
+    setFeedback: React.Dispatch<React.SetStateAction<{
+        like: string;
+        text: string;
+    }>>;
 }
 
 const Feedback = (props: FeedbackProps) => {
-  const { theme, state } = useContext(LessonContext);
-  const { setFeedback } = props;
-  const [thumb, setThumb] = useState('');
-  const [feedbackText, setFeedbackText] = useState('');
-  const [isRated, setIsRated] = useState<boolean>(false);
-  const [rating, setRating] = useState<number>(0);
+    const {theme, state} = useContext(LessonContext);
+    const {setFeedback} = props;
+    const [thumb, setThumb] = useState('');
+    const [feedbackText, setFeedbackText] = useState('');
+    const [isRated, setIsRated] = useState<boolean>(false);
+    const [rating, setRating] = useState<number>(0);
 
-  const handleThumb = (item: string) => {
-    setThumb(() => {
-      if (item === thumb) {
-        return '';
-      }
+    // const handleThumb = (item: string) => {
+    //     setThumb(() => {
+    //         if (item === thumb) {
+    //             return '';
+    //         }
+    //
+    //         return item;
+    //     });
+    // };
 
-      return item;
-    });
-  };
+    const handleTextChange = (e: { target: { value: string } }) => {
+        const {value} = e.target;
 
-  const handleTextChange = (e: { target: { value: string } }) => {
-    const { value } = e.target;
+        setFeedbackText(value);
+    };
 
-    setFeedbackText(value);
-  };
+    useEffect(() => {
+        const stringRating = rating.toString();
 
-  useEffect(() => {
-    setFeedback(() => {
-      return {
-        like: thumb,
-        text: feedbackText,
-      };
-    });
-  }, [thumb, feedbackText]);
+        setFeedback(() => {
+            return {
+                like: stringRating,
+                text: feedbackText,
+            };
+        });
+    }, [rating, feedbackText]);
 
-  return (
-    <div className={`w-full h-full rounded-xl`}>
-      <div className={`w-full`}>
-        <div className={`w-full flex flex-row text-xl ${theme.banner} border-b-4 border-sea-green`}>
-          
-          <h3>What did you think about the {state.data.lesson.type}?</h3>
+    return (
+        <div className={`w-full h-full rounded-xl`}>
+            <div className={`w-full`}>
+                <div className={`w-full flex flex-row text-xl ${theme.banner} border-b-4 border-sea-green`}>
 
-          <StarRating
-            isRated={isRated}
-            setIsRated={setIsRated}
-            rating={rating}
-            setRating={setRating}
-            handleStarRating={()=>console.log('handleStarRating(): ', ' test ')}
-          />
+                    <h3>What did you think about the {state.data.lesson.type}?</h3>
 
-          {/* <div
+                    <StarRating
+                        isRated={isRated}
+                        setIsRated={setIsRated}
+                        rating={rating}
+                        setRating={setRating}
+                        handleStarRating={() => console.log('handleStarRating(): ', ' test ')}
+                    />
+
+                    {/* <div
             className={`text-sm flex justify-between items-center rounded-full w-8 h-8 z-30 cursor-pointer`}
             onClick={() => handleThumb('up')}>
             {thumb === 'up' ? (
@@ -88,20 +86,20 @@ const Feedback = (props: FeedbackProps) => {
             )}
           </div> */}
 
-        </div>
-      </div>
+                </div>
+            </div>
 
-      <div className='w-full'>
+            <div className='w-full'>
         <textarea
-          id='text'
-          className={`h-40 ${theme.elem.textInput} w-full rounded-xl`}
-          value={feedbackText}
-          onChange={handleTextChange}
-          placeholder='Do you have any comments?'
+            id='text'
+            className={`h-40 ${theme.elem.textInput} w-full rounded-xl`}
+            value={feedbackText}
+            onChange={handleTextChange}
+            placeholder='Do you have any comments?'
         />
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 };
 
 export default Feedback;
