2eeb4611c87c55ce414c66040b88fcb7d1c69a02
samboosa5k <jasper.verbon@gmail.com>
2020-12-14 19:08:14 +0100

    changed password color profile page

--- src/components/Dashboard/Profile/Profile.tsx
+++ src/components/Dashboard/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect, Fragment } from 'react';
 import API, { graphqlOperation } from '@aws-amplify/api';
 import Storage from '@aws-amplify/storage';
 import { IconContext } from 'react-icons/lib/esm/iconContext';
-import { RiLock2Fill } from "react-icons/ri";
+import { RiLock2Fill } from 'react-icons/ri';
 import { FaPlus, FaEdit, FaTrashAlt } from 'react-icons/fa';
 import { IoArrowUndoCircleOutline } from 'react-icons/io5';
 import { Switch, useHistory, Route, useRouteMatch, Link, NavLink } from 'react-router-dom';
@@ -16,7 +16,7 @@ import ProfileVault from './ProfileVault';
 import ProfileEdit from './ProfileEdit';
 import LessonLoading from '../../Lesson/Loading/ComponentLoading';
 import * as customMutations from '../../../customGraphql/customMutations';
-import ToolTip from '../../General/ToolTip/ToolTip'
+import ToolTip from '../../General/ToolTip/ToolTip';
 import ProfileCropModal from './ProfileCropModal';
 import { getImageFromS3 } from '../../../utilities/services';
 import BreadCrums from '../../Atoms/BreadCrums';
@@ -25,54 +25,51 @@ import Buttons from '../../Atoms/Buttons';
 import Loader from '../../Atoms/Loader';
 
 export interface UserInfo {
-  authId: string
-  courses?: string
-  createdAt: string
-  email: string
-  externalId?: string
-  firstName: string
-  grade?: string
-  id: string
-  image?: string
-  institution?: string
-  language: string
-  lastName: string
-  preferredName?: string
-  role: string
-  status: string
-  phone: string
-  updatedAt: string
-  birthdate?: string
+  authId: string;
+  courses?: string;
+  createdAt: string;
+  email: string;
+  externalId?: string;
+  firstName: string;
+  grade?: string;
+  id: string;
+  image?: string;
+  institution?: string;
+  language: string;
+  lastName: string;
+  preferredName?: string;
+  role: string;
+  status: string;
+  phone: string;
+  updatedAt: string;
+  birthdate?: string;
 }
 
 const Profile: React.FC = () => {
-
-  const [person, setPerson] = useState<UserInfo>(
-    {
-      id: '',
-      authId: '',
-      courses: '',
-      createdAt: '',
-      email: '',
-      externalId: '',
-      firstName: '',
-      grade: null,
-      image: null,
-      institution: null,
-      language: '',
-      lastName: '',
-      preferredName: null,
-      role: '',
-      status: '',
-      phone: '',
-      updatedAt: '',
-      birthdate: null,
-    }
-  );
+  const [person, setPerson] = useState<UserInfo>({
+    id: '',
+    authId: '',
+    courses: '',
+    createdAt: '',
+    email: '',
+    externalId: '',
+    firstName: '',
+    grade: null,
+    image: null,
+    institution: null,
+    language: '',
+    lastName: '',
+    preferredName: null,
+    role: '',
+    status: '',
+    phone: '',
+    updatedAt: '',
+    birthdate: null,
+  });
 
   const match = useRouteMatch();
   const history = useHistory();
-  const pathName = location.pathname.replace(/\/$/, "");
+  const pathName = location.pathname.replace(/\/$/, '');
   const currentPath = pathName.substring(pathName.lastIndexOf('/') + 1);
   const { state, theme, dispatch } = useContext(GlobalContext);
   const [status, setStatus] = useState('');
@@ -80,17 +77,17 @@ const Profile: React.FC = () => {
   const [showCropper, setShowCropper] = useState(false);
   const [imageLoading, setImageLoading] = useState(false);
   const [upImage, setUpImage] = useState(null);
-  const [imageUrl, setImageUrl] = useState('')
+  const [imageUrl, setImageUrl] = useState('');
   const breadCrumsList = [
     { title: 'Home', url: '/dashboard', last: false },
     { title: 'Profile', url: '/dashboard/profile', last: true },
-  ]
+  ];
 
   const initials = (firstName: string, lastName: string) => {
-    let firstInitial = firstName.charAt(0).toUpperCase()
-    let lastInitial = lastName.charAt(0).toUpperCase()
+    let firstInitial = firstName.charAt(0).toUpperCase();
+    let lastInitial = lastName.charAt(0).toUpperCase();
     return firstInitial + lastInitial;
-  }
+  };
 
   const stringToHslColor = (str: string) => {
     let hash = 0;
@@ -101,9 +98,9 @@ const Profile: React.FC = () => {
 
     let h = hash % 360;
     return 'hsl(' + h + ', 70%, 72%)';
-  }
+  };
 
-  // TODO: 
+  // TODO:
   // Set type for file instead of any
 
   const uploadImageToS3 = async (file: any, id: string, type: string) => {
@@ -113,54 +110,57 @@ const Profile: React.FC = () => {
       Storage.put(`profile_image_${id}`, file, {
         contentType: type,
         ContentEncoding: 'base64',
-      }).then(result => {
-        console.log('File successfully uploaded to s3', result)
-        resolve(true)
-      }).catch(err => {
-        console.log('Error in uploading file to s3', err)
-        reject(err)
       })
+        .then((result) => {
+          console.log('File successfully uploaded to s3', result);
+          resolve(true);
+        })
+        .catch((err) => {
+          console.log('Error in uploading file to s3', err);
+          reject(err);
+        });
     });
-  }
-
+  };
 
   const deletImageFromS3 = (key: string) => {
     // Remove image from bucket
 
     return new Promise((resolve, reject) => {
-      Storage.remove(key).then(result => {
-        console.log('File successfully deleted from s3')
-        resolve(result)
-      }).catch(err => {
-        console.log('Error in deleting file from s3', err)
-        reject(err)
-      })
+      Storage.remove(key)
+        .then((result) => {
+          console.log('File successfully deleted from s3');
+          resolve(result);
+        })
+        .catch((err) => {
+          console.log('Error in deleting file from s3', err);
+          reject(err);
+        });
     });
-  }
+  };
 
   const cropSelecetedImage = async (e: any) => {
     if (e.target.files && e.target.files.length > 0) {
-      const file = e.target.files[0]
+      const file = e.target.files[0];
       const fileReader = new FileReader();
       fileReader.onload = function () {
-        setUpImage(fileReader.result)
-      }
+        setUpImage(fileReader.result);
+      };
       fileReader.readAsDataURL(file);
-      toggleCropper()
+      toggleCropper();
     }
-  }
+  };
 
   const toggleCropper = () => {
-    setShowCropper(!showCropper)
-  }
+    setShowCropper(!showCropper);
+  };
 
   const saveCroppedImage = async (image: string) => {
     setImageLoading(true);
     toggleCropper();
-    await uploadImageToS3(image, person.id, 'image/jpeg')
-    const imageUrl: any = await getImageFromS3(`profile_image_${person.id}`)
+    await uploadImageToS3(image, person.id, 'image/jpeg');
+    const imageUrl: any = await getImageFromS3(`profile_image_${person.id}`);
     setImageUrl(imageUrl);
-    setPerson({ ...person, image: `profile_image_${person.id}` })
+    setPerson({ ...person, image: `profile_image_${person.id}` });
     updateImageParam(`profile_image_${person.id}`);
     toggleCropper();
     dispatch({
@@ -172,16 +172,15 @@ const Profile: React.FC = () => {
         language: state.user.language,
         onBoardSurvey: state.user.onBoardSurvey ? state.user.onBoardSurvey : false,
         role: state.user.role,
-        image: `profile_image_${person.id}`
-      }
-    })
+        image: `profile_image_${person.id}`,
+      },
+    });
     setImageLoading(false);
-  }
+  };
 
   async function updateImageParam(imageKey: string) {
-
-    // TODO: 
-    // Need to check for update only required input values. 
+    // TODO:
+    // Need to check for update only required input values.
 
     const input = {
       id: person.id,
@@ -196,24 +195,23 @@ const Profile: React.FC = () => {
       phone: person.phone,
       birthdate: person.birthdate,
       email: person.email,
-      firstName: person.firstName
-    }
+      firstName: person.firstName,
+    };
     try {
-      const update: any = await API.graphql(graphqlOperation(customMutations.updatePerson, { input: input }))
+      const update: any = await API.graphql(graphqlOperation(customMutations.updatePerson, { input: input }));
       setPerson({
         ...person,
-        ...update.data.updatePerson
-      })
+        ...update.data.updatePerson,
+      });
     } catch (error) {
-      console.error("Error updating image on graphql", error)
+      console.error('Error updating image on graphql', error);
     }
   }
 
   const deletUserProfile = async () => {
-    await deletImageFromS3(`profile_image_${person.id}`)
+    await deletImageFromS3(`profile_image_${person.id}`);
     await removeImageUrlFromDb();
-  }
-
+  };
 
   const removeImageUrlFromDb = async () => {
     const input = {
@@ -229,32 +227,34 @@ const Profile: React.FC = () => {
       phone: person.phone,
       birthdate: person.birthdate,
       email: person.email,
-      firstName: person.firstName
-    }
+      firstName: person.firstName,
+    };
     try {
-      const update: any = await API.graphql(graphqlOperation(customMutations.updatePerson, { input: input }))
+      const update: any = await API.graphql(graphqlOperation(customMutations.updatePerson, { input: input }));
       setPerson({
         ...person,
-        ...update.data.updatePerson
-      })
+        ...update.data.updatePerson,
+      });
     } catch (error) {
-      console.error("Error Deleting image on graphql", error)
+      console.error('Error Deleting image on graphql', error);
     }
-  }
+  };
 
   async function getUser() {
     try {
-      const user: any = await API.graphql(graphqlOperation(queries.getPerson, { email: state.user.email, authId: state.user.authId }))
+      const user: any = await API.graphql(
+        graphqlOperation(queries.getPerson, { email: state.user.email, authId: state.user.authId })
+      );
       setPerson(user.data.getPerson);
       setStatus('done');
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
   useEffect(() => {
-    getUser()
-  }, [])
+    getUser();
+  }, []);
 
   useEffect(() => {
     async function getUrl() {
@@ -262,10 +262,10 @@ const Profile: React.FC = () => {
       setImageUrl(imageUrl);
     }
     getUrl();
-  }, [person.image])
+  }, [person.image]);
 
   if (status !== 'done') {
-    return (<LessonLoading />)
+    return <LessonLoading />;
   }
   {
     return (
@@ -278,52 +278,67 @@ const Profile: React.FC = () => {
               <Buttons btnClass="mr-4" onClick={history.goBack} Icon={IoArrowUndoCircleOutline} />
               {currentPath !== 'edit' ? (
                 <Buttons btnClass="mr-4" onClick={() => history.push(`${match.url}/edit`)} Icon={FaEdit} />
-              ) : null
-              }
+              ) : null}
             </div>
           </div>
           <div className={`w-full white_back p-8 mb-8 ${theme.elem.bg} ${theme.elem.text} ${theme.elem.shadow}`}>
             <div className="h-9/10 flex flex-col md:flex-row">
               <div className="w-auto p-4 flex flex-col text-center items-center">
-                <div className='relative' >
-                  {person.image ?
-                    (
-                      <button className="group hover:opacity-80 focus:outline-none focus:opacity-95">
-                        {!imageLoading ? <img
+                <div className="relative">
+                  {person.image ? (
+                    <button className="group hover:opacity-80 focus:outline-none focus:opacity-95">
+                      {!imageLoading ? (
+                        <img
                           className={`profile w-20 h-20 md:w-40 md:h-40 rounded-full border border-gray-400 shadow-elem-light`}
                           src={imageUrl}
-                        /> :
-                          <div className="w-20 h-20 md:w-40 md:h-40 p-2 md:p-4 flex justify-center items-center rounded-full border border-gray-400 shadow-elem-lightI">
-                            <Loader />
-                          </div>
-                        }
-                        <span className="hidden group-focus:flex justify-around mt-6">
-                          <label className="w-8 cursor-pointer">
-                            <IconContext.Provider value={{ size: '1.6rem', color: '#667eea' }}>
-                              <FaEdit />
-                            </IconContext.Provider>
-                            <input type="file" className="hidden" onChange={(e) => cropSelecetedImage(e)} accept="image/*" multiple={false} />
-                          </label>
-                          <span className="w-8 cursor-pointer" onClick={deletUserProfile}>
-                            <IconContext.Provider value={{ size: '1.6rem', color: '#fa0000' }}>
-                              <FaTrashAlt />
-                            </IconContext.Provider>
-                          </span>
+                        />
+                      ) : (
+                        <div className="w-20 h-20 md:w-40 md:h-40 p-2 md:p-4 flex justify-center items-center rounded-full border border-gray-400 shadow-elem-lightI">
+                          <Loader />
+                        </div>
+                      )}
+                      <span className="hidden group-focus:flex justify-around mt-6">
+                        <label className="w-8 cursor-pointer">
+                          <IconContext.Provider value={{ size: '1.6rem', color: '#667eea' }}>
+                            <FaEdit />
+                          </IconContext.Provider>
+                          <input
+                            type="file"
+                            className="hidden"
+                            onChange={(e) => cropSelecetedImage(e)}
+                            accept="image/*"
+                            multiple={false}
+                          />
+                        </label>
+                        <span className="w-8 cursor-pointer" onClick={deletUserProfile}>
+                          <IconContext.Provider value={{ size: '1.6rem', color: '#fa0000' }}>
+                            <FaTrashAlt />
+                          </IconContext.Provider>
                         </span>
-                      </button>) :
-                    (
-                      <label className={`w-20 h-20 md:w-40 md:h-40 p-2 md:p-4 flex justify-center items-center rounded-full border border-gray-400 shadow-elem-light`}>
-                        {!imageLoading ? <IconContext.Provider value={{ size: '3rem', color: '#4a5568' }}>
+                      </span>
+                    </button>
+                  ) : (
+                    <label
+                      className={`w-20 h-20 md:w-40 md:h-40 p-2 md:p-4 flex justify-center items-center rounded-full border border-gray-400 shadow-elem-light`}>
+                      {!imageLoading ? (
+                        <IconContext.Provider value={{ size: '3rem', color: '#4a5568' }}>
                           <FaPlus />
-                        </IconContext.Provider> : <Loader />}
-                        <input type="file" className="hidden" onChange={(e) => cropSelecetedImage(e)} accept="image/*" multiple={false} />
-                      </label>
-
-                    )
-                  }
+                        </IconContext.Provider>
+                      ) : (
+                        <Loader />
+                      )}
+                      <input
+                        type="file"
+                        className="hidden"
+                        onChange={(e) => cropSelecetedImage(e)}
+                        accept="image/*"
+                        multiple={false}
+                      />
+                    </label>
+                  )}
                   <span className="absolute top-7 left-8 w-8 h-8">
                     <NavLink to={`${match.url}/password`}>
-                      <IconContext.Provider value={{ size: '2rem', color: '#000000' }}>
+                      <IconContext.Provider value={{ size: '2rem', color: '#B22222' }}>
                         <RiLock2Fill />
                       </IconContext.Provider>
                     </NavLink>
@@ -337,7 +352,6 @@ const Profile: React.FC = () => {
               </div>
 
               <div className="relative w-full">
-
                 {/* TODO : Need to convert this into tabs instead of buttons. 
                     Currently we have only single tab so hiding this.
                 */}
@@ -387,58 +401,29 @@ const Profile: React.FC = () => {
                 </div> */}
 
                 <Switch>
-                  <Route
-                    exact
-                    path={`${match.url}/`}
-                    render={() => (
-                      <ProfileInfo
-                        user={person}
-                        status={status}
-                      />
-                    )}
-                  />
-                  <Route
-                    path={`${match.url}/about`}
-                    render={() => (
-                      <AboutMe />
-                    )}
-                  />
+                  <Route exact path={`${match.url}/`} render={() => <ProfileInfo user={person} status={status} />} />
+                  <Route path={`${match.url}/about`} render={() => <AboutMe />} />
                   <Route
                     path={`${match.url}/edit`}
-                    render={() => (
-                      <ProfileEdit
-                        user={person}
-                        status={status}
-                        setStatus={setStatus}
-                        getUser={getUser}
-                      />
-                    )}
+                    render={() => <ProfileEdit user={person} status={status} setStatus={setStatus} getUser={getUser} />}
                   />
-                  <Route
-                    path={`${match.url}/vault`}
-                    render={() => (
-                      <ProfileVault />
-                    )}
-                  />
-                  <Route
-                    path={`${match.url}/password`}
-                    render={() => (
-                      <ChangePassword />
-                    )}
-                  />
+                  <Route path={`${match.url}/vault`} render={() => <ProfileVault />} />
+                  <Route path={`${match.url}/password`} render={() => <ChangePassword />} />
                 </Switch>
                 {showCropper && (
-                  <ProfileCropModal upImg={upImage} saveCroppedImage={(img: string) => saveCroppedImage(img)} closeAction={toggleCropper} />
+                  <ProfileCropModal
+                    upImg={upImage}
+                    saveCroppedImage={(img: string) => saveCroppedImage(img)}
+                    closeAction={toggleCropper}
+                  />
                 )}
               </div>
             </div>
-
           </div>
         </div>
       </div>
-    )
+    );
   }
-}
-
+};
 
 export default Profile;
