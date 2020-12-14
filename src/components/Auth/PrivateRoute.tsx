8ab3ab92b987d5a83f6c548f96ab9a96991ea6cf
sublimedatasys <sahilshah@sublimedatasys.com>
2020-12-14 09:45:57 +0530

    Friday work commit: Fixed formatting and route imports.

--- src/components/Auth/PrivateRoute.tsx
+++ src/components/Auth/PrivateRoute.tsx
@@ -1,34 +1,34 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../../contexts/GlobalContext';
 import {
-    Route,
-    Redirect,
-  } from "react-router-dom";
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 interface PrivateRouteProps {
-    path: string
-    children: React.ReactNode
+  path: string
+  children: React.ReactNode
 }
 
-const PrivateRoute = ({children, path}: PrivateRouteProps) => {
-    const { state } = useContext(GlobalContext);
-    return (
-        <Route 
-        path={path}
-        render={({ location }) =>
-            state.isAuthenticated ? (
-                children
-            ) : (
-                <Redirect
-                    to={{
-                    pathname: "/login",
-                    state: { from: location }
-                    }}
-                 />
-            )
-        }
-        />
-    )
+const PrivateRoute = ({ children, path }: PrivateRouteProps) => {
+  const { state } = useContext(GlobalContext);
+  return (
+    <Route
+      path={path}
+      render={({ location }) =>
+        state.isAuthenticated ? (
+          children
+        ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+      }
+    />
+  )
 }
 
 export default PrivateRoute;
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -3,10 +3,10 @@ import React, { useContext, useEffect, useState, Suspense, lazy } from 'react';
 import API, { graphqlOperation } from '@aws-amplify/api';
 import { GlobalContext } from '../../contexts/GlobalContext';
 import {
-    useRouteMatch,
-    Switch,
-    Route,
-    Redirect
+  useRouteMatch,
+  Switch,
+  Route,
+  Redirect
 } from 'react-router-dom';
 // import PageHeaderBar from '../Header/PageHeaderBar';
 import SideMenu from './Menu/SideMenu';
@@ -27,130 +27,123 @@ import ComponentLoading from '../Lesson/Loading/ComponentLoading';
 
 
 type userObject = {
-    [key: string]: any,
+  [key: string]: any,
 }
 
 const Dashboard: React.FC = () => {
-    const match = useRouteMatch();
-    const [cookies, setCookie] = useCookies(['auth']);
-    const { state, dispatch } = useContext(GlobalContext);
-    const [currentPage, setCurrentPage] = useState<string>('');
+  const match = useRouteMatch();
+  const [cookies, setCookie] = useCookies(['auth']);
+  const { state, dispatch } = useContext(GlobalContext);
+  const [currentPage, setCurrentPage] = useState<string>('');
 
-    const setUser = (user: userObject) => {
-        let firstName = user.preferredName ? user.preferredName : user.firstName
-        dispatch({
-            type: 'SET_USER',
-            payload: {
-                id: user.id,
-                firstName: firstName,
-                lastName: user.lastName,
-                language: user.language,
-                onBoardSurvey: user.onBoardSurvey ? user.onBoardSurvey : false,
-                role: user.role,
-                image:user.image
-            }
-        })
+  const setUser = (user: userObject) => {
+    let firstName = user.preferredName ? user.preferredName : user.firstName
+    dispatch({
+      type: 'SET_USER',
+      payload: {
+        id: user.id,
+        firstName: firstName,
+        lastName: user.lastName,
+        language: user.language,
+        onBoardSurvey: user.onBoardSurvey ? user.onBoardSurvey : false,
+        role: user.role,
+        image: user.image
+      }
+    })
 
-        setCookie('auth', { ...cookies.auth, role: user.role, firstName: firstName, id: user.id })
+    setCookie('auth', { ...cookies.auth, role: user.role, firstName: firstName, id: user.id })
+  }
+
+  async function getUser() {
+    try {
+      // this any needs to be changed once a solution is found!!!
+      const user: any = await API.graphql(graphqlOperation(queries.getPerson, { email: state.user.email, authId: state.user.authId }))
+      // console.log(user)
+      setUser(user.data.getPerson);
+    } catch (error) {
+      console.error(error)
     }
+  }
 
-    async function getUser() {
-        try {
-            // this any needs to be changed once a solution is found!!!
-            const user: any = await API.graphql(graphqlOperation(queries.getPerson, { email: state.user.email, authId: state.user.authId }))
-            // console.log(user)
-            setUser(user.data.getPerson);
-        } catch (error) {
-            console.error(error)
-        }
+  if (state.user.role === 'FLW') {
+
+  }
+
+  useEffect(() => {
+    if (!state.user.firstName) {
+      getUser()
     }
+  }, [])
 
-    if (state.user.role === 'FLW') {
-
-    }
-
-    useEffect(() => {
-        if (!state.user.firstName) {
-            getUser()
-        }
-    }, [])
-
-    return (
-        <div className={`w-screen md:w-full h-screen md:h-full flex`}>
-            <SideMenu setCurrentPage={setCurrentPage} currentPage={currentPage}>
-                <ProfileLink setCurrentPage={setCurrentPage} currentPage={currentPage} />
-                <Links setCurrentPage={setCurrentPage} currentPage={currentPage} />
-            </SideMenu>
-                <PageHeaderBar setCurrentPage={setCurrentPage} currentPage={currentPage} />
-            <div className={`height h-full overflow-x-hidden overflow-y-scroll flex flex-col`}>
-                <Suspense fallback={
-                    <div className="min-h-screen w-full flex flex-col justify-center items-center">
-                        {/* <div className="min-h-full w-full flex flex-col justify-center items-center">
+  return (
+    <div className={`w-screen md:w-full h-screen md:h-full flex`}>
+      <SideMenu setCurrentPage={setCurrentPage} currentPage={currentPage}>
+        <ProfileLink setCurrentPage={setCurrentPage} currentPage={currentPage} />
+        <Links setCurrentPage={setCurrentPage} currentPage={currentPage} />
+      </SideMenu>
+      <PageHeaderBar setCurrentPage={setCurrentPage} currentPage={currentPage} />
+      <div className={`height h-full overflow-x-hidden overflow-y-scroll flex flex-col`}>
+        <Suspense fallback={
+          <div className="min-h-screen w-full flex flex-col justify-center items-center">
+            {/* <div className="min-h-full w-full flex flex-col justify-center items-center">
                         Give us one second! It is loading... 
                     </div> */}
-                        <ComponentLoading />
-                    </div>
-                }>
-                    <Switch>
-                        <Route
-                            exact
-                            path={`${match.url}`}
-                            render={() => (
-                                <Classroom />
-                            )}
-                        />
-                        {/* <Route
-                            exact
-                            path={`${match.url}`}
-                            render={() => (
-                                <LessonPlanHome />
-                            )}
-                        /> */}
-                        <Route
-                            path={`${match.url}/classroom`}
-                            render={({ location }) => (
-                                <Redirect
-                                    to={{
-                                        pathname: '/',
-                                        state: { from: location }
-                                    }} />
-                            )}
-                        />
-                        <Route
-                            path={`${match.url}/manage-users`}
-                            render={() => (
-                                <UserManagement />
-                            )}
-                        />
-                        <Route
-                            path={`${match.url}/registration`}
-                            render={() => (
-                                <Registration />
-                            )}
-                        />
-                        <Route
-                            path={`${match.url}/profile`}
-                            render={() => (
-                                <Profile />
-                            )}
-                        />
-                        <Route
-                            path={`${match.url}/lesson-planner`}
-                            render={() => (
-                                <LessonPlanHome />
-                            )}
-                        />
-                        <Route
-                            path={`${match.url}/manage-institutions`}
-                            render={() => (
-                                <InstitutionsHome />
-                            )}
-                        />
-                    </Switch>
-                </Suspense>
-            </div>
-        </div>
-    )
+            <ComponentLoading />
+          </div>
+        }>
+          <Switch>
+            <Route
+              exact
+              path={`${match.url}`}
+              render={() => (
+                <Classroom />
+              )}
+            />
+            <Route
+              path={`${match.url}/classroom`}
+              render={({ location }) => (
+                <Redirect
+                  to={{
+                    pathname: '/',
+                    state: { from: location }
+                  }} />
+              )}
+            />
+            <Route
+              path={`${match.url}/manage-users`}
+              render={() => (
+                <UserManagement />
+              )}
+            />
+            <Route
+              path={`${match.url}/registration`}
+              render={() => (
+                <Registration />
+              )}
+            />
+            <Route
+              path={`${match.url}/profile`}
+              render={() => (
+                <Profile />
+              )}
+            />
+            <Route
+              path={`${match.url}/lesson-planner`}
+              render={() => (
+                <LessonPlanHome />
+              )}
+            />
+            <Route
+              path={`${match.url}/manage-institutions`}
+              render={() => (
+                <InstitutionsHome />
+              )}
+            />
+          </Switch>
+        </Suspense>
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard;
