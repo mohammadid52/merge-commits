be83a022fd32da97880a19922ebee447a83a9c54
samboosa5k <jasper.verbon@gmail.com>
2020-12-15 14:18:27 +0100

    two truths and a lie basic styuling for teacher and student

--- src/components/Lesson/LessonComponents/ListPage/ListBreakdown/CoopDisplay.tsx
+++ src/components/Lesson/LessonComponents/ListPage/ListBreakdown/CoopDisplay.tsx
@@ -5,116 +5,145 @@ import Modules from './Modules';
 import Banner from './Banner';
 import ListOutput from './ListOutput';
 
-import { IconContext } from "react-icons/lib/esm/iconContext";
+import { IconContext } from 'react-icons/lib/esm/iconContext';
 import { FaExpand, FaCompress } from 'react-icons/fa';
 
 interface teacherData {
-    breakdownComponent: string,
-    studentInfo?: {
-        id: string
-        firstName: string
-        preferredName?: string
-        lastName: string
-    }
-    doFirstData?: { [key: string]: any }
-    warmUpData?: { [key: string]: any }
-    corelessonData?: { [key: string]: any }
-    activityData?: { [key: string]: any }
+  breakdownComponent: string;
+  studentInfo?: {
+    id: string;
+    firstName: string;
+    preferredName?: string;
+    lastName: string;
+  };
+  doFirstData?: { [key: string]: any };
+  warmUpData?: { [key: string]: any };
+  corelessonData?: { [key: string]: any };
+  activityData?: { [key: string]: any };
 }
 
 const CoopDisplay = () => {
-    const { state, theme, dispatch } = useContext(LessonContext);
-    const displayProps = state.componentState.story;
-    const [fullscreen, setFullscreen] = useState(false);
+  const { state, theme, dispatch } = useContext(LessonContext);
+  const displayProps = state.componentState.story;
+  const [fullscreen, setFullscreen] = useState(false);
 
-    const [teacherData, setTeacherData] = useState<teacherData>();
-    const student = state.displayData.studentInfo;
+  const [teacherData, setTeacherData] = useState<teacherData>();
+  const student = state.displayData.studentInfo;
 
-    const handleFullscreen = () => {
-        setFullscreen(fullscreen => {
-            return !fullscreen
-        });
+  const handleFullscreen = () => {
+    setFullscreen((fullscreen) => {
+      return !fullscreen;
+    });
+  };
+
+  const firstInitialFunc = (str: string) => {
+    if (typeof str !== 'string' || str === '') {
+      return 'Profile';
     }
+    let firstInitial = str.charAt(0);
+    firstInitial = firstInitial.toUpperCase() + '.';
+    return firstInitial;
+  };
 
-    const firstInitialFunc = (str: string) => {
-        if (typeof str !== 'string' || str === '') { return 'Profile' }
-        let firstInitial = str.charAt(0)
-        firstInitial = firstInitial.toUpperCase() + '.';
-        return firstInitial;
+  useEffect(() => {
+    dispatch({ type: 'ACTIVATE_LESSON', payload: 'warmup/breakdown' });
+  }, []);
+
+  useEffect(() => {
+    if (
+      state.displayData &&
+      state.displayData.breakdownComponent &&
+      state.displayData.breakdownComponent === 'warmup/breakdown'
+    ) {
+      // console.log( 'got it', state.displayData );
+      setTeacherData(state.displayData);
     }
+  }, [state.displayData]);
 
-    useEffect(() => {
-        dispatch({ type: 'ACTIVATE_LESSON', payload: 'warmup/breakdown' })
-    }, [])
+  // {console.log(state.displayData.breakdownComponent, 'state and find name')}
 
-    useEffect(() => {
-        if (state.displayData && state.displayData.breakdownComponent && state.displayData.breakdownComponent === 'warmup/breakdown') {
-            // console.log( 'got it', state.displayData );
-            setTeacherData(state.displayData)
-        }
-    }, [state.displayData])
+  return (
+    <div className={theme.section}>
+      <ReflectionQuestions questions={state.data.lesson.warmUp.breakdown.reflectionQuestions} />
 
-    // {console.log(state.displayData.breakdownComponent, 'state and find name')}
+      {/* teacher display */}
+      <div
+        className={`relative ${
+          fullscreen ? 'w-full' : ''
+        } h-full flex flex-col justify-between items-center rounded-lg border p-2 my-4 bg-white bg-opacity-20`}>
+        <div className="absolute top-2 right-0 cursor-pointer w-full text-xl m-2" onClick={handleFullscreen}>
+          <IconContext.Provider
+            value={{
+              color: '#E2E8F0',
+              size: '2rem',
+              style: { width: 'auto', right: '0', bottom: '0', position: 'absolute' },
+            }}>
+            {fullscreen ? <FaCompress /> : <FaExpand />}
+          </IconContext.Provider>
+        </div>
 
-    return (
-        <div className={theme.section}>
+        <div className=" w-full z-50 top-0 ">
+          <div className="w-2.5/10 h-auto w-auto py-1 text-gray-800 font-light flex flex-col justify-center items-center z-50">
+            <p>
+              <span className="text-white">
+                by:{' '}
+                <span className="font-medium text-yellow-600">
+                  {student ? (student.preferredName ? student.preferredName : student.firstName) : null}{' '}
+                  {student ? firstInitialFunc(student.lastName) : null}
+                </span>
+              </span>
+            </p>
+          </div>
+        </div>
 
-            <ReflectionQuestions questions={state.data.lesson.warmUp.breakdown.reflectionQuestions}  />
+        <Banner
+          title={
+            teacherData && teacherData.warmUpData && teacherData.warmUpData.title
+              ? teacherData.warmUpData.title
+              : 'Classmates List'
+          }
+          fullscreen={fullscreen}
+          display="COOP"
+        />
 
-            {/* teacher display */}
-            <div className={`relative ${fullscreen ? 'w-full' : ''} h-full flex flex-col justify-between items-center rounded-lg border p-2 my-4 bg-white bg-opacity-20`}>
-
-                <div className="absolute top-2 right-0 cursor-pointer w-full text-xl m-2" onClick={handleFullscreen}>
-                    <IconContext.Provider value={{ color: '#E2E8F0', size: '2rem', style: { width: 'auto', right: '0', bottom: '0', position: 'absolute' } }}>
-                        {fullscreen ? < FaCompress /> : < FaExpand />}
-                    </IconContext.Provider>
-                </div>
-
-                <div className=" w-full z-50 top-0 ">
-                    <div className="w-2.5/10 h-auto w-auto py-1 text-gray-800 font-light flex flex-col justify-center items-center z-50">
-                        <p><span className='text-white'>by: <span className='font-medium text-yellow-600'>{student ? student.preferredName ? student.preferredName : student.firstName : null} {student ? firstInitialFunc(student.lastName) : null}</span></span></p>
-                    </div>
-                </div>
-
-                <Banner title={teacherData && teacherData.warmUpData && teacherData.warmUpData.title ? teacherData.warmUpData.title : 'Classmates List'}
-                    fullscreen={fullscreen}
-                    display='COOP' />
-
-                <div className="w-full h-8.8/10 flex md:flex-col justify-between">
-                    <ListOutput list={teacherData && teacherData.warmUpData && teacherData.warmUpData.story} />
-                    {/* <Modules
+        <div className="w-full h-8.8/10 flex md:flex-col justify-between">
+          <ListOutput list={teacherData && teacherData.warmUpData && teacherData.warmUpData.story} />
+          {/* <Modules
                         additional={teacherData && teacherData.warmUpData && teacherData.warmUpData.additional ? teacherData.warmUpData.additional : null}
                         displayMode="COOP" /> */}
-                </div>
+        </div>
+      </div>
 
-            </div>
+      {/* self display */}
+      <div
+        className={`${
+          fullscreen ? 'hidden' : 'w-full'
+        } h-full flex flex-col justify-between items-center p-4 bg-darker-gray rounded-xl`}>
+        <div className=" w-full z-50 top-0 ">
+          <div className="w-2.5/10 h-auto w-auto py-1 text-gray-800 font-light flex flex-col justify-center items-center z-50">
+            <p>
+              <span className="text-white">
+                by: <span className="font-medium text-blueberry">You</span>
+              </span>
+            </p>
+          </div>
+        </div>
 
+        <Banner
+          title={displayProps && displayProps.title ? displayProps.title : 'Your List'}
+          display="SELFinCOOP"
+          fullscreen={fullscreen}
+        />
 
-
-            {/* self display */}
-            <div className={`${fullscreen ? 'hidden' : 'w-full'} h-full flex flex-col justify-between items-center p-4 bg-darker-gray rounded-xl`}>
-                
-                <div className=" w-full z-50 top-0 ">
-                    <div className="w-2.5/10 h-auto w-auto py-1 text-gray-800 font-light flex flex-col justify-center items-center z-50">
-                        <p><span className='text-white'>by: <span className='font-medium text-blueberry'>You</span></span></p>
-                    </div>
-                </div>
-
-                <Banner title={displayProps && displayProps.title ? displayProps.title : 'Your List'}
-                    display='SELFinCOOP' fullscreen={fullscreen} />
-
-
-                <div className="w-full h-8.8/10 flex flex-col justify-between">
-                    <ListOutput list={displayProps.story} />
-                    {/* <Modules
+        <div className="w-full h-8.8/10 flex flex-col justify-between">
+          <ListOutput list={displayProps.story} />
+          {/* <Modules
                         additional={displayProps.additional}
                         displayMode="SELFinCOOP" /> */}
-                </div>
-            </div>
-
         </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default CoopDisplay;
--- src/components/Lesson/LessonComponents/TruthGamePage/TruthGameBreakdown/Banner.tsx
+++ src/components/Lesson/LessonComponents/TruthGamePage/TruthGameBreakdown/Banner.tsx
@@ -16,43 +16,31 @@ const Banner = (props: BannerProps) => {
   return (
     <>
       {display === 'SELF' ? (
-        <div
-          className={`w-full h-1/10 ${theme.banner} flex flex-row justify-center items-center`}>
-          <IconContext.Provider value={{ color: '#EDF2F7', size: '2.75rem' }}>
-            <div className='bg-dark-red h-16 w-16 flex flex-col items-center rounded-lg justify-center z-20 shadow-2'>
+        <div className={`w-full h-1/10 text-4xl ${theme.banner}`}>
+          <IconContext.Provider value={{ color: '#EDF2F7', size: '1.5rem' }}>
+            <div className="w-auto h-auto mr-2">
               <FaGamepad />
             </div>
           </IconContext.Provider>
-          <div
-            className={`h-full text-xl text-gray-200 md:text-5xl z-10 font-medium flex justify-center items-center`}>
-            {title}
-          </div>
+          <div className={``}>{title !== null ? title : 'No title...'}</div>
         </div>
       ) : display === 'COOP' ? (
-        <div className='w-full h-full flex flex-row justify-center items-center'>
-          <IconContext.Provider value={{ color: '#EDF2F7', size: '2rem' }}>
-            <div className='bg-dark-red h-12 w-12 flex flex-col items-center justify-center z-20 rounded-lg shadow-2'>
+        <div className={`w-full h-1/10 text-4xl ${theme.banner}`}>
+          <IconContext.Provider value={{ color: '#EDF2F7', size: '1.5rem' }}>
+            <div className="w-auto h-auto mr-2">
               <FaGamepad />
             </div>
           </IconContext.Provider>
-          <div
-            className={`${
-              fullscreen ? 'text-4xl' : 'text-3xl'
-            } text-gray-200 h-full bg-dark-blue w-full flex flex-row font-medium justify-center items-center text-center rounded-lg z-10`}>
-            {title}
-          </div>
+          <div className={``}>{title !== null ? title : 'No title...'}</div>
         </div>
       ) : (
-        <div
-          className={`w-full h-1/10 ${theme.banner} flex flex-row justify-center items-center`}>
-          <IconContext.Provider value={{ color: '#EDF2F7', size: '2rem' }}>
-            <div className='bg-dark-red h-12 w-12 flex flex-col items-center  justify-center z-20 rounded-lg shadow-2'>
+        <div className={`w-full h-1/10 text-4xl ${theme.banner}`}>
+          <IconContext.Provider value={{ color: '#EDF2F7', size: '1.5rem' }}>
+            <div className="w-auto h-auto mr-2">
               <FaGamepad />
             </div>
           </IconContext.Provider>
-          <div className={`text-3xl h-full text-gray-200 z-10 font-medium flex justify-center items-center`}>
-            {title}
-          </div>
+          <div className={``}>{title !== null ? title : 'No title...'}</div>
         </div>
       )}
     </>
