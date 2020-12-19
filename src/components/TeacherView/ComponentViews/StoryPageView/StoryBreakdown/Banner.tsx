14cb867a649959a8df17124d7819ea18f255bccf
samboosa5k <jasper.verbon@gmail.com>
2020-12-19 10:07:38 +0100

    fixed type string -> string[] typescript error

--- src/components/TeacherView/ComponentViews/StoryPageView/StoryBreakdown/Banner.tsx
+++ src/components/TeacherView/ComponentViews/StoryPageView/StoryBreakdown/Banner.tsx
@@ -1,38 +1,37 @@
-import React, {useContext} from 'react';
-import { IconContext } from "react-icons/lib/esm/iconContext";
+import React, { useContext } from 'react';
+import { IconContext } from 'react-icons/lib/esm/iconContext';
 import { FaScroll } from 'react-icons/fa';
 import { LessonControlContext } from '../../../../../contexts/LessonControlContext';
 
 interface BannerProps {
-    dataProps?: {
-        title?: string,
-        story?: string,
-        additional?: [{
-            name: string,
-            input: string,
-        }]
-    }
-    fullscreen: boolean
+  dataProps?: {
+    title?: string;
+    story?: string[];
+    additional?: [
+      {
+        name: string;
+        input: string;
+      }
+    ];
+  };
+  fullscreen: boolean;
 }
 
 const Banner = (props: BannerProps) => {
-    const { dataProps, fullscreen } = props;
-    const { state, theme } = useContext(LessonControlContext);
+  const { dataProps, fullscreen } = props;
+  const { state, theme } = useContext(LessonControlContext);
 
-    return (
-        <div className={`w-full h-1/10 text-2xl ${theme.banner}`}>
-          <IconContext.Provider value={{ color: '#EDF2F7', size: '1.5rem' }}>
-            <div className='w-auto h-auto mr-2'>
-              <FaScroll />
-            </div>
-          </IconContext.Provider>
-
-          <div
-            className={``}>
-            { dataProps && dataProps.title ? dataProps.title : '' }
-          </div>
+  return (
+    <div className={`w-full h-1/10 text-2xl ${theme.banner}`}>
+      <IconContext.Provider value={{ color: '#EDF2F7', size: '1.5rem' }}>
+        <div className="w-auto h-auto mr-2">
+          <FaScroll />
         </div>
-    )
-}
+      </IconContext.Provider>
+
+      <div className={``}>{dataProps && dataProps.title ? dataProps.title : ''}</div>
+    </div>
+  );
+};
 
 export default Banner;
