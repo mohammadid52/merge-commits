bb333374d6207242220d4ef12eead51aebba9f63
samboosa5k <jasper.verbon@gmail.com>
2020-12-22 16:28:26 +0100

    Multi list now displays properly on breakdown pages

--- src/components/Lesson/LessonComponents/ListPage/ListBreakdown/ListOutput.tsx
+++ src/components/Lesson/LessonComponents/ListPage/ListBreakdown/ListOutput.tsx
@@ -16,13 +16,18 @@ const ListOutput = (props: ListOutputProps) => {
 
   return (
     <>
-      <div className={`w-full flex flex-col ${theme.blockQuote}`}>
+      <div className={`w-full flex flex-col`}>
         {props.list !== [''] ? (
-          <div
-            className={`text-gray-200 mb-2 align-middle leading-7`}
-            dangerouslySetInnerHTML={{ __html: parseBlankLines(props.list[0]) }}></div>
+          props.list.map((item: string, i: number) => (
+            <div
+              key={`listoutput_${i}`}
+              className={`text-gray-200 mb-2 align-middle leading-7 ${theme.blockQuote}`}
+              dangerouslySetInnerHTML={{ __html: parseBlankLines(item) }}></div>
+          ))
         ) : (
-          <div className={`${theme.elem.text} align-middle text-center`}>You didn't write a list :( </div>
+          <div className={`${theme.elem.text} ${theme.blockQuote} align-middle text-center`}>
+            You didn't write a list :({' '}
+          </div>
         )}
       </div>
     </>
