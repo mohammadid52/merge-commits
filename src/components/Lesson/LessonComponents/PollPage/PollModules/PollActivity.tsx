30c2842233c0b249f572eb027ae24d074136043d
samboosa5k <jasper.verbon@gmail.com>
2020-12-24 09:52:08 +0100

    starting context setup for new poll component on student side

--- src/components/Lesson/LessonComponents/PollPage/PollModules/PollActivity.tsx
+++ src/components/Lesson/LessonComponents/PollPage/PollModules/PollActivity.tsx
@@ -9,175 +9,117 @@ import PollForm from './PollForm';
 import { string } from 'prop-types';
 
 export interface PollInput {
-    id: string,
-    question: string,
-    options: {
-        id: string,
-        option: string,
-        isChoice: boolean
-    }
+  id: string;
+  question: string;
+  option: {
+    id: string;
+    option: string;
+    isChoice: boolean;
+  };
 }
 
-export type PollInputState = Array<PollInput>
-
-export const tempPollInputs = [
-    {
-      id: 'classes-school',
-      question: 'Would you rather have classes on Saturdays or school during the summer',
-      options: [
-        {
-          id: 'classes',
-          option: 'classes on Saturdays',
-        },
-        {
-          id: 'school',
-          option: 'school during the summer',
-        }
-      ]
-    },
-    {
-      id: 'clean-or-sleep',
-      question: 'Would you rather clean the house everyday or sleep in the backyard?',
-      options: [
-        {
-          id: 'clean',
-          option: 'clean the house everyday',
-        },
-        {
-          id: 'sleep',
-          option: 'sleep in the backyard',
-        }
-      ]
-    },
-    {
-      id: 'food-taste',
-      question: 'Would you rather only eat your favorite food for the rest of your life or lose your sense of taste but can eat whatever you want?',
-      options: [
-        {
-          id: 'fav-food',
-          option: 'eat your favorite food for the rest of your life',
-          
-        },
-        {
-          id: 'lose-sense',
-          option: 'lose your sense of taste but can eat whatever you want',
-        }
-      ]
-    },
-  ]
+export type PollInputState = Array<PollInput>;
 
 const Poll = () => {
-    const { state, dispatch } = useContext(LessonContext);
-    const [ cookies, setCookie ] = useCookies([`lesson-${state.classroomID}`]);
-    const inputs = state.data.lesson.warmUp.inputs;
-    const video = state.data.lesson.warmUp.instructions.link
-    const [ openPopup, setOpenPopup ] = useState(false)
+  const { state, dispatch } = useContext(LessonContext);
+  const [cookies, setCookie] = useCookies([`lesson-${state.classroomID}`]);
+  const inputs = state.data.lesson.warmUp.inputs;
+  const video = state.data.lesson.warmUp.instructions.link;
+  const [openPopup, setOpenPopup] = useState(false);
 
-    useEffect(() => {
-        if ( !cookies[`lesson-${state.classroomID}`].poll && !state.componentState.poll ) {
-             
-            let tempObj : {
-               pollArray: Array<{id: string, question: string, options: any | []}>
-               pollOptions: Array<{id: string, question: string, answer: string} | []>
-               additional?: Array<{name: string, text: string | []}>
-           }
+  useEffect(() => {
+    if (!cookies[`lesson-${state.classroomID}`].poll && !state.componentState.poll) {
+      let tempObj: {
+        pollArray: Array<{ id: string; question: string; option: any | [] }>;
+        pollOptions: Array<{ id: string; question: string; answer: string } | []>;
+        additional?: Array<{ name: string; text: string | [] }>;
+      };
 
-           tempObj = {
-               pollArray: [],
-               pollOptions: []
-           };
-// need to change here (tempPollInputs) when database is updated
+      tempObj = {
+        pollArray: [],
+        pollOptions: [],
+      };
 
-           let tempArr = tempPollInputs.map((item: {id: string, question: string, options: any}, ) => {  
-            let storageObj = {
-                    id: item.id,
-                    question: item.question,
-                    options: item.options
-                }
-                return storageObj
-                
-           } ) 
+      let tempArr = inputs.pollInputs.map((item: { id: string; question: string; option: any }) => {
+        let storageObj = {
+          id: item.id,
+          question: item.question,
+          option: item.option,
+        };
+        return storageObj;
+      });
 
-           tempObj.pollArray = tempArr
+      tempObj.pollArray = tempArr;
 
-            let tempOptions:Array<{id: string, question: string, answer: string} | []>= [];
-                tempPollInputs.map((input: { id: string, question: string, options: any }) => {
-                    let newInput = {
-                        id: input.id,
-                        question: input.question,
-                        answer: '',
-                    }
+      let tempOptions: Array<{ id: string; question: string; answer: string } | []> = [];
+      inputs.pollInputs.map((input: { id: string; question: string; option: any }) => {
+        let newInput = {
+          id: input.id,
+          question: input.question,
+          answer: '',
+        };
 
-                    tempOptions.push(newInput);
-                })
+        tempOptions.push(newInput);
+      });
 
-                tempObj.pollOptions = tempOptions;
-           
-             
+      tempObj.pollOptions = tempOptions;
 
-            if (inputs.additionalInputs.length > 0) {
-                let additional:Array<{name: string, text: string | []}>= [];
-                inputs.additionalInputs.forEach((input: { name: string; }) => {
-                    let newInput = {
-                        name: input.name,
-                        text: '',
-                    }
+      if (inputs.additionalInputs.length > 0) {
+        let additional: Array<{ name: string; text: string | [] }> = [];
+        inputs.additionalInputs.forEach((input: { name: string }) => {
+          let newInput = {
+            name: input.name,
+            text: '',
+          };
 
-                    additional.push(newInput);
-                })
+          additional.push(newInput);
+        });
 
-                tempObj.additional = additional;
-            }
+        tempObj.additional = additional;
+      }
 
-            dispatch({
-                type: 'SET_INITIAL_COMPONENT_STATE',
-                payload: {
-                    name: 'poll',
-                    content: tempObj
-                }
-            })
+      //    log TempObj
+      console.log('PollActivity --> ', tempObj);
 
-            setCookie(`lesson-${state.classroomID}`, { ...cookies[`lesson-${state.classroomID}`], poll: tempObj })
-        }
-        
-        if ( cookies[`lesson-${state.classroomID}`].poll ) {
-            dispatch({
-                type: 'SET_INITIAL_COMPONENT_STATE',
-                payload: {
-                    name: 'poll',
-                    content: cookies[`lesson-${state.classroomID}`].poll
-                }
-            })
-        }
+      dispatch({
+        type: 'SET_INITIAL_COMPONENT_STATE',
+        payload: {
+          name: 'poll',
+          content: tempObj,
+        },
+      });
 
-    }, []);
+      setCookie(`lesson-${state.classroomID}`, { ...cookies[`lesson-${state.classroomID}`], poll: tempObj });
+    }
 
+    if (cookies[`lesson-${state.classroomID}`].poll) {
+      dispatch({
+        type: 'SET_INITIAL_COMPONENT_STATE',
+        payload: {
+          name: 'poll',
+          content: cookies[`lesson-${state.classroomID}`].poll,
+        },
+      });
+    }
+  }, []);
 
-    return (
-       <>
-            <InstructionsPopup video={video} open={openPopup} setOpen={setOpenPopup}/>
-            <div className="w-full h-full flex flex-col justify-between items-center">
-                <Banner />
-                <div className="w-full h-8.8/10 flex flex-col items-center md:flex-row md:justify-between">
-                    <div className="md:w-4/10 h-full flex flex-col justify-between items-center">
-                        <InstructionsBlock />
-                        { inputs.additionalInputs.length > 0 ?
-                            <Modules 
-                                // breakdownProps={breakdownProps}
-                                // setBreakdownProps={setBreakdownProps}
-                                inputs={inputs.additionalInputs}
-                            />
-                            :
-                            null
-                        }
-                    </div>
-                    <div className="md:w-5.9/10 h-full flex flex-col items-center">
-                        <PollForm />
-                    </div>
-                </div>
-            </div>
-       </>
-    )
-}
+  return (
+    <>
+      <InstructionsPopup video={video} open={openPopup} setOpen={setOpenPopup} />
+      <div className="w-full h-full flex flex-col justify-between items-center">
+        <Banner />
+        <div className="w-full h-8.8/10 flex flex-col items-center md:flex-row md:justify-between">
+          <div className="md:w-4/10 h-full flex flex-col justify-between items-center">
+            <InstructionsBlock />
+            {inputs.additionalInputs.length > 0 ? <Modules inputs={inputs.additionalInputs} /> : null}
+          </div>
+          <div className="md:w-5.9/10 h-full flex flex-col items-center">
+            <PollForm />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Poll;
--- src/components/Lesson/LessonComponents/PollPage/PollModules/PollForm.tsx
+++ src/components/Lesson/LessonComponents/PollPage/PollModules/PollForm.tsx
@@ -5,63 +5,16 @@ import ToolTip from '../../../../General/ToolTip/ToolTip';
 import { MdSettingsBackupRestore } from 'react-icons/md';
 
 export interface PollInput {
-  id: string,
-  quesiton: string,
+  id: string;
+  question: string;
   options: {
-      id: string,
-      option: string,
-      isChoice: boolean
-  }
+    id: string;
+    option: string;
+    isChoice: boolean;
+  };
 }
 
-export type PollInputState = Array<PollInput>
-
-
-const tempData = [
-  {
-    id: 'classes-school',
-    question: 'Would you rather have classes on Saturdays or school during the summer',
-    options: [
-      {
-        id: 'classes',
-        option: 'classes on Saturdays',
-      },
-      {
-        id: 'school',
-        option: 'school during the summer',
-      }
-    ]
-  },
-  {
-    id: 'clean-or-sleep',
-    question: 'Would you rather clean the house everyday or sleep in the backyard?',
-    options: [
-      {
-        id: 'clean',
-        option: 'clean the house everyday',
-      },
-      {
-        id: 'sleep',
-        option: 'sleep in the backyard',
-      }
-    ]
-  },
-  {
-    id: 'food-taste',
-    question: 'Would you rather only eat your favorite food for the rest of your life or lose your sense of taste but can eat whatever you want?',
-    options: [
-      {
-        id: 'fav-food',
-        option: 'eat your favorite food for the rest of your life',
-        
-      },
-      {
-        id: 'lose-sense',
-        option: 'lose your sense of taste but can eat whatever you want',
-      }
-    ]
-  },
-]
+export type PollInputState = Array<PollInput>;
 
 const PollForm = () => {
   const { state, theme, dispatch } = useContext(LessonContext);
@@ -78,36 +31,35 @@ const PollForm = () => {
   //       : [],
   // });
 
-  const [input, setInput] = useState(state.componentState.poll && state.componentState.poll.pollOptions
-    ? state.componentState.poll.pollOptions
-    : []);
+  const [input, setInput] = useState(
+    state.componentState.poll && state.componentState.poll.pollOptions ? state.componentState.poll.pollOptions : []
+  );
 
-    useEffect(() => {
-      if ( cookies[`lesson-${state.classroomID}`]?.poll ) {
-        setInput(() => {
-          return (cookies[`lesson-${state.classroomID}`].poll.pollOptions);
-        });
-      }
-    }, []);
+  useEffect(() => {
+    if (cookies[`lesson-${state.classroomID}`]?.poll) {
+      setInput(() => {
+        return cookies[`lesson-${state.classroomID}`].poll.pollOptions;
+      });
+    }
+  }, []);
 
+  useEffect(() => {
+    if (state.componentState.poll) {
+      dispatch({
+        type: 'UPDATE_COMPONENT_STATE',
+        payload: {
+          componentName: 'poll',
+          inputName: 'pollOptions',
+          content: input,
+        },
+      });
 
-    useEffect(() => {
-      if ( state.componentState.poll ) {
-  
-       dispatch({
-         type: 'UPDATE_COMPONENT_STATE',
-         payload: {
-             componentName: 'poll',
-             inputName: 'pollOptions',
-             content: input
-         }
-     }) 
-
-        setCookie(`lesson-${state.classroomID}`, { ...cookies[`lesson-${state.classroomID}`], poll: {...cookies[`lesson-${state.classroomID}`].poll } });
-      }
-
-    }, [input]);
-
+      setCookie(`lesson-${state.classroomID}`, {
+        ...cookies[`lesson-${state.classroomID}`],
+        poll: { ...cookies[`lesson-${state.classroomID}`].poll },
+      });
+    }
+  }, [input]);
 
   // const handleInputChange = (e: { target: { id: string; value: string } }) => {
   //   setInput({
@@ -116,11 +68,7 @@ const PollForm = () => {
   //   });
   // };
 
-
-/////// below are all temporary
-
-
-  
+  /////// below are all temporary
 
   const [data, setData] = useState<any>([]);
   // console.log(options, 'test')
@@ -144,96 +92,90 @@ const PollForm = () => {
 
   //             })
   //             return {
-                
+
   //               ...options,
-  //               isChoice: true 
-  //             }} 
+  //               isChoice: true
+  //             }}
   //           //   else {
   //           //     return {...options}
-  //           // } 
+  //           // }
   //         }
   //         )}
   //     }
-        
+
   //     else {
   //       return {...item}
-        
-  //     } 
-      
+
+  //     }
+
   //   }) })
-    
+
   // };
 
   const handleRadioSelect = (passedKey: any, passedId: string, passedAns: string) => {
-
     setInput(() => {
-      return input ? input.map((item: {id: string, question: string, answer: any}, key: any) => {
-
-      if(item.id === passedKey) {
-        console.log(item.id, 'item.id')
-        console.log(passedKey, 'item.id')
-        console.log(passedId, 'passedID')
-          return {
-            ...item,
-          // id: item.id,
-          // question: item.question,
-          answer: passedAns
-        }        
-      }
-        
-      else {
-        return {...item}
-        
-      } 
-      
-    }) : null }) 
-    
+      return input
+        ? input.map((item: { id: string; question: string; answer: any }, key: any) => {
+            if (item.id === passedKey) {
+              return {
+                ...item,
+                // id: item.id,
+                // question: item.question,
+                answer: passedAns,
+              };
+            } else {
+              return { ...item };
+            }
+          })
+        : null;
+    });
   };
 
-
-
-console.log(cookies[`lesson-${state.classroomID}`], 'cookies')
-  console.log(data, 'data')
-  console.log(input, 'input')
-
   return (
-    <div className='bg-gradient-to-tl from-dark-blue to-med-dark-blue w-full h-full px-4 md:px-8 py-4 flex flex-col text-dark-blue rounded-lg border-l-4 border-orange-600'>
-      <h3
-        className={`text-xl text-gray-200 font-open font-light ${theme.underline}`}>
-        Poll{' '}
-      </h3>
-      <div className='relative h-full flex flex-col items-center mb-5 mt-2'>
-        {tempData ? tempData.map((item: {id: string, question:string, options: any}, key: number) => {
-          return (
-          <div key={key} className="flex flex-col p-4 items-center justify-between">
-            
-            <div id={item ? item.id : null} className="flex flex-col items-center justify-start py-4 font-light text-gray-400">
-              <label id={item ? item.id : null} className="w-full font-light text-gray-400 text-base flex justify-between items-center m-2 px-2">
-                {item ? item.question : null}
-              </label>
-              <div className="flex">
-
-                {
-                item ? item.options.map((option: {id: string, option: string, isChoice: boolean}, optionKey: number) => {
-
-                  return (
-                    <label key={optionKey} id={option.id} className="flex items-center text-sm cursor-pointer h-8">
-                      {console.log(input, 'input in loop')}
-                      <button key={optionKey} id={option.id} name='choice' onClick={() => handleRadioSelect(item.id, option.id, option.option)} className={`${option.option ? 'text-xl' : ''} w-auto px-4`}> {option.isChoice ? '❌'  : '⚪️'}</button>
-                        {option.option}
+    <div className="bg-gradient-to-tl from-dark-blue to-med-dark-blue w-full h-full px-4 md:px-8 py-4 flex flex-col text-dark-blue rounded-lg border-l-4 border-orange-600">
+      <h3 className={`text-xl text-gray-200 font-open font-light ${theme.underline}`}>Poll </h3>
+      <div className="relative h-full flex flex-col items-center mb-5 mt-2">
+        {pollInputs
+          ? pollInputs.map((item: { id: string; question: string; option: any }, key: number) => {
+              return (
+                <div key={key} className="flex flex-col p-4 items-center justify-between">
+                  <div
+                    id={item ? item.id : null}
+                    className="flex flex-col items-center justify-start py-4 font-light text-gray-400">
+                    <label
+                      id={item ? item.id : null}
+                      className="w-full font-light text-gray-400 text-base flex justify-between items-center m-2 px-2">
+                      {item ? item.question : null}
                     </label>
-                  )
-
-                }) : null}
-
-                
-
-              </div>
-            </div>
-            
-        </div> 
-        )
-        }) : null }
+                    <div className="flex">
+                      {item
+                        ? item.option.map(
+                            (option: { id: string; option: string; isChoice: boolean }, optionKey: number) => {
+                              return (
+                                <label
+                                  key={optionKey}
+                                  id={option.id}
+                                  className="flex items-center text-sm cursor-pointer h-8">
+                                  <button
+                                    key={optionKey}
+                                    id={option.id}
+                                    name="choice"
+                                    onClick={() => handleRadioSelect(item.id, option.id, option.option)}
+                                    className={`${option.option ? 'text-xl' : ''} w-auto px-4`}>
+                                    {option.isChoice ? '❌' : '⚪️'}
+                                  </button>
+                                  {option.option}
+                                </label>
+                              );
+                            }
+                          )
+                        : null}
+                    </div>
+                  </div>
+                </div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
