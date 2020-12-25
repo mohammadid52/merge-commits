8fb6510b96a813dd954ec50f0df9c8e13dbd2af6
sublimedatasys <sahilshah@sublimedatasys.com>
2020-12-25 11:50:27 +0530

    Name initials small fix.

--- src/components/Dashboard/Admin/Institutons/InstitutionInfo.tsx
+++ src/components/Dashboard/Admin/Institutons/InstitutionInfo.tsx
@@ -3,10 +3,11 @@ import { NavLink, useRouteMatch, useHistory } from 'react-router-dom';
 import { IconContext } from 'react-icons/lib/esm/iconContext';
 import { FaGraduationCap, FaChalkboardTeacher, FaHotel, FaHandshake } from 'react-icons/fa';
 
-import { initials, stringToHslColor } from '../../../../utilities/strings';
+import { initials, stringToHslColor, formatPhoneNumber, getHostNameFromUrl, getInitialsFromString } from '../../../../utilities/strings';
 import UnderlinedTabs from '../../../Atoms/UnderlinedTabs';
 import { IoPeople } from 'react-icons/io5';
 import { getImageFromS3 } from '../../../../utilities/services';
+// import ClassBuilder from './Builders/ClassBuilder';
 
 interface InstitutionInfoProps {
   institute?: InstInfo;
@@ -35,8 +36,8 @@ const InstitutionInfo = (instProps: InstitutionInfoProps) => {
   const tabs = [
     { index: 0, title: 'Service Providers', icon: <FaHandshake />, active: false, content: <p className="p-16 text-center">No data</p> },
     { index: 1, title: 'Staff', icon: <IoPeople />, active: false, content: <p className="p-16 text-center">No data</p> },
-    { index: 2, title: 'Classes', icon: <FaChalkboardTeacher />, active: false, content: <p className="p-16 text-center">No data</p> },
-    { index: 3, title: 'Curricular', icon: <FaGraduationCap />, active: true, content: <p className="p-16 text-center">No data</p> },
+    { index: 2, title: 'Classes', icon: <FaChalkboardTeacher />, active: true, content: <p className="p-16 text-center">No data</p> },
+    { index: 3, title: 'Curricular', icon: <FaGraduationCap />, active: false, content: <p className="p-16 text-center">No data</p> },
     { index: 4, title: 'Rooms', icon: <FaHotel />, active: false, content: <p className="p-16 text-center">No data</p> }
   ]
 
@@ -48,7 +49,7 @@ const InstitutionInfo = (instProps: InstitutionInfoProps) => {
     getUrl();
   }, [instProps?.institute.image]);
 
-  const { name, image } = instProps?.institute
+  const { name, image, type, address, addressLine2, city, state, district, zip, phone, website } = instProps?.institute
   return (
     <div>
       <div className="h-9/10 flex flex-col md:flex-row">
@@ -62,7 +63,9 @@ const InstitutionInfo = (instProps: InstitutionInfoProps) => {
               src={imageUrl}
             /> : <div className={`w-20 h-20 md:w-40 md:h-40 p-2 md:p-4 flex flex-shrink-0 justify-center items-center rounded-full border border-gray-400 shadow-elem-light`}>
               <div className="h-full w-full flex justify-center items-center text-5xl text-extrabold text-white rounded-full" style={{ background: `${stringToHslColor('I' + ' ' + 'N')}`, textShadow: '0.2rem 0.2rem 3px #423939b3' }}>
-                {initials('IN', 'NI')}
+                {name && (
+                  initials(getInitialsFromString(name)[0], getInitialsFromString(name)[1])
+                )}
               </div>
             </div>}
 
@@ -75,8 +78,6 @@ const InstitutionInfo = (instProps: InstitutionInfoProps) => {
 
         {/* General information section */}
         <div className='w-full'>
-          {/* <UnderlinedTabs tabs={tabs} /> */}
-
           <div className='bg-white shadow-5 overflow-hidden sm:rounded-lg mb-4'>
 
             <div className="px-4 py-5 border-b border-gray-200 sm:px-6">
@@ -90,22 +91,27 @@ const InstitutionInfo = (instProps: InstitutionInfoProps) => {
                 <p className="text-base leading-5 font-medium text-gray-500 my-3 flex">
                   <span className="text-gray-900 mr-2 w-3/10">Address:</span>
                   <span className="w-auto">
-                    1234 Somewhere St., <br />NYC street, Houston, <br />TX.-36002
+                    {address && (address + ', ')}
+                    {addressLine2 && (addressLine2 + ', ')}<br />
+                    {city && city}
+                    {district && district}<br />
+                    {state && state}
+                    {zip && ('-' + zip)}
                   </span>
                 </p>
                 <p className="text-base leading-5 font-medium text-gray-500 my-3 flex">
                   <span className="text-gray-900 mr-2 w-3/10">Contact No:</span>
-                  <span className="w-auto">1234567898</span>
+                  <span className="w-auto">{phone ? formatPhoneNumber(phone) : '--'}</span>
                 </p>
               </div>
               <div className="p-8">
                 <p className="text-base leading-5 font-medium text-gray-500 my-3 flex">
                   <span className="text-gray-900 mr-2 w-3/10">Institution Type:</span>
-                  <span className="w-auto">ORG</span>
+                  <span className="w-auto">{type ? type : '--'}</span>
                 </p>
                 <p className="text-base leading-5 font-medium text-gray-500 my-3 flex">
                   <span className="text-gray-900 mr-2 w-3/10">Website:</span>
-                  <span className="w-auto">iconoclastartist.org</span>
+                  <span className="w-auto">{website ? getHostNameFromUrl(website) : '--'}</span>
                 </p>
               </div>
             </div>
