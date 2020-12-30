18782edab159705408b902d906a39925a8f7e042
samboosa5k <jasper.verbon@gmail.com>
2020-12-30 15:07:10 +0100

    disabled quick register in teacher view, needs fix

--- src/components/TeacherView/TopMenu/HamburgerMenu.tsx
+++ src/components/TeacherView/TopMenu/HamburgerMenu.tsx
@@ -17,7 +17,7 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = (
   const { handleClick, setQuickRegister, handleHomePopup } = props;
   const [hamburgerOpen, setHamburgerOpen] = useState<boolean>(false);
 
- 
+
 
   return (
     <div className="w-auto mr-4 flex flex-col content-between ">
@@ -33,15 +33,13 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = (
 
         <div className="absolute w-full top-1">
           <div
-            className={`${
-              hamburgerOpen ? "visible" : "hidden"
-            } relative arrow-up right-1`}
+            className={`${hamburgerOpen ? "visible" : "hidden"
+              } relative arrow-up right-1`}
           />
 
           <div
-            className={`${
-              hamburgerOpen ? "visible" : "hidden"
-            } absolute w-32 h-auto transform right-0 flex  flex-col bg-gray-200 p-1 rounded-lg border border-light-gray border-opacity-40 overflow-hidden z-100`}
+            className={`${hamburgerOpen ? "visible" : "hidden"
+              } absolute w-32 h-auto transform right-0 flex  flex-col bg-gray-200 p-1 rounded-lg border border-light-gray border-opacity-40 overflow-hidden z-100`}
             onPointerLeave={() => setHamburgerOpen(false)}
           >
             <div className="flex flex-col w-full h-full bg-gray-400 rounded-lg z-100">
@@ -50,37 +48,25 @@ const HamburgerMenu: React.FC<HamburgerMenuProps> = (
                 className="h-8 w-full flex justify-center items-center bg-gray-200 hover:bg-white text-gray-600 text-xs rounded-t-lg border-b border-light-gray border-opacity-20 active:shadow-none cursor-pointer"
                 onClick={handleHomePopup}
               >
-                {/* <IconContext.Provider value={{ size: '1.5rem' }}>
-                  <span className='w-12'>
-                    <FaHome />
-                  </span>
-                </IconContext.Provider> */}
+
                 <span className="w-full text-left pl-2">Home</span>
               </div>
 
               {/* QUICK REGISTER */}
-              <div
+              {/* <div
                 className="h-8 w-full flex justify-center items-center bg-gray-200 hover:bg-white text-gray-600 text-xs active:shadow-none cursor-pointer"
-                onClick={()=>setQuickRegister(true)}
+                onClick={() => setQuickRegister(true)}
               >
-                {/* <IconContext.Provider value={{ size: '1.5rem' }}>
-                  <span className='w-12'>
-                    <FaHome />
-                  </span>
-                </IconContext.Provider> */}
+
                 <span className="w-full text-left pl-2">Quick Register</span>
-              </div>
+              </div> */}
 
               {/* USER MANAGEMENT */}
               <div
                 className="h-8 w-full flex justify-center items-center bg-gray-200 hover:bg-white text-gray-600 text-xs rounded-b-lg active:shadow-none cursor-pointer"
                 onClick={handleClick}
               >
-                {/* <IconContext.Provider value={{ size: '1.5rem' }}>
-                  <span className='w-12'>
-                    <FiUsers />
-                  </span>
-                </IconContext.Provider> */}
+
                 <span className="w-full text-left pl-2">User Management</span>
               </div>
             </div>
