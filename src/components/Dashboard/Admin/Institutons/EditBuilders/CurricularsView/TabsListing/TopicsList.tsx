482a41de3dab2b7cc559e9d20864333be899d042
Aman Gupta <aman@sublimedatasys.com>
2021-01-08 11:32:31 +0530

    topics list

--- src/components/Dashboard/Admin/Institutons/EditBuilders/CurricularsView/TabsListing/TopicsList.tsx
+++ src/components/Dashboard/Admin/Institutons/EditBuilders/CurricularsView/TabsListing/TopicsList.tsx
@@ -15,7 +15,7 @@ interface TopicsListProps {
 const TopicsList = (props: TopicsListProps) => {
   const { topicsList, curricularId } = props;
   const history = useHistory();
-
+  const [loading, setLoading] = useState(false)
   const [topics, setTopics] = useState([])
 
   const createNewTopic = () => {
@@ -29,11 +29,13 @@ const TopicsList = (props: TopicsListProps) => {
   }
 
   const fetchTopics = async () => {
+    setLoading(true)
     let list: any = await API.graphql(graphqlOperation(customQueries.listTopics, {
       filter: { curriculumID: { eq: curricularId } },
     }));
     list = list.data.listTopics?.items || []
     setTopics(list)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -45,46 +47,51 @@ const TopicsList = (props: TopicsListProps) => {
       <div className="">
         <PageWrapper>
           <h3 className="text-lg leading-6 font-medium text-gray-900 text-center pb-8 ">CURRICULAR TOPICS</h3>
-          {(topicsList && topicsList.length > 0) ? (
-            <Fragment>
-              <div className="flex justify-end w-8/10 m-auto ">
-                <Buttons btnClass="mx-4" label="Add new Topic" onClick={createNewTopic} />
-              </div>
-              <div className="my-8 w-8/10 m-auto max-h-88 overflow-y-auto">
+          {
+            !loading ?
+            <>
+              {(topics && topics.length > 0) ? (
+                <Fragment>
+                  <div className="flex justify-end w-8/10 m-auto ">
+                    <Buttons btnClass="mx-4" label="Add new Topic" onClick={createNewTopic} />
+                  </div>
+                  <div className="my-8 w-8/10 m-auto max-h-88 overflow-y-auto">
 
-                <div className="flex justify-between w-full  px-8 py-4 whitespace-no-wrap border-b border-gray-200">
-                  <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>No.</span>
-                  </div>
-                  <div className="w-6/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Topic Name</span>
-                  </div>
-                  <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Actions</span>
-                  </div>
-                </div>
-
-                {/* Edit as per respective topics fields.  */}
-                {topicsList.map((item, index) => (
-                  <div key={index} className="flex justify-between w-full px-8 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="flex w-1/10 items-center px-8 py-3 text-left text-s leading-4">{index + 1}.</div>
-                    <div className="flex w-6/10 items-center px-8 py-3 text-left text-s leading-4 font-medium ">
-                      {item.name ? item.name : ''}
+                    <div className="flex justify-between w-full  px-8 py-4 whitespace-no-wrap border-b border-gray-200">
+                      <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        <span>No.</span>
+                      </div>
+                      <div className="w-6/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        <span>Topic Name</span>
+                      </div>
+                      <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                        <span>Actions</span>
+                      </div>
                     </div>
-                    <span className="w-3/10 h-6 flex items-center text-left px-8 py-3 text-indigo-600 hover:text-indigo-900 cursor-pointer" onClick={() => editCurrentTopic(item.id)}>
-                      edit
+
+                    {/* Edit as per respective topics fields.  */}
+                    {topics.map((item, index) => (
+                      <div key={index} className="flex justify-between w-full px-8 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="flex w-1/10 items-center px-8 py-3 text-left text-s leading-4">{index + 1}.</div>
+                        <div className="flex w-6/10 items-center px-8 py-3 text-left text-s leading-4 font-medium ">
+                          {item.name}
+                        </div>
+                        <span className="w-3/10 h-6 flex items-center text-left px-8 py-3 text-indigo-600 hover:text-indigo-900 cursor-pointer" onClick={() => editCurrentTopic(item.id)}>
+                          edit
                   </span>
+                      </div>
+                    ))}
                   </div>
-                ))}
-              </div>
-            </Fragment>
-          ) : (
-              <Fragment>
-                <div className="flex justify-center mt-8">
-                  <Buttons btnClass="mx-4" label="Add new Topic" onClick={createNewTopic} />
-                </div>
-                <p className="text-center p-16">  This curricular does not have any topics. Please create a new one.</p>
-              </Fragment>)}
+                </Fragment>
+              ) : (
+                  <Fragment>
+                    <div className="flex justify-center mt-8">
+                      <Buttons btnClass="mx-4" label="Add new Topic" onClick={createNewTopic} />
+                    </div>
+                    <p className="text-center p-16">  This curricular does not have any topics. Please create a new one.</p>
+                  </Fragment>)}
+            </> : <div>Loading...</div>
+          }
         </PageWrapper>
       </div>
     </div>
