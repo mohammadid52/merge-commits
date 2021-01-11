5829a2ffe7336b52f37a88c8e73101be1b3d2c6b
sublimedatasys <sahilshah@sublimedatasys.com>
2021-01-11 15:42:20 +0530

    Design fixes for phase -1 flow.

--- src/components/Dashboard/Admin/Institutons/Listing/ClassList.tsx
+++ src/components/Dashboard/Admin/Institutons/Listing/ClassList.tsx
@@ -29,21 +29,22 @@ const ClassList = (props: ClassListProps) => {
                 <div className="flex justify-end w-8/10 m-auto ">
                   <Buttons btnClass="mx-4" label="Create new class" onClick={createNewClass} />
                 </div>
-                <div className="my-8 w-8/10 m-auto max-h-88 overflow-y-auto">
-                  <div className="flex justify-between w-full  px-8 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      <span>No.</span>
-                    </div>
-                    <div className="w-6/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      <span>Class Name</span>
-                    </div>
-                    {/* <div className="w-4/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+
+                <div className="flex justify-between w-8/10 m-auto px-8 py-4 whitespace-no-wrap border-b border-gray-200">
+                  <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    <span>No.</span>
+                  </div>
+                  <div className="w-6/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    <span>Class Name</span>
+                  </div>
+                  {/* <div className="w-4/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                       <span>Active Students</span>
                     </div> */}
-                    <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                      <span>Actions</span>
-                    </div>
+                  <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    <span>Actions</span>
                   </div>
+                </div>
+                <div className="w-8/10 m-auto max-h-88 overflow-y-auto">
                   {classes.items.map((item, index) => (
                     <div key={index} className="flex justify-between w-full px-8 py-4 whitespace-no-wrap border-b border-gray-200">
                       <div className="flex w-1/10 items-center px-8 py-3 text-left text-s leading-4">{index + 1}.</div>
--- src/components/Dashboard/Admin/Institutons/Listing/RoomsList.tsx
+++ src/components/Dashboard/Admin/Institutons/Listing/RoomsList.tsx
@@ -51,7 +51,7 @@ const RoomsList = (props: RoomListProps) => {
   }, [])
 
   return (
-    <div className="py-8 px-4 flex m-auto justify-center">
+    <div className="py-8 flex m-auto justify-center">
       <div className="">
         <PageWrapper>
           <h3 className="text-lg leading-6 font-medium text-gray-900 text-center pb-8 ">INSTITUTE ROOMS</h3>
@@ -61,39 +61,39 @@ const RoomsList = (props: RoomListProps) => {
               <div className="flex justify-end">
                 <Buttons btnClass="mx-4" label="Create new Room" onClick={createNewRoom} />
               </div>
-              <div className="my-8 m-auto max-h-88 overflow-y-auto overflow-x-auto">
 
-                <div className="flex justify-between w-full  px-8 py-4 border-b border-gray-200">
-                  <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>No.</span>
-                  </div>
-                  <div className="w-4/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Room Name</span>
-                  </div>
-                  <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Class Name</span>
-                  </div>
-                  <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Teacher</span>
-                  </div>
-                  <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Max. Students</span>
-                  </div>
-                  <div className="w-3/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                    <span>Actions</span>
-                  </div>
+              <div className="flex justify-between w-full  px-8 py-4 border-b border-gray-200">
+                <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  <span>No.</span>
                 </div>
+                <div className="w-2/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  <span>Room Name</span>
+                </div>
+                <div className="w-2/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  <span>Class Name</span>
+                </div>
+                <div className="w-2/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  <span>Teacher</span>
+                </div>
+                <div className="w-2/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  <span>Max. Students</span>
+                </div>
+                <div className="w-1/10 px-8 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  <span>Actions</span>
+                </div>
+              </div>
 
+              <div className="m-auto max-h-88 overflow-y-auto overflow-x-auto">
                 {roomList.map((item: any, i: number) => (
                   <div key={i} className="flex justify-between items-center w-full px-8 py-4 border-b border-gray-200">
                     <div className="flex w-1/10 items-center px-8 py-3 text-left text-s leading-4">{i + 1}.</div>
-                    <div className="flex w-4/10 items-center px-8 py-3 text-left text-s leading-4 font-medium ">
+                    <div className="flex w-2/10 items-center px-8 py-3 text-left text-s leading-4 font-medium whitespace-normal">
                       {item.name}
                     </div>
-                    <div className="flex w-3/10 items-center px-8 py-3 text-left text-s leading-4">{item.class?.name}</div>
-                    <div className="flex w-3/10 items-center px-8 py-3 text-left text-s leading-4">{item.teacher?.firstName || ''} {item.teacher?.lastName || ''}</div>
-                    <div className="flex w-3/10 items-center px-8 py-3 text-left text-s leading-4">{item.maxPersons}</div>
-                    <span className="w-3/10 h-6 flex items-center cursor-pointer text-left px-8 py-3 text-indigo-600 hover:text-indigo-900" onClick={() => editCurrentRoom(item.id)}>
+                    <div className="flex w-2/10 items-center px-8 py-3 text-left text-s leading-4">{item.class?.name}</div>
+                    <div className="flex w-2/10 items-center px-8 py-3 text-left text-s leading-4">{item.teacher?.firstName || ''} {item.teacher?.lastName || ''}</div>
+                    <div className="flex w-2/10 items-center px-8 py-3 text-left text-s leading-4">{item.maxPersons}</div>
+                    <span className="w-1/10 h-6 flex items-center cursor-pointer text-left px-8 py-3 text-indigo-600 hover:text-indigo-900" onClick={() => editCurrentRoom(item.id)}>
                       edit
                   </span>
                   </div>
