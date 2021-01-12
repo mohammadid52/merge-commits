041a2e0c074643207afbc2a7c700d3c28a3ef888
Aman Gupta <aman@sublimedatasys.com>
2021-01-12 14:25:37 +0530

    UATenv setup

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,16 +3,16 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:570f5ed7-e35a-4b86-9b32-1575f0b91ed5",
+    "aws_cognito_identity_pool_id": "us-east-1:d604ed31-4c20-440c-bfd8-4207f3ccb2b7",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_aZO8Dh08l",
-    "aws_user_pools_web_client_id": "5fqplej0kiqdp7pvjssq4i76v0",
+    "aws_user_pools_id": "us-east-1_jNgXBFd1o",
+    "aws_user_pools_web_client_id": "1hj7i8g22g21bahhegja4ath6d",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://iuj7bhgjvbhbhazv6e3oi2zqky.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://xcvfgyl75nhcnaaay35kmqvuka.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-6smjv3ukrndjbculzwsxwzdy6m",
-    "aws_user_files_s3_bucket": "iconoclaststorage141704-uat",
+    "aws_appsync_apiKey": "da2-7j4nhhaq7nbgrbt42zn6spmx5m",
+    "aws_user_files_s3_bucket": "iconoclaststorage83444-uatenv",
     "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
