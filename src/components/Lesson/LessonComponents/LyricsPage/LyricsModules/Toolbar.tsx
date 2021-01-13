6b8b1d886d7e5cf87a631845ef5e348c1bde7187
samboosa5k <jasper.verbon@gmail.com>
2021-01-13 14:26:28 +0100

    highlighter label changed

--- src/components/Lesson/LessonComponents/LyricsPage/LyricsModules/Toolbar.tsx
+++ src/components/Lesson/LessonComponents/LyricsPage/LyricsModules/Toolbar.tsx
@@ -33,7 +33,7 @@ const ToolBar = (props: ToolbarProps) => {
         className={`flex flex-row w-auto text-xl ${theme.banner} ${theme.underline} flex flex-row`}
         style={{ marginTop: '0', paddingBottom: '0' }}>
         <div className="w-3.3/10  flex flex-col justify-center items-center">
-          <span>Highlighters:</span>
+          <span>Select Highlighter:</span>
         </div>
         <div className={`w-3.3/10  flex  justify-center items-center z-50`}>
           <div
@@ -55,7 +55,7 @@ const ToolBar = (props: ToolbarProps) => {
                 id={button.color}
                 className={`${color === button.color ? 'border-2 border-white' : ''} 
                   bg-${button.color} 
-                  relative h-12 w-12 text-3xl rounded-lg mx-4 flex flex-row justify-center items-center
+                  relative h-12 w-12 text-3xl rounded-lg mx-2 flex flex-row justify-center items-center
                   `}
                 onClick={handleClick}>
                 <ToolTip
@@ -77,7 +77,7 @@ const ToolBar = (props: ToolbarProps) => {
 
             <div
               id="erase"
-              className={`relative bg-gray-200 h-12 w-12 text-3xl rounded-lg mb-2 mx-4 flex flex-row justify-center items-center`}
+              className={`relative bg-gray-200 h-12 w-12 text-3xl rounded-lg mb-2 mx-2 flex flex-row justify-center items-center`}
               onClick={handleClick}>
               <ToolTip
                 position="bottom"
@@ -95,7 +95,7 @@ const ToolBar = (props: ToolbarProps) => {
             </div>
           </div>
         </div>
-        <div className="w-3.3/10  w-16 flex items-center">
+        {/*<div className="w-3.3/10  w-16 flex items-center">
           <IconContext.Provider
             value={{
               className: 'ml-auto mr-0',
@@ -105,15 +105,15 @@ const ToolBar = (props: ToolbarProps) => {
             }}>
             <FaHighlighter />
           </IconContext.Provider>
-        </div>
+        </div>*/}
       </div>
 
       <div className="w-full leading-6 border-b border-white border-opacity-10 mb-4">
-        <p className="pl-2 text-sm text-left  text-white ">
-          1. To select a <b>sentence/phrase</b>, click the 'first word' and the 'last word'.
+        <p className="pl-2 text-sm text-left text-white">
+          1. <b>Click 2 separate words</b> to select a sentence.
         </p>
-        <p className="pl-2 text-sm text-left  text-white ">
-          2. To select <b>one</b> word, double click the word.
+        <p className="pl-2 text-sm text-left text-white">
+          2. <b>Double-Click 1 word</b> to select a word.
         </p>
       </div>
     </div>
