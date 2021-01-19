6816579eeb5a0153b40e1d71d01b2eb0cc7ce005
sublimedatasys <sahilshah@sublimedatasys.com>
2021-01-19 20:51:29 +0530

    comment out changes on getImageFromS3 function.

--- src/utilities/services.ts
+++ src/utilities/services.ts
@@ -3,13 +3,16 @@ import awsconfig from '../aws-exports';
 
 export const getImageFromS3 = (key: string, isPrivate?: boolean) => {
   if (key) {
-    if (!isPrivate) {
-      const bucketname = awsconfig.aws_user_files_s3_bucket;
-      const bucketRegion = awsconfig.aws_user_files_s3_bucket_region;
-      if (bucketname) {
-        return `https://${bucketname}.s3.${bucketRegion}.amazonaws.com/public/${key}`;
-      }
-    }
+
+    // Needs to fetch full URL to support image editing without refresh.
+
+    // if (!isPrivate) {
+    //   const bucketname = awsconfig.aws_user_files_s3_bucket;
+    //   const bucketRegion = awsconfig.aws_user_files_s3_bucket_region;
+    //   if (bucketname) {
+    //     return `https://${bucketname}.s3.${bucketRegion}.amazonaws.com/public/${key}`;
+    //   }
+    // }
     return new Promise((resolve, reject) => {
       Storage.get(key).then((result: string) => {
         resolve(result);
