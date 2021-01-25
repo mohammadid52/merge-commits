7fa82984dcf96a4bffa880389c64a4ebd9e43635
sublimedatasys <sahilshah@sublimedatasys.com>
2021-01-25 21:47:27 +0530

    Rich text editor implementation.

--- src/components/Atoms/RichTextEditor.tsx
+++ src/components/Atoms/RichTextEditor.tsx
@@ -6,7 +6,7 @@ import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 interface RichTextEditorProps {
-  onChange: (html: string) => void
+  onChange: (html: string, text: string) => void
   initialValue: string
 }
 
@@ -27,54 +27,58 @@ const RichTextEditor = (props: RichTextEditorProps) => {
   ]
   const onEditorStateChange = (editorState: any) => {
     const editorStateHtml: string = draftToHtml(convertToRaw(editorState.getCurrentContent()))
-    onChange(editorStateHtml)
-    setEditorState({
-      editorState,
-    });
+    const editorStatePlainText: string = editorState.getCurrentContent().getPlainText();
+    onChange(editorStateHtml, editorStatePlainText)
+    setEditorState(editorState);
   };
 
   useEffect(() => {
     const html = initialValue;
     const contentBlock = htmlToDraft(html);
+
     let editorState;
     if (contentBlock) {
       const contentState = ContentState.createFromBlockArray(
         contentBlock.contentBlocks
       );
       editorState = EditorState.createWithContent(contentState);
-
     }
-    //  else {
-    //   editorState = EditorState.createEmpty()
-    // }
+    else {
+      editorState = EditorState.createEmpty()
+    }
+    setEditorState(editorState);
   }, [])
 
   return (
     <Editor
       editorState={editorState}
-      onEditorStateChange={onEditorStateChange}
       toolbarClassName="toolbarClassName"
       wrapperClassName="wrapperClassName"
       editorClassName="editorClassName"
+      onEditorStateChange={onEditorStateChange}
       toolbar={{
         options: options,
         inline: {
           inDropdown: false,
           options: ['bold', 'italic', 'underline'],
+          className: 'toolItemClassName'
         },
-        list: { inDropdown: true },
-        textAlign: { inDropdown: true },
-        link: { inDropdown: true },
-        history: { inDropdown: true },
+        list: { inDropdown: true, className: 'dropdownClassName' },
+        textAlign: { inDropdown: true, className: 'dropdownClassName' },
+        link: { inDropdown: true, className: 'dropdownClassName' },
+        history: { inDropdown: true, className: 'dropdownClassName' },
         fontFamily: {
           options: ['Arial', 'Georgia', 'Impact', 'Courier', 'Times New Roman', 'Helvetica'],
-          className: 'plainText'
+          className: 'plainText dropdownBlockClassName'
         },
         blockType: {
-          className: 'plainText'
+          className: 'plainText dropdownBlockClassName'
         },
         fontSize: {
-          className: 'plainText'
+          className: 'plainText dropdownClassName'
+        },
+        colorPicker: {
+          className: 'toolItemClassName'
         }
       }}
     />
