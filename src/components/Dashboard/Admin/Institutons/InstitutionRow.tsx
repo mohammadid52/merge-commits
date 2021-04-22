0c2e0c84bf7147091ec86e96bd53479daf98c840
sublimedatasys <sahilshah@sublimedatasys.com>
2021-04-22 12:54:36 +0530

    fix/width-institute-name: Added tooltip for long institution names

--- src/components/Dashboard/Admin/Institutons/InstitutionRow.tsx
+++ src/components/Dashboard/Admin/Institutons/InstitutionRow.tsx
@@ -7,10 +7,11 @@ import {
   getHostNameFromUrl,
   initials,
   getInitialsFromString,
-  formatPhoneNumber
+  formatPhoneNumber,
 } from '../../../../utilities/strings';
 import { getImageFromS3 } from '../../../../utilities/services';
 import { GlobalContext } from '../../../../contexts/GlobalContext';
+import Tooltip from '../../../Atoms/Tooltip';
 
 interface InstitutionRowProps {
   id?: string;
@@ -26,9 +27,7 @@ interface InstitutionRowProps {
  * @param instRowPrps - Props which provide a single Institute object with the key/values as described in the interface above
  */
 
-const InstitutionRow: React.FC<InstitutionRowProps> = (
-  instRowPrps: InstitutionRowProps
-) => {
+const InstitutionRow: React.FC<InstitutionRowProps> = (instRowPrps: InstitutionRowProps) => {
   const match = useRouteMatch();
   const history = useHistory();
   const { theme, clientKey } = useContext(GlobalContext);
@@ -37,7 +36,7 @@ const InstitutionRow: React.FC<InstitutionRowProps> = (
 
   const handleInstitutionView = () => {
     history.push(`${match.url}/institution?id=${instRowPrps.id}`);
-  }
+  };
 
   useEffect(() => {
     async function getUrl() {
@@ -45,26 +44,49 @@ const InstitutionRow: React.FC<InstitutionRowProps> = (
       setImageUrl(imageUrl);
     }
     getUrl();
-  }, [instRowPrps.image])
+  }, [instRowPrps.image]);
+
+  const eclipse = (text: string, len: number): string => {
+    if (text) {
+      if (text.length <= len) {
+        return text;
+      } else {
+        return `${text.substring(0, len)}...`;
+      }
+    }
+  };
 
   return (
     <div id={instRowPrps.id} className="flex justify-between bg-white w-full border-b-0 border-gray-200">
-
       <div className="w-3/10 px-8 py-4 whitespace-nowrap">
         <div className="flex items-center">
           <div className="flex-shrink-0 h-10 w-10 flex items-center">
-            {instRowPrps.image ?
-              (<img
-                src={imageUrl}
-                className="h-8 w-8 rounded-full" />) :
-              <div className="h-8 w-8 rounded-full flex justify-center items-center text-white text-sm text-bold" style={{ background: `${stringToHslColor(getInitialsFromString(instRowPrps.name)[0] + ' ' + getInitialsFromString(instRowPrps.name)[1])}`, textShadow: '0.1rem 0.1rem 2px #423939b3' }} >
-                {instRowPrps.name ? initials(getInitialsFromString(instRowPrps.name)[0], getInitialsFromString(instRowPrps.name)[1]) : initials('N', 'A')}
-              </div>}
+            {instRowPrps.image ? (
+              <img src={imageUrl} className="h-8 w-8 rounded-full" />
+            ) : (
+              <div
+                className="h-8 w-8 rounded-full flex justify-center items-center text-white text-sm text-bold"
+                style={{
+                  background: `${stringToHslColor(
+                    getInitialsFromString(instRowPrps.name)[0] + ' ' + getInitialsFromString(instRowPrps.name)[1]
+                  )}`,
+                  textShadow: '0.1rem 0.1rem 2px #423939b3',
+                }}>
+                {instRowPrps.name
+                  ? initials(getInitialsFromString(instRowPrps.name)[0], getInitialsFromString(instRowPrps.name)[1])
+                  : initials('N', 'A')}
+              </div>
+            )}
           </div>
           <div className="ml-2">
-            <div id={instRowPrps.id} className="hover:text-gray-600 cursor-pointer text-sm leading-5 font-medium text-gray-900" onClick={handleInstitutionView} >
-              {instRowPrps.name}
-            </div>
+            <Tooltip text={instRowPrps.name} placement="bottomleft">
+              <div
+                id={instRowPrps.id}
+                className="hover:text-gray-600 cursor-pointer text-sm leading-5 font-medium text-gray-900"
+                onClick={handleInstitutionView}>
+                {eclipse(instRowPrps.name, 22)}
+              </div>
+            </Tooltip>
           </div>
         </div>
       </div>
@@ -75,7 +97,9 @@ const InstitutionRow: React.FC<InstitutionRowProps> = (
       </div>
       <div className="w-3.5/10 flex justify-center px-8 py-4 whitespace-nowrap">
         <div className="flex flex-col justify-center items-center cursor-pointer" onClick={handleInstitutionView}>
-          <div id={instRowPrps.id} className="w-auto text-sm leading-5 text-gray-900 hover:text-gray-600 ">{instRowPrps.website ? getHostNameFromUrl(instRowPrps.website) : '--'}</div>
+          <div id={instRowPrps.id} className="w-auto text-sm leading-5 text-gray-900 hover:text-gray-600 ">
+            {instRowPrps.website ? getHostNameFromUrl(instRowPrps.website) : '--'}
+          </div>
         </div>
       </div>
       <div className="w-1.5/10 flex justify-center items-center px-8 py-4 whitespace-nowrap">
@@ -83,8 +107,12 @@ const InstitutionRow: React.FC<InstitutionRowProps> = (
           {instRowPrps.phone ? formatPhoneNumber(instRowPrps.phone) : '--'}
         </span>
       </div>
-      <div className="w-1/10 flex justify-center items-center pr-4 py-4 cursor-pointer whitespace-nowrap text-right text-sm leading-5 font-medium" onClick={handleInstitutionView} >
-        <div id={instRowPrps.id} className={`flex justify-center ${theme.textColor[themeColor]}`}>Edit</div>
+      <div
+        className="w-1/10 flex justify-center items-center pr-4 py-4 cursor-pointer whitespace-nowrap text-right text-sm leading-5 font-medium"
+        onClick={handleInstitutionView}>
+        <div id={instRowPrps.id} className={`flex justify-center ${theme.textColor[themeColor]}`}>
+          Edit
+        </div>
       </div>
     </div>
   );
