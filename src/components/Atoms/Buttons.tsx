67f728bda1b5c180ac1b06d86d6b6e938621bb0e
samboosa5k <jasper.verbon@gmail.com>
2021-04-26 14:15:57 +0200

    added simple style differences to lesson card buttons

--- src/components/Atoms/Buttons.tsx
+++ src/components/Atoms/Buttons.tsx
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react';
-import { IconContext } from 'react-icons/lib/esm/iconContext';
-import { GlobalContext } from '../../contexts/GlobalContext';
-import { getAsset } from '../../assets';
+import React, {useContext} from 'react';
+import {IconContext} from 'react-icons/lib/esm/iconContext';
+import {GlobalContext} from '../../contexts/GlobalContext';
+import {getAsset} from '../../assets';
 
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset';
@@ -10,13 +10,24 @@ interface ButtonProps {
   transparent?: boolean;
   Icon?: any;
   btnClass?: string;
+  overrideClass?: boolean;
   labelClass?: string;
   disabled?: boolean;
 }
 
 const Buttons: React.FC<ButtonProps> = (btnPrps: ButtonProps) => {
-  const { label, Icon, transparent, type, onClick, btnClass, labelClass, disabled } = btnPrps;
-  const { theme, clientKey } = useContext(GlobalContext);
+  const {
+    label,
+    Icon,
+    transparent,
+    type,
+    onClick,
+    btnClass,
+    overrideClass,
+    labelClass,
+    disabled,
+  } = btnPrps;
+  const {theme, clientKey} = useContext(GlobalContext);
   const themeColor = getAsset(clientKey, 'themeClassName');
 
   return (
@@ -25,14 +36,16 @@ const Buttons: React.FC<ButtonProps> = (btnPrps: ButtonProps) => {
       type={type ? type : 'button'}
       className={`font-bold uppercase text-xs px-4 py-2 rounded-lg flex items-center justify-center w-auto ${
         theme.outlineNone
-      } ${transparent ? theme.btn.cancel : theme.btn[themeColor]} ${btnClass ? btnClass : ''} ${
-        disabled ? 'cursor-not-allowed' : 'cursor-pointer'
-      }`}
+      } ${transparent ? theme.btn.cancel : !overrideClass ? theme.btn[themeColor] : ''} ${
+        btnClass ? btnClass : ''
+      } ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
       onClick={onClick}>
-      {label ? <span className={`mx-2 ${labelClass ? labelClass : ''}`}>{label}</span> : null}
+      {label ? (
+        <span className={`mx-2 ${labelClass ? labelClass : ''}`}>{label}</span>
+      ) : null}
       {Icon ? (
         <span className="w-8 h-8 flex items-center">
-          <IconContext.Provider value={{ size: '1.5rem', color: '#ffffff' }}>
+          <IconContext.Provider value={{size: '1.5rem', color: '#ffffff'}}>
             <Icon />
           </IconContext.Provider>
         </span>
