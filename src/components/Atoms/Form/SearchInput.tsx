0f18139a07235352114665b3b31089e070600bac
sublimedatasys <sahilshah@sublimedatasys.com>
2021-04-28 16:06:16 +0530

    student-searchbar: Updated searchbar

--- src/components/Atoms/Form/SearchInput.tsx
+++ src/components/Atoms/Form/SearchInput.tsx
@@ -1,48 +1,68 @@
-import React, { useContext } from 'react';
-import { IconContext } from 'react-icons';
-import { IoSearchSharp, IoClose } from 'react-icons/io5';
-import { GlobalContext } from '../../../contexts/GlobalContext';
-import { getAsset } from '../../../assets';
+import React, {useContext} from 'react';
+import {IconContext} from 'react-icons';
+import {IoSearchSharp, IoClose} from 'react-icons/io5';
+import {GlobalContext} from '../../../contexts/GlobalContext';
+import {getAsset} from '../../../assets';
 
 interface SearchProps {
-  value?: string
-  onChange?: (str: string) => void
-  onKeyDown?: () => void
-  closeAction?: () => void
-  style?: string
+  value?: string;
+  onChange?: (str: string) => void;
+  onKeyDown?: () => void;
+  closeAction?: () => void;
+  style?: string;
 }
 
 const SearchInput: React.FC<SearchProps> = (searchProps: SearchProps) => {
-  const { value, onChange, onKeyDown, closeAction, style } = searchProps;
-  const { theme, clientKey } = useContext(GlobalContext);
+  const {value, onChange, onKeyDown, closeAction, style} = searchProps;
+  const {theme, clientKey} = useContext(GlobalContext);
   const themeColor = getAsset(clientKey, 'themeClassName');
 
   const search = (code: number) => {
     if (code === 13) {
-      onKeyDown()
+      onKeyDown();
     }
-  }
+  };
+
+  const [showFull, setShowFull] = React.useState(false);
+
   return (
-    <div className={`flex w-auto py-3 px-4 rounded  ${theme.formSelect} ${theme.outlineNone} ${style ? style : ''}`}>
-      <span className="w-6 mr-4 cursor-pointer" onClick={onKeyDown}>
-        <IconContext.Provider value={{ size: '1.5rem', color: theme.searchIcon[themeColor] }}>
+    <div
+      onBlur={() => setShowFull(false)}
+      className={`flex w-auto py-3 ${
+        showFull ? 'px-4' : 'px-2'
+      } rounded justify-center  ${theme.formSelect} ${theme.outlineNone} ${
+        style ? style : ''
+      }`}>
+      <span
+        className={`w-6 cursor-pointer ${showFull && 'mr-4'}`}
+        onClick={() => {
+          setShowFull(!showFull);
+          onKeyDown();
+        }}>
+        <IconContext.Provider
+          value={{size: '1.5rem', color: theme.searchIcon[themeColor]}}>
           <IoSearchSharp />
         </IconContext.Provider>
       </span>
-      <input
-        placeholder="Search..."
-        id='searchInput'
-        value={value ? value : ''}
-        onChange={(e: any) => onChange(e.target.value)}
-        onKeyDown={(e: any) => search(e.keyCode)}
-        className={`${theme.outlineNone}`} />
-      {value !== '' && <span className="w-6 ml-4 cursor-pointer" onClick={closeAction}>
-        <IconContext.Provider value={{ size: '1.5rem', color: '#000000' }}>
-          <IoClose />
-        </IconContext.Provider>
-      </span>}
+      {showFull && (
+        <input
+          placeholder="Search..."
+          id="searchInput"
+          value={value ? value : ''}
+          onChange={(e: any) => onChange(e.target.value)}
+          onKeyDown={(e: any) => search(e.keyCode)}
+          className={`${theme.outlineNone}`}
+        />
+      )}
+      {value !== '' && (
+        <span className="w-6 ml-4 cursor-pointer" onClick={closeAction}>
+          <IconContext.Provider value={{size: '1.5rem', color: '#000000'}}>
+            <IoClose />
+          </IconContext.Provider>
+        </span>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default SearchInput
+export default SearchInput;
--- src/components/Dashboard/Home/StudentsTiles.tsx
+++ src/components/Dashboard/Home/StudentsTiles.tsx
@@ -88,7 +88,7 @@ const StudentsTiles = (props: {
               list.length > 12 && isTeacher ? 'center' : 'end'
             } w-full`}>
             {isTeacher && (
-              <div className="w-8/10">
+              <div className="w-auto">
                 <SearchInput
                   value={searchInput.value}
                   onChange={setSearch}
@@ -113,10 +113,10 @@ const StudentsTiles = (props: {
       <ContentCard
         hasBackground={false}
         additionalClass="shadow bg-white mb-20 rounded-b-lg">
-        <div className="max-w-7xl mx-auto py-12 px-4 text-center sm:px-6 lg:px-8">
+        <div className="py-12 px-4 text-center sm:px-6 lg:px-8">
           <div className="space-y-8 sm:space-y-12">
             {list && list.length > 0 ? (
-              <ul className="mx-auto grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
+              <ul className="grid grid-cols-2 justify-center items-center gap-x-4 gap-y-8 sm:grid-cols-4 md:gap-x-6 lg:max-w-5xl lg:gap-x-8 lg:gap-y-12 xl:grid-cols-6">
                 {list &&
                   list.length > 0 &&
                   list.slice(0, viewMore ? list.length - 1 : 12).map(
