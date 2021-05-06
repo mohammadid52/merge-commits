a3b8efea944c787d260294acbb54f2da9c3f522c
samboosa5k <jasper.verbon@gmail.com>
2021-05-06 12:22:41 +0200

    now only single highlighter and better highlight color

--- src/components/Lesson/LessonComponents/LyricsPage/LyricsModules/LyricsActivity.tsx
+++ src/components/Lesson/LessonComponents/LyricsPage/LyricsModules/LyricsActivity.tsx
@@ -32,7 +32,7 @@ export interface FinalText {
 }
 
 const Body = () => {
-  const { state, theme, dispatch } = useContext(LessonContext);
+  const { state, dispatch } = useContext(LessonContext);
   const [color, setColor] = useState('');
   const [selected, setSelected] = useState<Array<SelectObject>>(
     state.componentState.lyrics && state.componentState.lyrics.selected
@@ -41,8 +41,8 @@ const Body = () => {
   );
   const [cookies, setCookie] = useCookies([`lesson-${state.syllabusLessonID}`]);
   const [fullscreen, setFullscreen] = useState(false);
-  const { video, link } = state.data.lesson.coreLesson.instructions;
-  const [openPopup, setOpenPopup] = useState(false);
+
+
   //  text
   const [finalText, setFinalText] = useState<FinalText>({});
   const [initialSelectedText, setInitialSelectedText] = useState<SelectedTextGroup>({});
@@ -157,21 +157,21 @@ const Body = () => {
     }
   }, [selected]);
 
-  const colorPicker = (colorName: string): string => {
-    switch (colorName) {
-      case 'dark-red':
-        return '#CA2222';
-      case 'blueberry':
-        return '#488AC7';
-      case 'sea-green':
-        return '#17A589';
-      case 'fire-orange':
-        return '#FF5733';
-      case 'erase':
-      default:
-        return '';
-    }
-  };
+  // const colorPicker = (colorName: string): string => {
+  //   switch (colorName) {
+  //     case 'dark-red':
+  //       return '#CA2222';
+  //     case 'blueberry':
+  //       return '#488AC7';
+  //     case 'sea-green':
+  //       return '#17A589';
+  //     case 'fire-orange':
+  //       return '#FF5733';
+  //     case 'erase':
+  //     default:
+  //       return '';
+  //   }
+  // };
 
   /**
    * COOKIE loading for previously highlighted text WOOO!!!
@@ -190,12 +190,11 @@ const Body = () => {
             <div className='z-50 flex flex-col justify-between items-center'>
               <InstructionBlock />
             </div>
-            {/* <Toolbar setColor={setColor} color={color} /> */}
             <VideoBlock link={state.data.lesson.coreLesson.content.link} fullscreen={fullscreen} />
           </div>
 
           <div className='w-7/10 h-full max-w-256 flex flex-col items-start z-50'>
-            <Toolbar setColor={setColor} color={color} colorPicker={colorPicker} />
+            <Toolbar setColor={setColor} color={color} />
 
             <div
               ref={ref}
@@ -203,7 +202,6 @@ const Body = () => {
               style={{ height: `${winSize.height - toptop - 12}px` }}>
               <LyricsBlock
                 color={color}
-                colorPicker={colorPicker}
                 selected={selected}
                 setSelected={setSelected}
                 fullscreen={fullscreen}
