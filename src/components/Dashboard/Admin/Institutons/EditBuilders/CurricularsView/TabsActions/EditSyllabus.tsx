b13376be20ea71f81d8931e663f7936900f4c999
sublimedatasys <sahilshah@sublimedatasys.com>
2021-05-07 11:57:29 +0530

    fix/sort-lessons: Updated delete id to uniqLessonId

--- src/components/Dashboard/Admin/Institutons/EditBuilders/CurricularsView/TabsActions/EditSyllabus.tsx
+++ src/components/Dashboard/Admin/Institutons/EditBuilders/CurricularsView/TabsActions/EditSyllabus.tsx
@@ -312,7 +312,7 @@ const EditSyllabus = (props: EditSyllabusProps) => {
           closeLessonAction();
           const result: any = await API.graphql(
             graphqlOperation(mutations.deleteSyllabusLesson, {
-              input: {id: item.id},
+              input: {id: item.uniqlessonId},
             })
           );
 
