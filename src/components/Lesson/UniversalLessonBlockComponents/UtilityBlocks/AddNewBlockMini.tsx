04a7fd0b8d603f8b166fa68d8653e473212e63c0
samboosa5k <jasper.verbon@gmail.com>
2021-05-20 18:23:02 +0200

    added hover effects to ui

--- src/components/Lesson/UniversalLessonBlockComponents/UtilityBlocks/AddNewBlockMini.tsx
+++ src/components/Lesson/UniversalLessonBlockComponents/UtilityBlocks/AddNewBlockMini.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
-import { RowWrapperProps } from '../../../../interfaces/UniversalLessonBuilderInterfaces';
+import {RowWrapperProps} from '../../../../interfaces/UniversalLessonBuilderInterfaces';
 
 export const AddNewBlockMini = (props: RowWrapperProps) => {
-  const {
-    mode,
-    hasContent,
-    contentID,
-    hoveredID,
-    handleModalPopToggle,
-  } = props;
+  const {mode, hasContent, contentID, hoveredID, handleModalPopToggle} = props;
 
   return (
     <div
       className={`
+      group
       w-full
       flex flex-col justify-center items-center
       z-50`}>
-      <div
+      <button
         onClick={() => handleModalPopToggle('ADD_CONTENT')}
-        className={`w-auto cursor-pointer bg-gray-600 rounded-full z-50`}>
-        <p className={`p-2 text-xs text-center`}>Add New Block</p>
-      </div>
-        <div className={`absolute border-b-0 border-dashed border-blue-400`}/>
+        className={`
+        w-auto 
+        p-2
+        cursor-pointer 
+        text-xs text-center text-gray-600 group-hover:text-gray-200
+        bg-gray-600 bg-opacity-20 
+        group-hover:bg-opacity-95 
+        rounded-full z-50`}>
+        Add New Block
+      </button>
+      <div
+        className={`absolute border-b-0 border-dashed border-blue-400 border-opacity-20 group-hover:border-opacity-80`}
+      />
     </div>
   );
 };
