a349e28500d72c525574b18942de6ed57784909f
Mohammad <mohammad@sublimedatasys.com>
2021-05-22 18:20:48 +0530

    format-builder: Now can go to preview form without saving

--- src/components/Molecules/ModalPopUp.tsx
+++ src/components/Molecules/ModalPopUp.tsx
@@ -1,36 +1,64 @@
-import React from 'react'
+import React from 'react';
 import Modal from '../Atoms/Modal';
 import Buttons from '../Atoms/Buttons';
 
 interface ModalProps {
-  saveAction?: () => void
-  closeAction?: () => void
-  message?: string
-  saveLabel?: string
-  onlyInfo?: boolean
-  deleteModal?: boolean
-  deleteLabel?: string
-  cancelLabel?: string
+  saveAction?: () => void;
+  closeAction?: () => void;
+  message?: string;
+  saveLabel?: string;
+  onlyInfo?: boolean;
+  deleteModal?: boolean;
+  deleteLabel?: string;
+  cancelLabel?: string;
 }
 
 const ModalPopUp = (props: ModalProps) => {
-  const { saveAction, closeAction, message, saveLabel, onlyInfo, deleteModal, deleteLabel, cancelLabel } = props;
+  const {
+    saveAction,
+    closeAction,
+    message,
+    saveLabel,
+    onlyInfo,
+    deleteModal,
+    deleteLabel,
+    cancelLabel,
+  } = props;
   return (
-    <Modal showHeader={true} showHeaderBorder={false} showFooter={false} closeAction={closeAction}>
+    <Modal
+      showHeader={true}
+      showHeaderBorder={false}
+      showFooter={false}
+      closeAction={closeAction}>
       <div className="w-168 text-center my-8">
         <p className="my-4 px-6 text-gray-800 text-lg font-medium leading-8">{message}</p>
-        <div className="flex justify-around mt-16 w-5/10 mx-auto">
-          {!onlyInfo &&
-            <Buttons label={cancelLabel ? cancelLabel : "Cancel"} btnClass='px-8 py-3 mr-4' onClick={closeAction} transparent />
-          }
-          {deleteModal ? <Buttons label={deleteLabel ? deleteLabel : 'Delete'} btnClass='px-10 py-3 ml-4 text-red-600 border-red-600 hover:bg-gray-100 hover:text-red-500' onClick={saveAction} transparent />
-            :
-            <Buttons label={saveLabel ? saveLabel : "Save"} btnClass='px-10 py-3 ml-4' onClick={saveAction} />
-          }
+        <div className="flex justify-center mt-16 w-full mx-auto">
+          {!onlyInfo && (
+            <Buttons
+              label={cancelLabel ? cancelLabel : 'Cancel'}
+              btnClass="px-8 py-3 mr-4"
+              onClick={closeAction}
+              transparent
+            />
+          )}
+          {deleteModal ? (
+            <Buttons
+              label={deleteLabel ? deleteLabel : 'Delete'}
+              btnClass="px-10 py-3 ml-4 text-red-600 border-red-600 hover:bg-gray-100 hover:text-red-500"
+              onClick={saveAction}
+              transparent
+            />
+          ) : (
+            <Buttons
+              label={saveLabel ? saveLabel : 'Save'}
+              btnClass="px-10 py-3 ml-4"
+              onClick={saveAction}
+            />
+          )}
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
 
-export default ModalPopUp
+export default ModalPopUp;
