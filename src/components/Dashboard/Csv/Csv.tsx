02f23b18c1594c292fc1202a00eb630c2a7e337a
aman-sds <aman@sublimedatasys.com>
2021-05-28 16:01:36 +0530

    csv changes

--- src/components/Dashboard/Csv/Csv.tsx
+++ src/components/Dashboard/Csv/Csv.tsx
@@ -1,21 +1,21 @@
-import React, {useEffect, useState, useContext} from 'react';
-import {GlobalContext} from '../../../contexts/GlobalContext';
-import API, {graphqlOperation} from '@aws-amplify/api';
+import React, { useEffect, useState, useContext } from 'react';
+import { GlobalContext } from '../../../contexts/GlobalContext';
+import API, { graphqlOperation } from '@aws-amplify/api';
 import * as customQueries from '../../../customGraphql/customQueries';
 import Selector from '../../Atoms/Form/Selector';
-import {createFilterToFetchSpecificItemsOnly} from '../../../utilities/strings';
-import {CSVLink} from 'react-csv';
+import { createFilterToFetchSpecificItemsOnly } from '../../../utilities/strings';
+import { CSVLink } from 'react-csv';
 import DateAndTime from '../DateAndTime/DateAndTime';
-import {getAsset} from '../../../assets';
+import { getAsset } from '../../../assets';
 import SectionTitleV3 from '../../Atoms/SectionTitleV3';
 import useDictionary from '../../../customHooks/dictionary';
-import {uniqBy} from 'lodash';
+import { uniqBy } from 'lodash';
 
-interface Csv {}
+interface Csv { }
 
 const Csv = (props: Csv) => {
-  const {state, theme, dispatch, clientKey, userLanguage} = useContext(GlobalContext);
-  const {CsvDict} = useDictionary(clientKey);
+  const { state, theme, dispatch, clientKey, userLanguage } = useContext(GlobalContext);
+  const { CsvDict } = useDictionary(clientKey);
   const [institutions, setInstitutions] = useState([]);
   const [selectedInst, setSelectedInst] = useState(null);
 
@@ -51,6 +51,8 @@ const Csv = (props: Csv) => {
   const [unitsLoading, setUnitsLoading] = useState(false);
   const [surveysLoading, setSurveysLoading] = useState(false);
   const [csvGettingReady, setCsvGettingReady] = useState(false);
+  const [surveyDates, setSurveyDates] = useState({ start: '', last: '' });
+  const [studentsWithNoSurveyAnswers, setStudentsWithNoSurveyAnswers] = useState([]);
 
   // methods to clear state data
 
@@ -114,7 +116,7 @@ const Csv = (props: Csv) => {
     setClassRoomLoading(true);
     try {
       let sInst = selectedInst;
-      let inst = {id, name, value};
+      let inst = { id, name, value };
       setSelectedInst(inst);
       if (!sInst || sInst.id !== inst.id) {
         resetInstitution();
@@ -131,12 +133,12 @@ const Csv = (props: Csv) => {
             cr.curricula?.items && Array.isArray(cr.curricula?.items)
               ? cr.curricula?.items[0].curriculum
               : null;
-          instCRs.push({id: cr.id, name: cr.name, value: cr.name});
+          instCRs.push({ id: cr.id, name: cr.name, value: cr.name });
           return {
             id: cr.id,
             name: cr.name,
             value: cr.name,
-            class: {...cr.class},
+            class: { ...cr.class },
             curriculum,
           };
         });
@@ -154,7 +156,7 @@ const Csv = (props: Csv) => {
   const onClassRoomSelect = async (id: string, name: string, value: string) => {
     try {
       let sCR = selectedClassRoom;
-      let cr = {id, name, value};
+      let cr = { id, name, value };
       setSelectedClassRoom(cr);
       if (!sCR || sCR.id !== cr.id) {
         let classroom = classRoomsList.filter((c) => c.id === cr.id)[0];
@@ -183,7 +185,7 @@ const Csv = (props: Csv) => {
       );
       let units = curriculumData?.data.getCurriculum?.syllabi?.items || [];
       units = units.map((syl: any) => {
-        return {id: syl.id, name: syl.name, value: syl.name};
+        return { id: syl.id, name: syl.name, value: syl.name };
       });
       setUnits(units);
       let curricularCheckpoints =
@@ -194,7 +196,7 @@ const Csv = (props: Csv) => {
         cCheckpoints.push(cc.checkpoint.id);
         let questions = cc.checkpoint?.questions?.items || [];
         questions.map((q: any) => {
-          demographicsQues.push({question: q.question, checkpointID: cc.checkpoint.id});
+          demographicsQues.push({ question: q.question, checkpointID: cc.checkpoint.id });
         });
       });
       setUnitsLoading(false);
@@ -215,7 +217,7 @@ const Csv = (props: Csv) => {
       graphqlOperation(customQueries.getStudentResponse, {
         filter: {
           ...createFilterToFetchSpecificItemsOnly(checkpointIds, 'checkpointID'),
-          syllabusLessonID: {eq: syllabusLessonID},
+          syllabusLessonID: { eq: syllabusLessonID },
           ...createFilterToFetchSpecificItemsOnly(studentsEmails, 'email'),
         },
       })
@@ -226,7 +228,7 @@ const Csv = (props: Csv) => {
   };
 
   const onUnitSelect = (id: string, name: string, value: string) => {
-    let unit = {id, name, value};
+    let unit = { id, name, value };
     setSelectedUnit(unit);
     fetchSurveys(unit.id);
   };
@@ -283,7 +285,7 @@ const Csv = (props: Csv) => {
   };
 
   const onSurveySelect = async (id: string, name: string, value: string) => {
-    let survey = {id, name, value};
+    let survey = { id, name, value };
     setSelectedsurvey(survey);
     await listQuestions(survey.id);
   };
@@ -302,7 +304,7 @@ const Csv = (props: Csv) => {
         cCheckpoints.push(cp.checkpointID);
         let ques = cp?.checkpoint?.questions?.items;
         ques.map((q: any) => {
-          questions.push({question: q.question, checkpointID: cp.checkpointID});
+          questions.push({ question: q.question, checkpointID: cp.checkpointID });
         });
       });
       setSurveyQuestions(questions);
@@ -328,7 +330,7 @@ const Csv = (props: Csv) => {
         graphqlOperation(customQueries.getStudentResponse, {
           filter: {
             ...createFilterToFetchSpecificItemsOnly(checkpointIds, 'checkpointID'),
-            syllabusLessonID: {eq: syllabusLessonID},
+            syllabusLessonID: { eq: syllabusLessonID },
             ...createFilterToFetchSpecificItemsOnly(studsEmails, 'email'),
           },
         })
@@ -346,10 +348,12 @@ const Csv = (props: Csv) => {
     setCsvGettingReady(true);
     let students = classStudents;
     let qids: any = [];
+    let stdsWithNoSurveyAnswers: any = [];
     let surveyQuestionHeaders = surveyQuestions.map((ques: any) => {
       qids.push(ques.question.id);
-      return {label: `${ques.question.question}`, key: `${ques.question.id}`};
+      return { label: `${ques.question.question}`, key: `${ques.question.id}` };
     });
+    let surveyAnswerDates: any = [];
 
     /* Enable this code if demographics questions */
 
@@ -362,28 +366,29 @@ const Csv = (props: Csv) => {
     });
 
     setCSVHeaders([
-      {label: 'AuthId', key: 'authId'},
-      {label: 'Email', key: 'email'},
-      {label: 'First Name', key: 'firstName'},
-      {label: 'Last Name', key: 'lastName'},
-      {label: 'Institute', key: 'institute'},
-      {label: 'Curriculum', key: 'curriculum'},
-      {label: 'Unit', key: 'unit'},
-      {label: 'Classroom', key: 'classroom'},
-      {label: 'Survey name', key: 'surveyName'},
+      { label: 'AuthId', key: 'authId' },
+      { label: 'Email', key: 'email' },
+      { label: 'First Name', key: 'firstName' },
+      { label: 'Last Name', key: 'lastName' },
+      { label: 'Institute', key: 'institute' },
+      { label: 'Curriculum', key: 'curriculum' },
+      { label: 'Unit', key: 'unit' },
+      { label: 'Classroom', key: 'classroom' },
+      { label: 'Survey name', key: 'surveyName' },
       ...demographicsQuestionHeaders, // Enable this line for demographics question
       ...surveyQuestionHeaders,
     ]);
 
     let data = students.map((stu: any) => {
       let studentAnswers: any = {};
-      const dates: any[] = [];
+      let hasTakenSurvey = false;
 
       SCQAnswers.map((ans: any) => {
         if (ans.person.id === stu.id) {
+          hasTakenSurvey = true;
           ans.responseObject.map((resp: any) => {
             if (qids.indexOf(resp.qid) >= 0) {
-              dates.push(ans.updatedAt);
+              surveyAnswerDates.push(ans.updatedAt);
               studentAnswers[resp.qid] =
                 Array.isArray(resp.response) && resp.response.length
                   ? resp.response[0]
@@ -392,6 +397,9 @@ const Csv = (props: Csv) => {
           });
         }
       });
+      if (!hasTakenSurvey) {
+        stdsWithNoSurveyAnswers.push(stu)
+      }
 
       /* Enable this code if demographics questions */
       DCQAnswers.map((ans: any) => {
@@ -408,8 +416,7 @@ const Csv = (props: Csv) => {
       });
 
       // @ts-ignore
-      const sortedDates = dates.sort((a: any, b: any) => new Date(b) - new Date(a));
-
+      surveyAnswerDates = surveyAnswerDates.sort((a: any, b: any) => new Date(b) - new Date(a));
       return {
         ...stu,
         institute: selectedInst.name,
@@ -419,13 +426,14 @@ const Csv = (props: Csv) => {
         surveyName: selectedSurvey.name,
         ...studentAnswers,
         first:
-          (sortedDates[sortedDates.length - 1] &&
-            new Date(sortedDates[sortedDates.length - 1]).toLocaleString('en-US')) ||
+          (surveyAnswerDates[surveyAnswerDates.length - 1] &&
+            new Date(surveyAnswerDates[surveyAnswerDates.length - 1]).toLocaleString('en-US')) ||
           '-',
-        last: (sortedDates[0] && new Date(sortedDates[0]).toLocaleString('en-US')) || '-',
+        last: (surveyAnswerDates[0] && new Date(surveyAnswerDates[0]).toLocaleString('en-US')) || '-',
       };
     });
-
+    setSurveyDates({ last: surveyAnswerDates[0] || '', start: surveyAnswerDates[surveyAnswerDates.length - 1] || '' })
+    setStudentsWithNoSurveyAnswers(stdsWithNoSurveyAnswers);
     setCSVData(data);
     setIsCSVDownloadReady(true);
     setCsvGettingReady(false);
@@ -462,25 +470,25 @@ const Csv = (props: Csv) => {
               <table className="min-w-full divide-y divide-gray-200">
                 <thead className="bg-gray-100">
                   <tr>
-                    <th scope="col" style={{width: '15%'}} className={theadStyles}>
-                      Institute Name
+                    <th scope="col" style={{ width: '15%' }} className={theadStyles}>
+                      Id
                     </th>
-                    <th scope="col" style={{width: '20%'}} className={theadStyles}>
-                      Curriculum
+                    <th scope="col" style={{ width: '20%' }} className={theadStyles}>
+                      first name
                     </th>
-                    <th scope="col" style={{width: '15%'}} className={theadStyles}>
-                      Unit Name
+                    <th scope="col" style={{ width: '15%' }} className={theadStyles}>
+                      last Name
                     </th>
-                    <th scope="col" style={{width: '20%'}} className={theadStyles}>
+                    <th scope="col" style={{ width: '20%' }} className={theadStyles}>
                       Classroom
                     </th>
-                    <th scope="col" style={{width: '20%'}} className={theadStyles}>
-                      Survey Name
+                    <th scope="col" style={{ width: '20%' }} className={theadStyles}>
+                      email
                     </th>
-                    <th scope="col" style={{width: '20%'}} className={theadStyles}>
+                    <th scope="col" style={{ width: '20%' }} className={theadStyles}>
                       Start
                     </th>
-                    <th scope="col" style={{width: '20%'}} className={theadStyles}>
+                    <th scope="col" style={{ width: '20%' }} className={theadStyles}>
                       End
                     </th>
                   </tr>
@@ -488,26 +496,26 @@ const Csv = (props: Csv) => {
                 <tbody>
                   {CSVData.map((listItem, idx) => (
                     <tr key={idx} className={idx % 2 === 0 ? 'bg-white' : 'bg-gray-100'}>
-                      <td style={{width: '15%'}} className={tdataStyles}>
+                      <td style={{ width: '15%' }} className={tdataStyles}>
                         {listItem.institute}
                       </td>
-                      <td style={{width: '20%'}} className={tdataStyles}>
+                      <td style={{ width: '20%' }} className={tdataStyles}>
                         {listItem.curriculum}
                       </td>
-                      <td style={{width: '15%'}} className={tdataStyles}>
+                      <td style={{ width: '15%' }} className={tdataStyles}>
                         {listItem.unit}
                       </td>
-                      <td style={{width: '20%'}} className={tdataStyles}>
+                      <td style={{ width: '20%' }} className={tdataStyles}>
                         {listItem.classroom}
                       </td>
-                      <td style={{width: '20%'}} className={tdataStyles}>
+                      <td style={{ width: '20%' }} className={tdataStyles}>
                         {listItem.surveyName}
                       </td>
 
-                      <td style={{width: '10%'}} className={tdataStyles}>
+                      <td style={{ width: '10%' }} className={tdataStyles}>
                         {listItem.first}
                       </td>
-                      <td style={{width: '10%'}} className={tdataStyles}>
+                      <td style={{ width: '10%' }} className={tdataStyles}>
                         {listItem.last}
                       </td>
                     </tr>
@@ -609,6 +617,30 @@ const Csv = (props: Csv) => {
           </div>
         )}
       </div>
+      {
+        isCSVDownloadReady &&
+        <div>
+          <p>Institution name {selectedInst.name} </p>
+          <p>Class room name {selectedClassRoom.name} </p>
+          <p>Curriculum {selectedCurriculum.name} </p>
+          <p>Unit name {selectedUnit.name} </p>
+          <p>Survey name: {selectedSurvey ? selectedSurvey.name : ''} </p>
+          <p>Survey first: {surveyDates.start} </p>
+          <p>Survey last: {surveyDates.last} </p>
+          <p>Total students: {classStudents.length} </p>
+          <p>Students who havent participated in the survey: {studentsWithNoSurveyAnswers.length} </p>
+          <>
+            <div>
+              {
+                studentsWithNoSurveyAnswers.map(item => {
+                  return <p>{item.email}</p>
+                })
+              }
+            </div>
+          </>
+
+        </div>
+      }
 
       {/* <span> ====**===========**======= ====**===========**======= </span>
       {selectedUnit ? (
