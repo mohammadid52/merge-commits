023eaa23d937e5746c6912470451e245a577765d
Mohammad Dehgamwala <mohammad@sublimedatasys.com>
2021-06-01 21:23:10 +0530

    new-dev: Updated dictionary #1

--- src/assets.tsx
+++ src/assets.tsx
@@ -71,7 +71,7 @@ const curate_assets: any = {
   dashboardBanner1:
     'https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
   dashboardBanner2:
-    'https://images.unsplash.com/photo-1604134967494-8a9ed3adea0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80',
+    'https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80',
 };
 
 const demo_assets: any = {
