5dd50d15cba9745eaba31fe751e24cb7afe6cea7
Mohammad <mohammad@sublimedatasys.com>
2021-06-08 18:22:42 +0530

    ulb/header-component: Optimized Code

--- src/components/Lesson/UniversalLessonBuilder/views/BuilderWrapper.tsx
+++ src/components/Lesson/UniversalLessonBuilder/views/BuilderWrapper.tsx
@@ -26,7 +26,7 @@ import {EditQuestionModalDict} from '../../../../dictionary/dictionary.iconoclas
 import {uniqueId} from 'lodash';
 import Selector from '../../../Atoms/Form/Selector';
 import ColorPicker from '../UI/ColorPicker/ColorPicker';
-
+import HeaderModalComponent from '../UI/FormElements/Header';
 interface ExistingLessonTemplateProps extends ULBSelectionProps {
   mode?: 'building' | 'viewing';
   universalBuilderStep?: string;
@@ -146,38 +146,7 @@ const BuilderWrapper = (props: ExistingLessonTemplateProps) => {
     });
   };
 
-  const getValue = (id: string) => inputFields[id];
   const closeAction = () => setAddContentModal({type: '', show: false});
-
-  const onSave = (fieldId: string) => {
-    const value: string = getValue(fieldId);
-    const pageContentId: string = uniqueId(`${selectedPageID}_`);
-    const partContentId: string = uniqueId(`${pageContentId}_`);
-    const fontSizeClass: string = convertSizeNameToClass(selectedValues.size);
-    const bgColorClass: string = selectedValues.color;
-    const newDataObject = {
-      id: pageContentId,
-      partType: 'default',
-      class: 'rounded-lg',
-      partContent: [
-        {
-          id: partContentId,
-          type: 'header-section',
-          value: [value],
-          class: `${fontSizeClass} border-${bgColorClass}`,
-        },
-      ],
-    };
-    // add data to list
-    addFromULBHandler(selectedPageID, newDataObject);
-    // close modal after saving
-    closeAction();
-    // clear fields
-    setInputFields({
-      ...inputFields,
-      [fieldId]: '',
-    });
-  };
 
   function capitalizeFirstLetter(str: string = '') {
     if (str.length > 0) {
@@ -186,40 +155,26 @@ const BuilderWrapper = (props: ExistingLessonTemplateProps) => {
     }
   }
 
-  const fontSizeList = [
-    {id: 1, name: 'smallest'},
-    {id: 2, name: 'small'},
-    {id: 3, name: 'medium'},
-    {id: 4, name: 'large'},
-    {id: 5, name: 'largest'},
-  ];
+  const modalByType = (type: 'header' | string) => {
+    switch (type) {
+      case 'header':
+        return (
+          <HeaderModalComponent
+            inputValue={inputFields[type]}
+            onChange={onChange}
+            selectedPageID={selectedPageID}
+            setInputFields={setInputFields}
+            inputFields={inputFields}
+            addFromULBHandler={addFromULBHandler}
+            closeAction={closeAction}
+          />
+        );
 
-  const [selectedValues, setSelectedValues] = useState({
-    size: 'medium',
-    color: 'sea-green',
-  });
-
-  const convertSizeNameToClass = (sizeName: string) => {
-    switch (sizeName) {
-      case 'smallest':
-        return 'text-base';
-      case 'small':
-        return 'text-lg';
-      case 'medium':
-        return 'text-xl';
-      case 'large':
-        return 'text-2xl';
-      case 'largest':
-        return 'text-3xl';
       default:
-        return 'text-xl';
+        break;
     }
   };
-  const [colorPickerActive, setColorPickerActive] = useState<boolean>(false);
-  const handleColorPickerSelect = (pickedColor: string) => {
-    setSelectedValues({...selectedValues, color: pickedColor});
-    setColorPickerActive(false);
-  };
+
   return (
     <div
       id={`builderWrapper`}
@@ -259,57 +214,7 @@ const BuilderWrapper = (props: ExistingLessonTemplateProps) => {
           showHeaderBorder={true}
           showFooter={false}
           closeAction={closeAction}>
-          <div className="min-w-256">
-            <div className="grid grid-cols-2 my-2 gap-4">
-              <div className="col-span-2">
-                <FormInput
-                  onChange={onChange}
-                  label={capitalizeFirstLetter(addContentModal.type)}
-                  isRequired
-                  value={getValue(addContentModal.type)}
-                  id={addContentModal.type}
-                  placeHolder={`Enter ${addContentModal.type}`}
-                  type="text"
-                />
-              </div>
-              <Selector
-                onChange={(c: any, name: string) =>
-                  setSelectedValues({...selectedValues, size: name})
-                }
-                list={fontSizeList}
-                placeholder="Select font size"
-                selectedItem={selectedValues.size}
-              />
-              <button
-                onClick={() => setColorPickerActive(!colorPickerActive)}
-                className={`border-0 border-gray-300 rounded shadow-xs flex items-center justify-center`}>
-                <span className={'text-gray-700 w-auto text-sm mr-2'}>
-                  Select Border Color{' '}
-                </span>
-
-                <span
-                  className={`h-4 block w-4 bg-${selectedValues.color} rounded-full`}></span>
-              </button>
-              {colorPickerActive && (
-                <ColorPicker classString={''} callbackColor={handleColorPickerSelect} />
-              )}
-            </div>
-            <div className="flex mt-8 justify-center px-6 pb-4">
-              <div className="flex justify-end">
-                <Buttons
-                  btnClass="py-1 px-4 text-xs mr-2"
-                  label={EditQuestionModalDict[userLanguage]['BUTTON']['CANCEL']}
-                  onClick={closeAction}
-                  transparent
-                />
-                <Buttons
-                  btnClass="py-1 px-8 text-xs ml-2"
-                  label={EditQuestionModalDict[userLanguage]['BUTTON']['SAVE']}
-                  onClick={() => onSave(addContentModal.type)}
-                />
-              </div>
-            </div>
-          </div>
+          <div className="min-w-256">{modalByType(addContentModal.type)}</div>
         </Modal>
       )}
 
