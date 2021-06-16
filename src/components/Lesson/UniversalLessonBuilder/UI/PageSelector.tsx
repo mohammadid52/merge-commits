0302b5d9dc8881068d83083f0118f3f0422c326e
Mohammad <mohammad@sublimedatasys.com>
2021-06-16 19:13:02 +0530

    Added: move page left and right arrows feature

--- src/components/Lesson/UniversalLessonBuilder/UI/PageSelector.tsx
+++ src/components/Lesson/UniversalLessonBuilder/UI/PageSelector.tsx
@@ -9,8 +9,11 @@ import {IconContext} from 'react-icons';
 import PageTile from './common/PageTile';
 import {ULBSelectionProps} from '../../../../interfaces/UniversalLessonBuilderInterfaces';
 import {HiPencil} from 'react-icons/hi';
-import {AiOutlineDelete} from 'react-icons/ai';
+import {AiOutlineDelete, AiOutlineLeft, AiOutlineRight} from 'react-icons/ai';
 import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import {reorder} from '../../../../utilities/strings';
+import {update} from 'lodash';
+import {useULBContext} from '../../../../contexts/UniversalLessonBuilderContext';
 
 interface PageSelectorProps extends ULBSelectionProps {
   universalLessonDetails: UniversalLesson;
@@ -55,6 +58,8 @@ const PageSelector = (props: PageSelectorProps) => {
     }
   };
 
+  const {setUniversalLessonDetails} = useULBContext();
+
   const DroppablePages = () => {
     const [movablePagesList, setMovablePagesList] = useState(pages);
     const handleOnDragEnd = (result: any) => {
@@ -65,61 +70,116 @@ const PageSelector = (props: PageSelectorProps) => {
       items.splice(result.destination.index, 0, reorderedItem);
       setMovablePagesList(items);
     };
+    const [movingPage, setMovingPage] = useState(null);
+
     return (
       <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="partContent" direction="horizontal">
+        <Droppable isDropDisabled={true} droppableId="partContent" direction="horizontal">
           {(provided) => (
             <ul
               {...provided.droppableProps}
               ref={provided.innerRef}
               className="flex w-auto items-center">
               {movablePagesList.map((page: UniversalLessonPage, idx: number) => (
-                <Draggable draggableId={page.id} index={idx} key={page.id}>
-                  {(provided) => (
-                    <li
-                      key={`pageSelector_${idx}`}
-                      id={`pageThumb_${page.id}`}
-                      onClick={() => handleSelectPage(page.id)}
-                      className={`${
-                        selectedPageID === page.id ? 'bg-gray-300' : ''
-                      } w-auto ml-4 flex flex-col cursor-pointer border-0 hover:bg-gray-300 p-2 px-6 rounded-md transition-all duration-300`}
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}>
-                      <PageTile />
-                      <p className={`text-center text-sm text-gray-600`}>{page.id}</p>
-                      <div className="flex items-center justify-between">
-                        <button
-                          onClick={() => deleteFromULBHandler(page.id)}
-                          className={`mr-1 text-center border-red-200 border-3 hover:bg-red-200  text-xs font-semibold  p-1 cursor-pointer rounded mt-2`}>
-                          <AiOutlineDelete
-                            className="hover:text-red-600 text-red-400"
-                            size={18}
-                          />
-                        </button>
-                        <button
-                          onClick={(e: any) => {
-                            e.stopPropagation();
-                            setEditModal({
-                              show: true,
-                              content: {
-                                id: page.id,
-                                title: page.title,
-                                description: page.description,
-                                label: page.label,
-                              },
-                              editOnlyId: false,
-                            });
-                          }}
-                          className={`text-center text-xs font-semibold border-indigo-200  border-3 hover:bg-indigo-200 p-1 cursor-pointer rounded mt-2`}>
-                          <HiPencil
-                            className="hover:text-indigo-400 text-indigo-400"
-                            size={18}
-                          />
-                        </button>
-                      </div>
-                    </li>
-                  )}
+                <Draggable
+                  isDragDisabled={true}
+                  draggableId={page.id}
+                  index={idx}
+                  key={page.id}>
+                  {(provided) => {
+                    const DISABLE_PREV = idx === 0;
+                    const DISABLE_NEXT = idx === movablePagesList.length - 1;
+                    const isMoving = movingPage === idx;
+                    const PATH_TO_PAGE = `lessonPlan`;
+
+                    const moveRight = (idx: number) => {
+                      setMovingPage(idx);
+                      const modifiedArr = reorder(pages, idx, idx + 1);
+                      update(universalLessonDetails, PATH_TO_PAGE, () => modifiedArr);
+                      setUniversalLessonDetails({...universalLessonDetails});
+                      setMovingPage(null);
+                    };
+                    const moveLeft = (idx: number) => {
+                      setMovingPage(idx);
+                      const modifiedArr = reorder(pages, idx, idx - 1);
+                      update(universalLessonDetails, PATH_TO_PAGE, () => modifiedArr);
+                      setUniversalLessonDetails({...universalLessonDetails});
+                      setMovingPage(null);
+                    };
+                    return (
+                      <li
+                        key={`pageSelector_${idx}`}
+                        id={`pageThumb_${page.id}`}
+                        onClick={() => handleSelectPage(page.id)}
+                        className={`${
+                          selectedPageID === page.id || isMoving ? 'bg-gray-300' : ''
+                        } w-auto ml-4 flex flex-col cursor-default border-0 hover:bg-gray-300 p-2 px-6 rounded-md transition-all duration-300`}
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}>
+                        <div className="flex items-center justify-center mb-2">
+                          <button
+                            onClick={(e: any) => {
+                              e.stopPropagation();
+                              moveLeft(idx);
+                            }}
+                            disabled={DISABLE_PREV}
+                            className={`mr-1 text-center ${
+                              DISABLE_PREV
+                                ? 'text-opacity-40'
+                                : 'hover:bg-gray-600 hover:text-white'
+                            }   text-xs font-semibold  p-1 cursor-pointer rounded mt-2  text-gray-500 transition-all duration-300`}>
+                            <AiOutlineLeft className="" size={18} />
+                          </button>
+                          <button
+                            disabled={DISABLE_NEXT}
+                            onClick={(e: any) => {
+                              e.stopPropagation();
+                              moveRight(idx);
+                            }}
+                            className={`mr-1 text-center ${
+                              DISABLE_NEXT
+                                ? 'text-opacity-40'
+                                : 'hover:bg-gray-600 hover:text-white'
+                            }   text-xs font-semibold  p-1 cursor-pointer rounded mt-2  text-gray-500 transition-all duration-300`}>
+                            <AiOutlineRight className="" size={18} />
+                          </button>
+                        </div>
+                        <PageTile />
+                        <p className={`text-center text-sm text-gray-600`}>{page.id}</p>
+                        <div className="flex items-center justify-between">
+                          <button
+                            onClick={() => deleteFromULBHandler(page.id)}
+                            className={`mr-1 text-center border-red-200 border-3 hover:bg-red-200  text-xs font-semibold  p-1 cursor-pointer rounded mt-2`}>
+                            <AiOutlineDelete
+                              className="hover:text-red-600 text-red-400"
+                              size={18}
+                            />
+                          </button>
+                          <button
+                            onClick={(e: any) => {
+                              e.stopPropagation();
+                              setEditModal({
+                                show: true,
+                                content: {
+                                  id: page.id,
+                                  title: page.title,
+                                  description: page.description,
+                                  label: page.label,
+                                },
+                                editOnlyId: false,
+                              });
+                            }}
+                            className={`text-center text-xs font-semibold border-indigo-200  border-3 hover:bg-indigo-200 p-1 cursor-pointer rounded mt-2`}>
+                            <HiPencil
+                              className="hover:text-indigo-400 text-indigo-400"
+                              size={18}
+                            />
+                          </button>
+                        </div>
+                      </li>
+                    );
+                  }}
                 </Draggable>
               ))}
             </ul>
