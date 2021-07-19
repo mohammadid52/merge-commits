a762d9a330902bcf17a667304128b8011e9f351e
Mohammad <mohammad@sublimedatasys.com>
2021-07-19 20:26:44 +0530

    Added PIP for video blocks

--- src/components/Lesson/UniversalLessonBlockComponents/Blocks/ImageBlock.tsx
+++ src/components/Lesson/UniversalLessonBlockComponents/Blocks/ImageBlock.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import useOnScreen from '../../../../customHooks/useOnScreen';
 import {RowWrapperProps} from '../../../../interfaces/UniversalLessonBuilderInterfaces';
 import {getImageFromS3Static} from '../../../../utilities/services';
 
@@ -17,11 +18,63 @@ export const ImageBlock = (props: ImageBlockProps) => {
     width: width === 'auto' ? 'auto' : `${width}px`,
     height: height === 'auto' ? 'auto' : `${height}px`,
   };
+
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const videoRef = React.useRef(null);
+  const isVideoOnScreen = useOnScreen(videoRef);
+
+  const [showPIP, setShowPIP] = useState(false);
+  console.log('🚀 ~ file: ImageBlock.tsx ~ line 28 ~ ImageBlock ~ showPIP', showPIP);
+
+  useEffect(() => {
+    if (isPlaying) {
+      if (!isVideoOnScreen) {
+        setShowPIP(true);
+      } else {
+        setShowPIP(false);
+      }
+    }
+  }, [isVideoOnScreen, isPlaying]);
+
+  // if playing and if not on screen show pip
+  // if playing and on screen hide pip
+
+  useEffect(() => {
+    if (showPIP) {
+      // @ts-ignore
+      if (document?.pictureInPictureEnabled) {
+        videoRef?.current?.requestPictureInPicture();
+      }
+    } else {
+      // @ts-ignore
+      if (document.pictureInPictureElement) {
+        // @ts-ignore
+        document?.exitPictureInPicture();
+      }
+    }
+  }, [showPIP]);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.addEventListener('playing', () => {
+        setIsPlaying(true);
+      });
+      videoRef.current.addEventListener('pause', () => {
+        setIsPlaying(false);
+      });
+    }
+    return videoRef.current.addEventListener('pause', () => {
+      setIsPlaying(false);
+    });
+  }, [videoRef.current]);
+
   return (
     <div id={id} data-id={dataIdAttribute} className={`px-4 py-5 sm:p-6`}>
       {customVideo ? (
         <video
           width={width}
+          ref={videoRef}
           height={height}
           controls
           className="rounded-lg"
