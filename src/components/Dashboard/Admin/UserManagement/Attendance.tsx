edd2efbed88b0e1dfec74c4542424df94f53add7
Sonu <sonu@chapter247.com>
2021-07-22 14:11:13 +0530

    add search by date

--- src/components/Dashboard/Admin/UserManagement/Attendance.tsx
+++ src/components/Dashboard/Admin/UserManagement/Attendance.tsx
@@ -2,11 +2,23 @@ import React, {useEffect, useState} from 'react';
 import {FaArrowUp, FaArrowDown} from 'react-icons/fa';
 import API, {graphqlOperation} from '@aws-amplify/api';
 import orderBy from 'lodash/orderBy';
+import DatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 import * as queries from '../../../../graphql/queries';
 
+import Loader from '../../../Atoms/Loader';
+
+const pad = (num: any) => {
+  return `0${num}`.slice(-2);
+};
+
 const Attendance = ({id}: any) => {
-  const [AttendanceList, setAttendanceList] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [attendanceList, setAttendanceList] = useState<any>([]);
+  const [focused, setFocused] = useState(false);
+  const [date, setDate] = useState(null);
   const [sortConfig, setSortConfig] = useState<{
     fieldName: string;
     order: boolean | 'desc' | 'asc';
@@ -21,15 +33,36 @@ const Attendance = ({id}: any) => {
     }
   }, [id]);
 
-  const fetchAttendance = async () => {
-    const list: any = await API.graphql(
-      graphqlOperation(queries.listAttendances, {
-        filter: {
-          studentID: {eq: id},
-        },
-      })
-    );
-    setAttendanceList(list?.data.listAttendances?.items);
+  const fetchAttendance = async (date?: Date | null) => {
+    try {
+      setLoading(true);
+      let payload: any = {
+        studentID: id,
+        sortDirection: 'ASC',
+        date
+      };
+      if (date) {
+        const dayNumber = date.getDate();
+        const monthNumber = date.getMonth();
+        const year = date.getFullYear();
+        
+        payload.date = {
+          eq: `${year}-${pad(monthNumber + 1)}-${pad(dayNumber)}`,
+        };
+      }
+      const list: any = await API.graphql(
+        graphqlOperation(queries.attendanceByStudent, payload)
+      );
+      setAttendanceList(list?.data.attendanceByStudent?.items);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const handleDateChange = (date: Date | null) => {
+    setDate(date);
+    fetchAttendance(date);
   };
 
   const handleOrderBy = (fieldName: string, order: boolean | 'desc' | 'asc') => {
@@ -37,7 +70,7 @@ const Attendance = ({id}: any) => {
       fieldName,
       order,
     });
-    setAttendanceList(orderBy(AttendanceList, [fieldName], [order]));
+    setAttendanceList(orderBy(attendanceList, [fieldName], [order]));
   };
 
   const withOrderBy = (columnName: string, fieldName: string) => {
@@ -46,20 +79,20 @@ const Attendance = ({id}: any) => {
         <span className="w-auto">{columnName}</span>
         <span className="inline-flex items-center ml-1 cursor-pointer">
           <span
-            className={`w-2 h-2 ${
+            className={`w-auto ${
               fieldName === sortConfig.fieldName && sortConfig.order === 'desc'
                 ? 'text-dark-gray'
                 : ''
             }`}>
-            <FaArrowDown />
+            <FaArrowDown className="w-2" />
           </span>
           <span
-            className={`w-2 h-2 ${
+            className={`w-auto ${
               fieldName === sortConfig.fieldName && sortConfig.order === 'asc'
                 ? 'text-dark-gray'
                 : ''
             }`}>
-            <FaArrowUp />
+            <FaArrowUp className="w-2" />
           </span>
         </span>
       </span>
@@ -67,96 +100,130 @@ const Attendance = ({id}: any) => {
   };
 
   return (
-    <div className="flex flex-col">
-      <div className="overflow-x-auto">
-        <div className="align-middle inline-block min-w-full">
-          <div className=" overflow-hidden border-b-0 border-gray-200 sm:rounded-lg">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-100">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                    onClick={() =>
-                      handleOrderBy(
-                        'curriculum',
-                        sortConfig.fieldName === 'curriculum'
-                          ? sortConfig.order === 'desc'
-                            ? 'asc'
+    <div className="">
+      <div className="flex justify-end mb-2">
+        <div className="w-64 relative">
+          <DatePicker
+            dateFormat={'dd/MM/yyyy'}
+            selected={date}
+            placeholderText={'Search by date'}
+            onChange={handleDateChange}
+            className="text-dark-gray"
+            // isClearable={true}
+          />
+
+          {/* <SingleDatePicker
+                  date={date}
+                  displayFormat={'DD/MM/YYYY'}
+                  focused={focused}
+                  id={id}
+                  readOnly={true}
+                  // isOutsideRange={(day) => !isInclusivelyAfterDay(day, new Date())}
+                  numberOfMonths={1}
+                  onDateChange={(date) => console.log(date, 'date')}
+                  onFocusChange={({focused}) => setFocused(focused)}
+                /> */}
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto">
+          <div className="align-middle inline-block min-w-full">
+            <div className="overflow-hidden border-b-0 border-gray-200 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-100">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
+                      onClick={() =>
+                        handleOrderBy(
+                          'curriculum',
+                          sortConfig.fieldName === 'curriculum'
+                            ? sortConfig.order === 'desc'
+                              ? 'asc'
+                              : 'desc'
                             : 'desc'
-                          : 'desc'
-                      )
-                    }>
-                    {withOrderBy('Curriculum', 'curriculum')}
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                    onClick={() =>
-                      handleOrderBy(
-                        'syllabus',
-                        sortConfig.fieldName === 'syllabus'
-                          ? sortConfig.order === 'desc'
-                            ? 'asc'
+                        )
+                      }>
+                      {withOrderBy('Curriculum', 'curriculum')}
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
+                      onClick={() =>
+                        handleOrderBy(
+                          'syllabus',
+                          sortConfig.fieldName === 'syllabus'
+                            ? sortConfig.order === 'desc'
+                              ? 'asc'
+                              : 'desc'
                             : 'desc'
-                          : 'desc'
-                      )
-                    }>
-                    {withOrderBy('Syllabus', 'syllabus')}
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
-                    onClick={() =>
-                      handleOrderBy(
-                        'lesson',
-                        sortConfig.fieldName === 'lesson'
-                          ? sortConfig.order === 'desc'
-                            ? 'asc'
+                        )
+                      }>
+                      {withOrderBy('Syllabus', 'syllabus')}
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider"
+                      onClick={() =>
+                        handleOrderBy(
+                          'lesson',
+                          sortConfig.fieldName === 'lesson'
+                            ? sortConfig.order === 'desc'
+                              ? 'asc'
+                              : 'desc'
                             : 'desc'
-                          : 'desc'
-                      )
-                    }>
-                    {withOrderBy('Lesson', 'lesson')}
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
-                    Date
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
-                    Time
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {AttendanceList.map((item: any, idx: number) => {
-                  return (
-                    <tr
-                      key={`${item.class?.name}_${idx}`}
-                      className={idx % 2 === 0 ? 'bg-white' : 'bg-gray-100'}>
-                      <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm font-medium text-gray-900">
-                        {item.curriculum}
-                      </td>
-                      <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
-                        {'-'}
-                      </td>
-                      <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
-                        {'-'}
-                      </td>
-                      <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
-                        {new Date(item.date).toLocaleDateString()}
-                      </td>
-                      <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
-                        {item?.time}
+                        )
+                      }>
+                      {withOrderBy('Lesson', 'lesson')}
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                      Date
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 w-auto text-left text-xs font-semibold text-gray-500 uppercase tracking-wider">
+                      Time
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loading ? (
+                    <tr>
+                      <td colSpan={5} className="py-4">
+                        <Loader />
                       </td>
                     </tr>
-                  );
-                })}
-              </tbody>
-            </table>
+                  ) : (
+                    attendanceList.map((item: any, idx: number) => {
+                      return (
+                        <tr
+                          key={`${item.class?.name}_${idx}`}
+                          className={idx % 2 === 0 ? 'bg-white' : 'bg-gray-100'}>
+                          <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm font-medium text-gray-900">
+                            {item.curriculum?.name || '-'}
+                          </td>
+                          <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
+                            {item.syllabus?.name || '-'}
+                          </td>
+                          <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
+                            {item.lesson?.title || '-'}
+                          </td>
+                          <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
+                            {new Date(item.date).toLocaleDateString()}
+                          </td>
+                          <td className="px-6 py-4 w-auto whitespace-nowrap text-left text-sm text-gray-500">
+                            {item?.time}
+                          </td>
+                        </tr>
+                      );
+                    })
+                  )}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
