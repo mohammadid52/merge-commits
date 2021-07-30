054e944277b27b690b9fcf18149b0a0f04d089cd
Mohammad <mohammad@sublimedatasys.com>
2021-07-30 16:44:17 +0530

    Added new toolbar in teacher view

--- src/components/TeacherView/LessonControlBar/StageButton.tsx
+++ src/components/TeacherView/LessonControlBar/StageButton.tsx
@@ -1,9 +1,10 @@
-import React, { useContext, ReactNode, useState, ReactElement } from 'react';
-import { useHistory, useRouteMatch } from 'react-router-dom';
-import { LessonControlContext } from '../../../contexts/LessonControlContext';
+import React, {useContext, ReactNode, useState, ReactElement} from 'react';
+import {useHistory, useRouteMatch} from 'react-router-dom';
+import {LessonControlContext} from '../../../contexts/LessonControlContext';
 
 import StageLabels from '../../General/LabelSwitch';
-import { GlobalContext } from '../../../contexts/GlobalContext';
+import {GlobalContext} from '../../../contexts/GlobalContext';
+import {UniversalLessonPage} from '../../../interfaces/UniversalLessonInterfaces';
 
 interface StageButtonProps {
   iconID?: number;
@@ -11,11 +12,13 @@ interface StageButtonProps {
   open?: boolean;
   disabled?: boolean;
   label?: string;
+  id?: string;
   breakdown?: boolean;
   menuOpen?: boolean;
   handleOpenMenu?: (stage: string) => void;
   handlePageChange?: any;
   counter?: number;
+  page?: UniversalLessonPage;
 }
 
 const StageButton = (props: StageButtonProps) => {
@@ -30,8 +33,12 @@ const StageButton = (props: StageButtonProps) => {
     label,
     handlePageChange,
     counter,
+    page,
   } = props;
-  const {lessonState, lessonDispatch, controlState, controlDispatch} = useContext(GlobalContext);
+  const {lessonState, id, lessonDispatch, controlState, controlDispatch} = useContext(
+    GlobalContext
+  );
+
   const match = useRouteMatch();
   const history = useHistory();
 
@@ -42,7 +49,7 @@ const StageButton = (props: StageButtonProps) => {
   const stageIsClosed = PAGES[iconID].open === false;
 
   const handleView = () => {
-    controlDispatch({ type: 'QUIT_STUDENT_VIEWING' });
+    controlDispatch({type: 'QUIT_STUDENT_VIEWING'});
 
     /**
      *
@@ -65,7 +72,11 @@ const StageButton = (props: StageButtonProps) => {
               handleView();
             }}
             className={`text-sm font-medium text-gray-500 
-            ${stageIsDisabled ? null : 'hover:font-bold hover:underline hover:text-sea-green'} 
+            ${
+              stageIsDisabled
+                ? null
+                : 'hover:font-bold hover:underline hover:text-sea-green'
+            } 
             ${stageIsViewed && !stageIsClosed && !stageIsDisabled ? 'font-bold' : null}`}>
             <div className="text-blueberry text-center flex flex-row">
               <StageLabels label={PAGES ? label : 'n/a'} />
@@ -94,8 +105,16 @@ const StageButton = (props: StageButtonProps) => {
             ${stageIsClosed || stageIsDisabled ? 'text-gray-500' : null}
             ${stageIsClosed ? 'hover:underline' : null}
             ${stageIsViewed && stageIsClosed ? 'font-bold underline' : null}
-            ${!stageIsClosed && !stageIsDisabled ? 'hover:text-sea-green hover:underline' : null}
-            ${stageIsViewed && !stageIsClosed && !stageIsDisabled ? 'font-bold text-sea-green underline' : null}`}>
+            ${
+              !stageIsClosed && !stageIsDisabled
+                ? 'hover:text-sea-green hover:underline'
+                : null
+            }
+            ${
+              stageIsViewed && !stageIsClosed && !stageIsDisabled
+                ? 'font-bold text-sea-green underline'
+                : null
+            }`}>
             <div className={`pl-2 text-center flex flex-row`}>
               <StageLabels label={PAGES ? label : 'n/a'} />
             </div>
@@ -119,7 +138,11 @@ const StageButton = (props: StageButtonProps) => {
               handleView();
             }}
             className={`text-sm font-medium text-gray-500 
-            ${stageIsDisabled ? null : 'hover:font-bold hover:underline hover:text-sea-green'} 
+            ${
+              stageIsDisabled
+                ? null
+                : 'hover:font-bold hover:underline hover:text-sea-green'
+            } 
             ${stageIsViewed && !stageIsClosed && !stageIsDisabled ? 'font-bold' : null}`}>
             <div
               className={`text-ketchup pl-2 text-center  flex flex-row ${
@@ -133,9 +156,42 @@ const StageButton = (props: StageButtonProps) => {
     }
   };
 
+  const Nav = () => {
+    return (
+      <div className="flex items-center w-auto group">
+        <svg
+          className="flex-shrink-0 w-6 h-full text-gray-300 group-hover:text-gray-400 transition-all duration-150 "
+          viewBox="0 0 24 44"
+          preserveAspectRatio="none"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true">
+          <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+        </svg>
+
+        <a
+          href={page.href}
+          className={` ${
+            stageIsViewed && !stageIsClosed && !stageIsDisabled
+              ? 'border-b-0 border-indigo-400 text-indigo-600 hover:text-indigo-700'
+              : 'text-gray-600 hover:text-gray-700'
+          }
+            ml-4 cursor-pointer w-auto  text-sm font-medium transform hover:scale-110 transition-transform duration-150`}>
+          {page.label}
+        </a>
+      </div>
+    );
+  };
+
   return (
-    <li className="relative flex w-full">{stageButtonChoice()}</li>
-    // </div>
+    <li
+      onClick={() => {
+        handleView();
+      }}
+      className="relative flex w-auto">
+      <Nav />
+      {/* {stageButtonChoice()} */}
+    </li>
   );
 };
 
