22cf7d32e8f9335572957fa03cb165efdf8bca55
Mohammad <mohammad@sublimedatasys.com>
2021-07-30 17:32:01 +0530

    fixed open/close button styles

--- src/components/TeacherView/LessonControlBar/StageButton.tsx
+++ src/components/TeacherView/LessonControlBar/StageButton.tsx
@@ -66,29 +66,34 @@ const StageButton = (props: StageButtonProps) => {
   const stageButtonChoice = () => {
     if (iconID === 0) {
       return (
-        <div className="flex items-center">
+        <div className="flex items-center w-auto group">
+          <svg
+            className="flex-shrink-0 w-6 h-full text-gray-300 group-hover:text-gray-400 transition-all duration-150 "
+            viewBox="0 0 24 44"
+            preserveAspectRatio="none"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true">
+            <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+          </svg>
+
           <a
-            onClick={() => {
-              handleView();
-            }}
-            className={`text-sm font-medium text-gray-500 
-            ${
-              stageIsDisabled
-                ? null
-                : 'hover:font-bold hover:underline hover:text-sea-green'
-            } 
-            ${stageIsViewed && !stageIsClosed && !stageIsDisabled ? 'font-bold' : null}`}>
-            <div className="text-blueberry text-center flex flex-row">
-              <StageLabels label={PAGES ? label : 'n/a'} />
-            </div>
+            href={page.href}
+            className={`  ${
+              stageIsViewed && !stageIsClosed && !stageIsDisabled
+                ? 'border-b-0 border-indigo-400 text-indigo-600 hover:text-indigo-700'
+                : null
+            }        ${stageIsDisabled ? null : 'text-gray-500 '}
+            ml-4 cursor-pointer w-auto  text-sm font-medium transform hover:scale-110 transition-transform duration-150`}>
+            {page.label}
           </a>
         </div>
       );
     } else if (iconID < PAGES.length - 1) {
       return (
-        <div className="flex items-center">
+        <div className="flex items-center w-auto group">
           <svg
-            className="flex-shrink-0 w-2 h-6 text-light-gray"
+            className="flex-shrink-0 w-6 h-full text-gray-300 group-hover:text-gray-400 transition-all duration-150 "
             viewBox="0 0 24 44"
             preserveAspectRatio="none"
             fill="currentColor"
@@ -96,36 +101,31 @@ const StageButton = (props: StageButtonProps) => {
             aria-hidden="true">
             <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
           </svg>
+
           <a
-            onClick={() => {
-              handleView();
-            }}
-            className={`text-sm font-medium 
-            ${stageIsDisabled ? 'line-through' : null}
-            ${stageIsClosed || stageIsDisabled ? 'text-gray-500' : null}
+            href={page.href}
+            className={`${
+              stageIsDisabled || stageIsClosed ? 'line-through text-gray-500' : null
+            }
+            
             ${stageIsClosed ? 'hover:underline' : null}
             ${stageIsViewed && stageIsClosed ? 'font-bold underline' : null}
-            ${
-              !stageIsClosed && !stageIsDisabled
-                ? 'hover:text-sea-green hover:underline'
-                : null
-            }
+            ${!stageIsClosed && !stageIsDisabled ? 'text-gray-500 ' : null}
             ${
               stageIsViewed && !stageIsClosed && !stageIsDisabled
-                ? 'font-bold text-sea-green underline'
+                ? 'border-b-0 border-indigo-400 text-indigo-600 hover:text-indigo-700'
                 : null
-            }`}>
-            <div className={`pl-2 text-center flex flex-row`}>
-              <StageLabels label={PAGES ? label : 'n/a'} />
-            </div>
+            }
+            ml-4 cursor-pointer w-auto  text-sm font-medium transform hover:scale-110 transition-transform duration-150`}>
+            {page.label}
           </a>
         </div>
       );
     } else {
       return (
-        <div className="flex items-center">
+        <div className="flex items-center w-auto group">
           <svg
-            className="flex-shrink-0 w-2 h-6 text-light-gray"
+            className="flex-shrink-0 w-6 h-full text-gray-300 group-hover:text-gray-400 transition-all duration-150 "
             viewBox="0 0 24 44"
             preserveAspectRatio="none"
             fill="currentColor"
@@ -133,54 +133,24 @@ const StageButton = (props: StageButtonProps) => {
             aria-hidden="true">
             <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
           </svg>
+
           <a
-            onClick={() => {
-              handleView();
-            }}
-            className={`text-sm font-medium text-gray-500 
-            ${
-              stageIsDisabled
-                ? null
-                : 'hover:font-bold hover:underline hover:text-sea-green'
-            } 
-            ${stageIsViewed && !stageIsClosed && !stageIsDisabled ? 'font-bold' : null}`}>
-            <div
-              className={`text-ketchup pl-2 text-center  flex flex-row ${
-                stageIsDisabled ? 'line-through text-gray-500' : null
-              }`}>
-              <StageLabels label={PAGES ? label : 'n/a'} />
-            </div>
+            href={page.href}
+            className={`${
+              stageIsViewed && !stageIsClosed && !stageIsDisabled
+                ? 'border-b-0 border-indigo-400 text-indigo-600 hover:text-indigo-700'
+                : null
+            } ${
+              stageIsDisabled || stageIsClosed
+                ? 'line-through text-gray-500'
+                : 'text-gray-500'
+            }
+            ml-4 cursor-pointer w-auto  text-sm font-medium transform hover:scale-110 transition-transform duration-150`}>
+            {page.label}
           </a>
         </div>
       );
     }
-  };
-
-  const Nav = () => {
-    return (
-      <div className="flex items-center w-auto group">
-        <svg
-          className="flex-shrink-0 w-6 h-full text-gray-300 group-hover:text-gray-400 transition-all duration-150 "
-          viewBox="0 0 24 44"
-          preserveAspectRatio="none"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-hidden="true">
-          <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
-        </svg>
-
-        <a
-          href={page.href}
-          className={` ${
-            stageIsViewed && !stageIsClosed && !stageIsDisabled
-              ? 'border-b-0 border-indigo-400 text-indigo-600 hover:text-indigo-700'
-              : 'text-gray-600 hover:text-gray-700'
-          }
-            ml-4 cursor-pointer w-auto  text-sm font-medium transform hover:scale-110 transition-transform duration-150`}>
-          {page.label}
-        </a>
-      </div>
-    );
   };
 
   return (
@@ -189,8 +159,7 @@ const StageButton = (props: StageButtonProps) => {
         handleView();
       }}
       className="relative flex w-auto">
-      <Nav />
-      {/* {stageButtonChoice()} */}
+      {stageButtonChoice()}
     </li>
   );
 };
