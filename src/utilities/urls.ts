9f58cb5aa2ef46b6ea7d6c57772d476fec107fcc
aman-sds <aman@sublimedatasys.com>
2021-08-09 19:43:37 +0530

    create user lambda url

--- src/utilities/urls.ts
+++ src/utilities/urls.ts
@@ -1,1 +1,1 @@
-export const createUserUrl = 'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/create-user';
+export const createUserUrl = 'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/demosite-create-user';
