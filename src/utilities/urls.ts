a3f90960abef0085de30ce92c01b84116580349d
aman-sds <aman@sublimedatasys.com>
2021-08-16 19:38:57 +0530

    connected reset password API lambda fucntion

--- src/utilities/urls.ts
+++ src/utilities/urls.ts
@@ -1,2 +1,2 @@
 export const createUserUrl = 'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/demosite-create-user';
-export const requestResetPassword = '';
+export const requestResetPassword = 'https://eogdfg6pj0.execute-api.us-east-1.amazonaws.com/demosite-request-reset-password';
