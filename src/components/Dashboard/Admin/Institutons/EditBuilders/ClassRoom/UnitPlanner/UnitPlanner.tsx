c3f5d5b66ece16b12d03364dce55edbdc84c1d54
Sonu <sonu@chapter247.com>
2021-09-02 13:04:10 +0530

    fix issue for responsive chevrons on teacher view

--- src/components/Dashboard/Admin/Institutons/EditBuilders/ClassRoom/UnitPlanner/UnitPlanner.tsx
+++ src/components/Dashboard/Admin/Institutons/EditBuilders/ClassRoom/UnitPlanner/UnitPlanner.tsx
@@ -4,6 +4,8 @@ import moment, {Moment} from 'moment';
 
 import * as customQueries from '../../../../../../../customGraphql/customQueries';
 import DatePickerInput from '../../../../../../Atoms/Form/DatePickerInput';
+import Loader from '../../../../../../Atoms/Loader';
+
 import {IImpactLog} from '../ClassRoomHolidays';
 
 const frequencyMapping: {[key: string]: {unit: any; step: number}} = {
@@ -12,6 +14,7 @@ const frequencyMapping: {[key: string]: {unit: any; step: number}} = {
 };
 
 const UnitPlanner = ({roomData}: any) => {
+  const [loading, setLoading] = useState(true);
   const [syllabusList, setSyllabusList] = useState([]);
   const [lessonImpactLogs, setLessonImpactLogs] = useState<IImpactLog[]>([]);
 
@@ -23,12 +26,14 @@ const UnitPlanner = ({roomData}: any) => {
   }, [roomData.curricular?.id]);
 
   const fetchClassRoomSyllabus = async () => {
+    setLoading(true);
     const list: any = await API.graphql(
       graphqlOperation(customQueries.getClassroomSyllabus, {
         id: roomData.curricular?.id,
       })
     );
     setSyllabusList(list.data?.getCurriculum?.universalSyllabus.items || []);
+    setLoading(false);
   };
 
   const getImpactLogs = async () => {
@@ -45,7 +50,7 @@ const UnitPlanner = ({roomData}: any) => {
   const calculateAvailableStartDate = (
     date: Moment,
     frequency: any,
-    step:number,
+    step: number,
     duration: number,
     scheduleDates: Date[]
   ) => {
@@ -125,7 +130,7 @@ const UnitPlanner = ({roomData}: any) => {
                   lastOccupiedDate = Number.isInteger(count)
                     ? moment(item.estEndDate).add(
                         frequencyMapping[roomData.frequency].step,
-                        frequencyMapping[roomData.frequency].unit,
+                        frequencyMapping[roomData.frequency].unit
                       )
                     : item.estEndDate;
                   count = count >= 1 ? 0 : count;
@@ -140,77 +145,87 @@ const UnitPlanner = ({roomData}: any) => {
 
   return (
     <div className="p-4">
-      {syllabusList.map((syllabus: any, index: number) => (
-        <div className="border-0 border-gray-400 rounded-md my-2" key={syllabus.id}>
-          <div className="mb-4 bg-gray-200 flex justify-between">
-            <div className="px-4 py-2">
-              <div className="text-lg">{syllabus.name}</div>
-            </div>
-            <div className="inline-flex">
-              <span className="w-30 inline-flex items-center">Start Date</span>
-              <div className="px-4 py-2">
-                <DatePickerInput
-                  date={syllabus.startDate}
-                  placeholder={'Start Date'}
-                  minDate={new Date()}
-                  onChange={(date: Date | null) => handleDateChange(date, index)}
-                />
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="w-full flex justify-between mt-4">
-              <div className="w-4/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
-                Lesson Name
-              </div>
-              <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
-                Duration ({roomData.frequency})
-              </div>
-              <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
-                Start date
-              </div>
-              <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
-                Est. end date
-              </div>
-              <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
-                Act. end date
-              </div>
-            </div>
-
-            <div className="mb-4 w-full m-auto max-h-88 overflow-y-auto">
-              {syllabus.lessons.items?.length ? (
-                syllabus.lessons.items.map((item: any, idx: number) => {
-                  return (
-                    <div
-                      key={`${idx}`}
-                      className={`flex justify-between bg-white w-full`}>
-                      <div className="w-4/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
-                        {item.lesson?.title}
-                      </div>
-                      <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
-                        {item.lesson?.duration}
-                      </div>
-                      <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
-                        {item.startDate
-                          ? new Date(item.startDate).toLocaleDateString()
-                          : '-'}
-                      </div>
-                      <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
-                        {item.estEndDate
-                          ? new Date(item.estEndDate).toLocaleDateString()
-                          : '-'}
-                      </div>
-                      <div className="w-2/10 flex px-4 py-3 text-gray-500">-</div>
-                    </div>
-                  );
-                })
-              ) : (
-                <div className="text-center p-5">No lesson assigned</div>
-              )}
-            </div>
+      {loading ? (
+        <div className="py-20 text-center mx-auto flex justify-center items-center w-full h-48">
+          <div className="w-5/10">
+            <Loader color="rgba(107, 114, 128, 1)" />
           </div>
         </div>
-      ))}
+      ) : syllabusList.length ? (
+        syllabusList.map((syllabus: any, index: number) => (
+          <div className="border-0 border-gray-400 rounded-md my-2" key={syllabus.id}>
+            <div className="mb-4 bg-gray-200 flex justify-between">
+              <div className="px-4 py-2">
+                <div className="text-lg">{syllabus.name}</div>
+              </div>
+              <div className="inline-flex">
+                <span className="w-30 inline-flex items-center">Start Date</span>
+                <div className="px-4 py-2">
+                  <DatePickerInput
+                    date={syllabus.startDate}
+                    placeholder={'Start Date'}
+                    minDate={new Date()}
+                    onChange={(date: Date | null) => handleDateChange(date, index)}
+                  />
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="w-full flex justify-between mt-4">
+                <div className="w-4/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
+                  Lesson Name
+                </div>
+                <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
+                  Duration ({roomData.frequency})
+                </div>
+                <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
+                  Start date
+                </div>
+                <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
+                  Est. end date
+                </div>
+                <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-700 uppercase tracking-wider whitespace-normal">
+                  Act. end date
+                </div>
+              </div>
+
+              <div className="mb-4 w-full m-auto max-h-88 overflow-y-auto">
+                {syllabus.lessons.items?.length ? (
+                  syllabus.lessons.items.map((item: any, idx: number) => {
+                    return (
+                      <div
+                        key={`${idx}`}
+                        className={`flex justify-between bg-white w-full`}>
+                        <div className="w-4/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
+                          {item.lesson?.title}
+                        </div>
+                        <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
+                          {item.lesson?.duration}
+                        </div>
+                        <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
+                          {item.startDate
+                            ? new Date(item.startDate).toLocaleDateString()
+                            : '-'}
+                        </div>
+                        <div className="w-2/10 flex px-4 py-3 text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider whitespace-normal">
+                          {item.estEndDate
+                            ? new Date(item.estEndDate).toLocaleDateString()
+                            : '-'}
+                        </div>
+                        <div className="w-2/10 flex px-4 py-3 text-gray-500">-</div>
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div className="text-center p-5">No lesson assigned</div>
+                )}
+              </div>
+            </div>
+          </div>
+        ))
+      ) : (
+        <div>No unit added in the course</div>
+      )}
     </div>
   );
 };
