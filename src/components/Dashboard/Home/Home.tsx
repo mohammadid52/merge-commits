f60e46f648fc8bdf7723d12afdcd801fe918f285
samboosa5k <jasper.verbon@gmail.com>
2021-10-08 10:23:30 +0200

    fixed student crashing

--- src/components/Dashboard/Home/Home.tsx
+++ src/components/Dashboard/Home/Home.tsx
@@ -94,7 +94,7 @@ const Home = (props: ClassroomControlProps) => {
     homeData &&
       homeData.length > 0 &&
       homeData.forEach((item: any) => {
-        if (item?.class?.room.length > 0) {
+        if (item?.class?.room) {
           if (item?.class?.room.coTeachers.items.length > 0) {
             item?.class?.room.coTeachers.items.map((_item: any) => {
               if (!uniqIds.includes(_item.teacher.authId)) {
