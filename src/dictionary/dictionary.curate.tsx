0b51a0e8f7183f700a9c616f227ffa10270814f8
sonu247 <sonu@chapter247.com>
2021-10-15 10:17:02 +0530

    add room stepper variable in curate dictionary

--- src/dictionary/dictionary.curate.tsx
+++ src/dictionary/dictionary.curate.tsx
@@ -1697,7 +1697,10 @@ const RoomEDITdict: any = {
     CONFERENCE_CALL_LINK_PLACEHOLDER: 'Enter zoom, teams or meets link here',
     LOCATION_LABEL: 'Classroom location',
     LOCATION_PLACEHOLDER: 'Enter location here',
-    CLASS_DETAILS_TAB_DESCRIPTION: 'Create a classroom & course schedule',
+    CLASS_DETAILS_TAB_HEADING: 'Class Details',
+    CLASS_DETAILS_TAB_DESCRIPTION: 'Create a classroom',
+    CLASS_STUDENT_TAB_HEADING: 'Students',
+    CLASS_STUDENT_TAB_DESCRIPTION: 'Add students to classroom',
     CLASS_DYNAMICS_TAB_HEADING: 'Class Dynamics(optional)',
     CLASS_DYNAMICS_TAB_DESCRIPTION:
       'Manage subject proficiency & create course partners and student groups',
--- src/dictionary/dictionary.iconoclast.tsx
+++ src/dictionary/dictionary.iconoclast.tsx
@@ -1662,7 +1662,7 @@ const RoomEDITdict: any = {
     LOCATION_LABEL: 'Classroom location',
     LOCATION_PLACEHOLDER: 'Enter location here',
     CLASS_DETAILS_TAB_HEADING: 'Class Details',
-    CLASS_DETAILS_TAB_DESCRIPTION: 'Create a classroom & course schedule',
+    CLASS_DETAILS_TAB_DESCRIPTION: 'Create a classroom',
     CLASS_STUDENT_TAB_HEADING: 'Students',
     CLASS_STUDENT_TAB_DESCRIPTION: 'Add students to classroom',
     CLASS_DYNAMICS_TAB_HEADING: 'Class Dynamics(optional)',
