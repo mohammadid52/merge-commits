73a21ca99fdec1274093e8d6091f1357230f8019
ZOICloudServicesCompany <50948576+ZOICloudServicesCompany@users.noreply.github.com>
2021-10-19 09:29:25 +0200

    uatChanges

--- src/utilities/urls.ts
+++ src/utilities/urls.ts
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 export const createUserUrl = 'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/prod-create-user';
 export const requestResetPassword = 'https://eogdfg6pj0.execute-api.us-east-1.amazonaws.com/prod-request-reset-password';
-=======
-export const createUserUrl =
-  'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/create-user';
-export const requestResetPassword =
-  'https://eogdfg6pj0.execute-api.us-east-1.amazonaws.com/uatenv-request-reset-password';
->>>>>>> new-dev
