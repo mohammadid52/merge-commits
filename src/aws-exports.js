d90ee3afc7384cb473b23147f8fbc1d568f27ea5
Mike <micahel.russell@zcsc.co>
2021-10-28 15:10:42 +0200

    uatChanges

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -8,12 +8,6 @@ const awsmobile = {
     "aws_user_pools_id": "us-east-1_MVf87ox0v",
     "aws_user_pools_web_client_id": "79bnbhng10ml2ucaur9vuie7vi",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://m2cm45pahrh5hdf35cngjfdaui.appsync-api.us-east-1.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-nugdxn4bwzhizjsrr2pv2m64ne",
-    "aws_user_files_s3_bucket": "iconoclaststorage143306-dev",
-    "aws_user_files_s3_bucket_region": "us-east-1",
     "aws_cognito_login_mechanisms": [
         "EMAIL"
     ],
@@ -30,7 +24,13 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://m2cm45pahrh5hdf35cngjfdaui.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-nugdxn4bwzhizjsrr2pv2m64ne",
+    "aws_user_files_s3_bucket": "iconoclaststorage143306-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
 
