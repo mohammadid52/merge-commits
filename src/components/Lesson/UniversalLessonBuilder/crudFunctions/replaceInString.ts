7be39e91b02644ca7a07e449b8deb71354a01940
samboosa5k <jasper.verbon@gmail.com>
2021-11-10 14:33:35 +0100

    fixed some small regex issue causing safari cresh

--- src/components/Lesson/UniversalLessonBuilder/crudFunctions/replaceInString.ts
+++ src/components/Lesson/UniversalLessonBuilder/crudFunctions/replaceInString.ts
@@ -1,13 +1,14 @@
 export const replaceTailwindClass = (inputString: string, replacementValue: string) => {
   if (inputString?.includes('bg-') && replacementValue.includes('bg-')) {
-    return inputString.replace(/((?<!:)bg-\w+-\d+)/, replacementValue);
+    return inputString.replace(/bg-(\w+-\d+)/g, replacementValue);
   } else if (inputString?.includes('border-') && replacementValue.includes('border-')) {
-    return inputString.replace(/((?<!:)border-\w\w+-\d+)/, replacementValue);
+    return inputString.replace(/border-(\w+-\d+)/g, replacementValue);
   } else if (inputString?.includes('grid-') && replacementValue.includes('grid-')) {
-    return inputString.replace(/((?<!:)grid-\w+-\d+)/, replacementValue);
-  } else if (inputString ?.includes('text-') && replacementValue.includes('text-')) {
-    return inputString.replace(/((?<!:)text-\w+)/, replacementValue);
+    return inputString.replace(/grid-(\w+-\d+)/g, replacementValue);
+  } else if (inputString?.includes('text-') && replacementValue.includes('text-')) {
+    return inputString.replace(/text-(\w+-\d+)/g, replacementValue);
   } else {
-    return `${inputString||""} ${replacementValue}`;
+    return `${inputString || ''} ${replacementValue}`;
   }
-};
+  // return `${inputString || ''} ${replacementValue}`;
+};;
