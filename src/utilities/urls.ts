1fa3fbf1f79f6eeb650b728c69deb36135be95f6
samboosa5k <jasper.verbon@gmail.com>
2021-11-12 11:15:46 +0100

    urls

--- src/utilities/urls.ts
+++ src/utilities/urls.ts
@@ -10,5 +10,8 @@ export const requestResetPassword =
    * export const createUserUrl = 'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/prod-create-user';
 export const requestResetPassword = 'https://eogdfg6pj0.execute-api.us-east-1.amazonaws.com/prod-request-reset-password';
    * 
+   * CUR:
    * 
+   * export const createUserUrl = 'https://9jk0le8cae.execute-api.us-east-1.amazonaws.com/demosite-create-user';
+export const requestResetPassword = 'https://eogdfg6pj0.execute-api.us-east-1.amazonaws.com/demosite-request-reset-password';
    */
