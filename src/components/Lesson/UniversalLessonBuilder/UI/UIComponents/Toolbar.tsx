64d0eec60c07155c0dbc9453fec14ab7b055db43
Mohammad <mohammad@sublimedatasys.com>
2021-11-15 12:55:26 +0530

    npm install

--- src/components/Lesson/UniversalLessonBuilder/UI/UIComponents/Toolbar.tsx
+++ src/components/Lesson/UniversalLessonBuilder/UI/UIComponents/Toolbar.tsx
@@ -97,7 +97,7 @@ const Toolbar = ({
             ? 'opacity-0 -translate-y-12 scale-90'
             : 'opacity-100 scale-100 translate-y-0'
         } customShadow transform ${
-          previewMode ? 'fixed' : ''
+          previewMode ? 'fixed bottom-4 right-4' : ''
         } rounded-lg toolbar bg-white dark:bg-gray-700 z-1000 ease-out transition-all duration-200  w-auto p-2`}>
         <div className="flex items-center">
           <div className="flex items-center w-auto">
