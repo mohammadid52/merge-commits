a5ea55574cef2091f5a1c79850883fa3ea04796a
Mohammad <reactingdeveloper@gmail.com>
2022-06-01 20:48:40 +0530

    updated text

--- src/components/Dashboard/Classroom/Start.tsx
+++ src/components/Dashboard/Classroom/Start.tsx
@@ -443,15 +443,27 @@ const Start: React.FC<StartProps> = ({
     });
   };
 
+  const buttonText = `${firstPart()} ${secondPart()}`;
+
+  const updateBtnText = () => {
+    switch (buttonText) {
+      case 'SURVEY OPEN':
+        return 'Go To Survey';
+      case 'START LESSON':
+        return 'Go To Lesson';
+
+      default:
+        return buttonText;
+    }
+  };
+
   return (
     <div>
       <Buttons
         type="submit"
         onClick={!preview ? handleLink : noop}
         label={
-          loading
-            ? classRoomDict[userLanguage]['MESSAGES'].PLEASE_WAIT
-            : `${firstPart()} ${secondPart()}`
+          loading ? classRoomDict[userLanguage]['MESSAGES'].PLEASE_WAIT : updateBtnText()
         }
         disabled={
           loading ||
