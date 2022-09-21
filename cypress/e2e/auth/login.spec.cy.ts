f398121afd9eb34fda00909227446d202e166cdd
reactingdeveloper <reactingdeveloper@gmail.com>
2022-09-21 15:45:17 +0530

    cy on fail console added

--- cypress/e2e/auth/login.spec.cy.ts
+++ cypress/e2e/auth/login.spec.cy.ts
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
+
+// test cred
+const testEmail = 'demoteacher@zoiq.io';
+const testPass = 'admin123';
+
 describe('Should Login', () => {
   it('Should login', function () {
     cy.visit('http://localhost:8085').get('p').contains('Verify Email');
-    cy.get('input[placeholder="Email"]').type('demoteacher@zoiq.io');
+
+    cy.get('input[placeholder="Email"]').type(testEmail);
     cy.get('button').contains('Enter').click();
     cy.get('p').contains('Log In');
-    cy.get('input[placeholder="Password"]').type('admin123');
+    cy.get('input[placeholder="Password"]').type(testPass);
     cy.get('button').contains('Login').click();
   });
 });
