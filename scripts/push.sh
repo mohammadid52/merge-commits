629124b9104c4483c56ea0770d394d4de105c901
reactingdeveloper <reactingdeveloper@gmail.com>
2022-10-06 14:14:40 +0530

    push script change

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,6 +5,12 @@ branch=$(git symbolic-ref --short HEAD)
 echo "Do you want to run cypress test on this branch? (y/N) " 
 read run_test
 
+if [ "$branch" == "new-dev" ]; then
+  cp ./src/config/aws-exports.ia.js ./src/aws-exports.js
+  git add ./src/aws-exports.js
+  git commit -m "update aws-exports.js"
+  fi
+
 if [ "$run_test" = "y" ] || [ "$run_test" = "Y" ]; then
   echo "Running cypress test..."
   npm run cypress:run
