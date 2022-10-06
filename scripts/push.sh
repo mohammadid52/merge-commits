da3157bf0eefa26f9b09b643a9593975e9a3ee8a
reactingdeveloper <reactingdeveloper@gmail.com>
2022-10-06 14:31:16 +0530

    added switch case

--- scripts/push.sh
+++ scripts/push.sh
@@ -5,11 +5,38 @@ branch=$(git symbolic-ref --short HEAD)
 echo "Do you want to run cypress test on this branch? (y/N) " 
 read run_test
 
-if [ "$branch" == "new-dev" ]; then
-  cp ./src/config/aws-exports.ia.js ./src/aws-exports.js
-  git add ./src/aws-exports.js
-  git commit -m "update aws-exports.js"
-  fi
+commit (){
+      git add ./src/aws-exports.js
+      git commit -m "update aws-exports.js"
+}
+
+# run switch case
+case $branch in
+    "new-dev")
+          echo "Changing environment to Iconoclast production"
+          cp ./src/config/aws-exports.ia.js ./src/aws-exports.js
+          commit
+        ;;
+    "master")
+          echo "Changing environment to Iconoclast production"
+          cp ./src/config/aws-exports.ia.js ./src/aws-exports.js
+          commit
+        ;;
+    "master-curate")
+          echo "Changing environment to Project Curate production"
+          cp ./src/config/aws-exports.pc.js ./src/aws-exports.js
+          commit
+        ;;
+    *)
+          echo "Changing environment to Development"
+          cp ./src/config/aws-exports.uatenv.js ./src/aws-exports.js
+          commit
+          
+esac
+
+
+
+
 
 if [ "$run_test" = "y" ] || [ "$run_test" = "Y" ]; then
   echo "Running cypress test..."
