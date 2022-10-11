ccde7d3eafcf6fd0de5e2871acac4c1d842fe860
ZOICloudServicesCompany <50948576+ZOICloudServicesCompany@users.noreply.github.com>
2022-10-11 17:15:19 -0500

    sdk

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -32,7 +32,7 @@ const awsmobile = {
     "aws_appsync_apiKey": "da2-m22wnkd3avh73pnioyjnba6grq",
     "aws_user_files_s3_bucket": "iconoclaststorage143306-dev",
     "aws_user_files_s3_bucket_region": "us-east-1",
-    "AWS_SDK_LOAD_CONFIG": 1
+
 };
 
 
