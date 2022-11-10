786366e9d87e4e3453579acdd332f32f27b5ff0b
Zuhair Abbas <zuhairabs@gmail.com>
2022-11-10 17:08:45 +0530

    Add logs to test

--- cypress/e2e/admin/Analytics Manager/research.spec.cy.ts
+++ cypress/e2e/admin/Analytics Manager/research.spec.cy.ts
@@ -2,34 +2,49 @@
 
 import {loginConfig, urlConfig} from '../../config';
 
-const dropdownDetail = {
-  title: 'Analytics Manager',
-  item: 'Download CSV'
-};
+// const dropdownDetail = {
+//   title: 'Analytics Manager',
+//   item: 'Download CSV'
+// };
 
 describe('Research and Analytics', () => {
   beforeEach(() => {
-    cy.login(loginConfig.admin.username, loginConfig.admin.password); // login as admin
+    cy.log('Login as admin');
+    cy.login(loginConfig.admin.username, loginConfig.admin.password);
   });
 
   it('', {defaultCommandTimeout: 20000}, function () {
+    cy.log('Wait for page to load');
     cy.wait(10000); // wait for page to load
-    cy.url().should('contain', urlConfig.dashboardURL); // check if it is on dashboard page
-    cy.wait(10000); // wait for page to load
+    cy.log('Check if it is on the dashboard');
+    cy.url().should('contain', urlConfig.dashboardURL);
+    cy.log('Wait for page to load');
+    cy.wait(10000);
     // cy.get(`button:contains(${dropdownDetail.title})`).trigger('mouseover');
     // cy.get(`div:contains(${dropdownDetail.item})`).first().click();
-    cy.visit(urlConfig.analyticsURL); // go to registration page
-    cy.wait(10000); // wait for page to load
-    cy.dataCy('analytics-classroom-button').click(); // click on dropdown
-    cy.dataCy('analytics-classroom-item-3956fdc3-d36d-43db-b43e-75efa30ea8cc').click(); // click on first item
+    cy.log('Go to registration page');
+    cy.visit(urlConfig.analyticsURL);
+    cy.log('Wait for page to load');
+    cy.wait(10000);
+    cy.log('Click on classroom selector');
+    cy.dataCy('analytics-classroom-button').click();
+    cy.log('Click on the first item');
+    cy.dataCy('analytics-classroom-item-3956fdc3-d36d-43db-b43e-75efa30ea8cc').click();
 
-    cy.wait(3000); // wait for data to load
-    cy.dataCy('analytics-unit-button').click(); // click on dropdown
-    cy.dataCy('analytics-unit-item-1bc60136-42aa-4db8-a9f2-6dc0bf05cd43').click(); // click on first item
+    cy.log('Wait for data to load');
+    cy.wait(3000);
+    cy.log('Click on unit selector');
+    cy.dataCy('analytics-unit-button').click();
+    cy.log('Click on the first item');
+    cy.dataCy('analytics-unit-item-1bc60136-42aa-4db8-a9f2-6dc0bf05cd43').click();
 
-    cy.wait(3000); // wait for data to load
-    cy.dataCy('analytics-survey-button').click(); // click on dropdown
+    cy.log('Wait for data to load');
+    cy.wait(3000);
+    cy.log('Click on survey selector');
+    cy.dataCy('analytics-survey-button').click();
+    cy.log('Click on the first item');
     cy.dataCy('analytics-survey-item-7546b398-acea-440e-ad74-3c007922e2e2').click(); // click on first item
+    cy.log('Table should exists');
     cy.get('table').should('exist'); // check if table exists
   });
 });
