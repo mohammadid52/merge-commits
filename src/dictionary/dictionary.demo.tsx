e82b4ef1a37479d63d27a18548353f0eb98df1bc
reactingdeveloper <reactingdeveloper@gmail.com>
2022-11-15 02:13:42 +0530

    ff

--- src/dictionary/dictionary.demo.tsx
+++ src/dictionary/dictionary.demo.tsx
@@ -311,7 +311,7 @@ const dashboardTestCasesDict: any = {
 const anthologyDict: any = {
   EN: {
     TITLE_CONTAINER: 'Your Notebooks',
-    TITLE: 'Notebooks',
+    TITLE: 'Notebooks p',
     NO_SELECTED: 'No notebook selected...',
     TABS: {
       A: 'Journal',
