5ac411f41c5897a1c63e58e1533a90a425120bdf
Mohammad <mohammadidehgamwala5@gmail.com>
2023-01-02 21:32:10 +0530

    updates to table

--- src/components/Dashboard/Csv/UploadLogsPage.tsx
+++ src/components/Dashboard/Csv/UploadLogsPage.tsx
@@ -7,10 +7,12 @@ import useAuth from '@customHooks/useAuth';
 import useGraphqlQuery from '@customHooks/useGraphqlQuery';
 import {ListUploadLogsQueryVariables, UploadLogs} from 'API';
 import moment from 'moment';
-import React from 'react';
+import React, {useState} from 'react';
 import {Redirect} from 'react-router';
 import Buttons from '@components/Atoms/Buttons';
 import {BiCloudDownload} from 'react-icons/bi';
+import {orderBy} from 'lodash';
+import {HiChevronDown} from 'react-icons/hi';
 
 const UploadLogsPage = () => {
   const {authId, email} = useAuth();
@@ -29,21 +31,20 @@ const UploadLogsPage = () => {
       custom: true,
       enabled: checkIfAdmin(),
       onSuccess: (data) => {
-        // const orderedList = orderBy([...data], ['errorTime'], ['desc']);
-        // setData([...orderedList]);
+        const orderedList = orderBy([...data], ['createdAt'], ['desc']);
+        setData([...orderedList]);
       }
     }
   );
 
-  async function download(key: string) {
-    if (key) {
-      const result = await Storage.get(key, {download: true});
-      // @ts-ignore
-      downloadBlob(result.Body, 'csv');
-    } else {
-      console.error('@download lesson plan key missing');
-    }
-  }
+  const [currentOrder, setCurrentOrder] = useState<boolean | 'asc' | 'desc'>('asc');
+
+  const reorder = () => {
+    setCurrentOrder(currentOrder === 'asc' ? 'desc' : 'asc');
+
+    const orderedList = orderBy([...data], ['createdAt'], [currentOrder]);
+    setData([...orderedList]);
+  };
 
   const commonClass =
     ' text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider';
@@ -54,8 +55,19 @@ const UploadLogsPage = () => {
         <table className="border-collapse table-auto w-full table-hover table-striped">
           <thead className="thead-light bg-gray-50">
             <tr className="p-4 flex items-center justify-between">
-              <th scope="col" className={`w-1/10 ${commonClass}`}>
-                Upload date
+              <th
+                onClick={reorder}
+                scope="col"
+                className={`w-1/10 cursor-pointer hover:theme-text ${commonClass}`}>
+                <span className="flex items-center">
+                  Upload date{' '}
+                  <HiChevronDown
+                    style={{
+                      transform: currentOrder === 'asc' ? 'rotate(180deg)' : 'rotate(0)'
+                    }}
+                    className="text-gray-800 w-auto ml-2"
+                  />
+                </span>
               </th>
 
               <th scope="col" className={`w-1/10 ${commonClass}`}>
